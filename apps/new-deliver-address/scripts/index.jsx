f51c92f517b0de2db2e23a4a3d898771c2c094fa
delong <w.del@qq.com>
2016-04-15 18:28:05 +0800

    fix

--- apps/new-deliver-address/scripts/index.jsx
+++ apps/new-deliver-address/scripts/index.jsx
@@ -14,7 +14,7 @@ const Address = React.createClass({
     },
     saveHandler: function () {
         $FW.Ajax({
-            url: 'mall/api/v1/delivery_address/create.json',
+            url: API_PATH + 'mall/api/v1/delivery_address/create.json',
             method: 'post',
             data: {
                 username: this.state.username,
