40fc7f58fc85471158c764bab23b51ac51a9da15
delong <w.del@qq.com>
2016-04-19 19:27:52 +0800

    fix

--- apps/deliver-address/scripts/index.jsx
+++ apps/deliver-address/scripts/index.jsx
@@ -18,12 +18,16 @@ const DeliverAddress = React.createClass({
         let productBizNo = $FW.Format.urlQuery().productBizNo;
 
         let address = function (i, index) {
+            let link;
+            if (!_this.props.preview) {
+                link = "/order/confirm?address_id=" + i.address_id + "&productBizNo=" + productBizNo
+            }
             return (
                 <div key={index} className="address-panel"
                      onClick={function(){_this.clickHandler(i,index)}}
                      style={{background:"#fff url("+STATIC_PATH+"images/"+(index == _this.state.cur_index ? "checked-circle" :"check-circle")+".png) no-repeat 20px center"}}
                 >
-                    <a href={"/order/confirm?address_id=" + i.address_id + "&productBizNo=" + productBizNo}>
+                    <a href={link}>
                         <div className="username">{i.username}</div>
                         <div className="phone">{i.phone}</div>
                         <div className="address">{i.address}</div>
@@ -31,6 +35,15 @@ const DeliverAddress = React.createClass({
                 </div>
             )
         };
+
+        let create_address;
+        if (!this.props.preview) {
+            create_address = (
+                <div className="bottom-panel">
+                    <a href="/delivery_address/create" className="create-address">+新建地址</a>
+                </div>
+            )
+        }
 
         return (
             <div>
@@ -42,9 +55,7 @@ const DeliverAddress = React.createClass({
                 <div className="address-list">
                     {this.props.address.map(address)}
                 </div>
-                <div className="bottom-panel">
-                    <a href="/delivery_address/create" className="create-address">+新建地址</a>
-                </div>
+                {create_address}
             </div>
         )
     }
@@ -55,7 +66,8 @@ $FW.DOMReady(function () {
     $FW.Ajax({
         url: API_PATH + 'mall/api/member/v1/delivery_address.json',
         success: function (data) {
-            ReactDOM.render(<DeliverAddress address={data.address_list}/>, document.getElementById('cnt'));
+            let preview = $FW.Format.urlQuery().preview == 'true';
+            ReactDOM.render(<DeliverAddress address={data.address_list} preview={}/>, document.getElementById('cnt'));
         }
     });
 });
