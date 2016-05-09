15e328d21099bb13cb8bb6e4eb67871ba4f532f8
delong <w.del@qq.com>
2016-05-09 16:38:04 +0800

    typo

--- apps/order-confirm/scripts/index.jsx
+++ apps/order-confirm/scripts/index.jsx
@@ -157,8 +157,8 @@ $FW.DOMReady(function () {
     if (!query.productBizNo) $FW.Component.Alert('product bizNo not in url query');
 
     $FW.Ajax({
-        //url: API_PATH + 'mall/api/order/v1/pre_pay_order.json?productBizNo=' + query.productBizNo + '&buyNum=' + query.count,
-        url: 'http://localhost/pre_pay_order.json',
+        url: API_PATH + 'mall/api/order/v1/pre_pay_order.json?productBizNo=' + query.productBizNo + '&buyNum=' + query.count,
+        //url: 'http://localhost/pre_pay_order.json',
         enable_loading: true,
         success: function (data) {
 
