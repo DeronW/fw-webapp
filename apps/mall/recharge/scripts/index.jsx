c8236f4cb8cd046d5733a877972bc7c16c520d54
彭焱 <pengyan@ucfgroup.com>
2016-07-07 15:09:16 +0800

    react coding without any sparking clues

--- apps/mall/recharge/scripts/index.jsx
+++ apps/mall/recharge/scripts/index.jsx
@@ -3,11 +3,25 @@
 const API_PATH = document.getElementById('api-path').value;
 
 const Recharge = React.createClass({
+    //getInitialState: function(){
+    //    return {
+    //        fee: null
+    //    }
+    //},
+    //componentDidMount: function(){
+    //    $FW.Ajax({
+    //        success: {
+    //            _this.setState({fee: fee})
+    //        }
+    //    })
+    //},
     render : function(){
         return (
-            <div>
+            <div id="recharge">
                 <Chargetab/>
-                <Telchargecon/>
+                <div className="telconfirm-btn">
+                    <a href="javascript:void(0)">立即充值</a>
+                </div>
                 <Confirmpop/>
             </div>
         );
@@ -27,27 +41,44 @@ const Chargetab = React.createClass({
     }
 });
 
-const Phonenumber = React.createClass({
-    propTypes : {
-        number: React.PropTypes.number
-    },
+const Feecharge = React.createClass({
     render : function(){
+        let card = (data, index) => {
+            if(index==0){
+                return <div className="value-box" onclick={this.clickHandler}>
+                    <span className="value-num">{data+"元"}</span>
+                    <span className="limited-sale">限时抢购</span>
+                </div>
+            }else{
+                return <div className="value-box" onclick={this.clickHandler}>
+                    <span className="value-num">{data+"元"}</span>
+                </div>
+            }
+        };
         return (
-            <div className="phonenumber-wrap">
-                <input className="phone-input" type="text" placeholder="请输入手机号" number="true"/>
-                <span className="phone-icon"></span>
+            <div>
+                <div className="phonenumber-wrap">
+                    <input className="phone-input" type="text" placeholder="请输入手机号" number="true"/>
+                    <span className="phone-icon"></span>
+                </div>
+                <div className="value-wrap">
+                   {this.props.commodity_data.charge.map(card)}
+                </div>
             </div>
         )
     }
 });
 
 const Telpayment = React.createClass({
+    //changeScore: function(x){
+    //    this.setState({needPay: x})
+    //},
     render : function(){
         return (
             <div className="payment-wrap">
                 <div className="payment-way">支付方式<span>工分支付</span></div>
                 <div className="avail-gongfeng-wrap">
-                    <div className="avail-gonfeng">可用工分：<span>{this.props.user_data.is_login ? "--" : this.props.user_data.score}</span></div>
+                    <div className="avail-gonfeng">可用工分：<span>{this.props.user_data.is_login ? this.props.user_data.score : "--"}</span></div>
                     <div className="total-gonfeng">总计：<span>100工分</span></div>
                 </div>
             </div>
@@ -78,72 +109,25 @@ const Confirmpop = React.createClass({
     }
 });
 
-const Telchargecon = React.createClass({
-    getInitialState : function(){
-        return {
-            isDefault : true,
-            selected : true,
-            marked : 5
-        }
-    },
-    clickHandler: function(){
-        this.setState({selected : !selected});
-    },
-    render : function(){
-        let card = (data, index) => {
-            var selected = this.state.selected ? "selected" : null;
-            if(index==0){
-                return <div className="value-box" onclick={this.clickHandler}>
-                    <span className="value-num">{data+"元"}</span>
-                    <span className="limited-sale">限时抢购</span>
-                </div>
-            }else{
-                return <div className="value-box" onclick={this.clickHandler}>
-                    <span className="value-num">{data+"元"}</span>
-                </div>
-            }
-        };
-        return (
-            <div>
-                <Phonenumber/>
-                <div className="value-wrap">
-                    {[10, 20, 30, 50, 100, 200, 300, 500].map(card)}
-                </div>
-                <Telpayment/>
-                <div className="telconfirm-btn">
-                    <a href="javascript:void(0)">立即充值</a>
-                </div>
-            </div>
-        );
-    }
-});
-
 $FW.DOMReady(function () {
     NativeBridge.setTitle('充值专区');
     if (!$FW.Browser.inApp()) {
         ReactDOM.render(<Header title={"充值专区"}/>, document.getElementById('header'));
     }
     $FW.Ajax({
-        url: 'http://10.10.100.112/mockjs/4/api/v1/phone/fee/recharge.json',
+        url: 'http://localhost/recharge.json',
         method: 'get',
-        enable_loading: true,
         success : function(data){
-            console.log(data);
-            //var code1 = data.code, bizNo = data.charge.bizNo, price = data.charge.price, subtitle = data.charge.sub_title, title = data.charge.title;
-            ReactDOM.render(<Telchargecon commodity_data={data}/>,
-                document.getElementById('cnt'));
+            ReactDOM.render(<Feecharge commodity_data={data}/>,
+                document.getElementById('recharge'));
         }
     });
     $FW.Ajax({
-        url: 'http://10.10.100.112/mockjs/4/api/v1/user-state.json',
+        url: 'http://localhost/user-state.json',
         method: 'get',
-        enable_loading: true,
         success : function(data){
-            console.log(data);
-            //var code0 = data.code, islogin = data.is_login, score = data.score;
-            console.log(data.is_login);
             ReactDOM.render(<Telpayment user_data={data} />,
-                document.getElementById('cnt'));
+                document.getElementById('recharge'));
         }
     });
     ReactDOM.render(<Recharge/>, document.getElementById('cnt'));
