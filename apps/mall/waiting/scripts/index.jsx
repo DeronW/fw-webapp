7823dc3aee7ffb15bec2921b5dc2c02d38c76d99
delong <w.del@qq.com>
2016-07-11 17:25:54 +0800

    no message

--- apps/mall/waiting/scripts/index.jsx
+++ apps/mall/waiting/scripts/index.jsx
@@ -24,12 +24,6 @@ const Waiting = React.createClass({
         return (
             <div>
                 {$FW.Browser.inApp() ? null : <Header title={'敬请期待'} background={'transparent'}/>}
-                <BannerGroup className="xxx" autoPlay={3000} loop={true} images={[
-                    'http://placehold.it/350x150',
-                    'http://placehold.it/350x150',
-                    'http://placehold.it/350x150',
-                    'http://placehold.it/350x150'
-                ]} />
                 <div className="waiting-wrap">
                     <img style={style_a} src="images/working.png"/>
                     <div style={style_b}>正在建设中<br/>敬请期待</div>
