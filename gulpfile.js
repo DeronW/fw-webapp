c4bc10754b41643625b64a042fbd88ade79648ce
彭焱 <pengyan@ucfgroup.com>
2016-07-21 11:45:10 +0800

    add slash to the testing env

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,7 @@ MALL_APP_NAMES.forEach(function (i) {
 
     gt('mall', i, {
         debug: true,
-        api_path: 'http://m2mall.9888.cn',
+        api_path: 'http://localhost/fake/',
         include_components: common_components,
         include_common_js: common_js,
         width_swipe: true
