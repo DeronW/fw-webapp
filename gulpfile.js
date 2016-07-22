a32751bc7f8f5e2dc8dc2481bf9120494be97981
彭焱 <pengyan@ucfgroup.com>
2016-07-22 12:37:07 +0800

    css modify

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,7 @@ MALL_APP_NAMES.forEach(function (i) {
 
     gt('mall', i, {
         debug: true,
-        api_path: 'http://localhost/fake/',
+        api_path: 'http://10.105.7.71/',
         include_components: common_components,
         include_common_js: common_js,
         width_swipe: true
