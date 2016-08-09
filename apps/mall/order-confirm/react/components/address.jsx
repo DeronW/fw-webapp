85e5ccbc0ddd03d9cf5b385b6181114dc40d867d
delong <w.del@qq.com>
2016-08-09 13:43:35 +0800

    no message

--- apps/mall/order-confirm/react/components/address.jsx
+++ apps/mall/order-confirm/react/components/address.jsx
@@ -33,7 +33,7 @@ AddressPanel.New = React.createClass({
 AddressPanel.List = React.createClass({
     render: function () {
         let address = this.props.address;
-        let address_list_link = "/static/mall/delivery-address/index.html?productBizNo=" +
+        let address_list_link = "/static/mall/deliver-address/index.html?productBizNo=" +
             this.props.product_biz_no + '&count=' + this.props.product_count + '&address_id=' + this.props.address.id;
 
         return (
