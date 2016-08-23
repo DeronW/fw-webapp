46bdcf37847c2841d074d8b579f764f4540ba98b
delong <w.del@qq.com>
2016-08-23 14:23:00 +0800

    no message

--- apps/main/vip-prerogative/react/index.jsx
+++ apps/main/vip-prerogative/react/index.jsx
@@ -1,3 +1,7 @@
 $FW.DOMReady(function () {
-    ReactDOM.render(<Header title={'升级攻略'}/>, document.getElementById('header'))
+    if ($FW.Browser.inApp()) {
+        NativeBridge.setTitle('升级攻略');
+    } else {
+        ReactDOM.render(<Header title={'升级攻略'}/>, document.getElementById('header'));
+    }
 });
