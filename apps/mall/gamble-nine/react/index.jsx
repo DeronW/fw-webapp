8c8fbbd5ad65fc65ebe20f68756bfbd6e232ac71
xiangwenlong <xiangwenlong@ucfgroup.com>
2016-08-25 15:21:37 +0800

    1

--- apps/mall/gamble-nine/react/index.jsx
+++ apps/mall/gamble-nine/react/index.jsx
@@ -1,5 +1,4 @@
 'use strict';
-
 const API_PATH = document.getElementById('api-path').value;
 const NineActivity = React.createClass({
     getInitialState: function () {
@@ -8,7 +7,7 @@ const NineActivity = React.createClass({
             showPopPrize: false,
             usableDraw: true,
             moveNum: 0,
-            prize_list:this.props.prizeList.list,    
+            prize_list:this.props.prizeList.list,
             usableScore:this.props.cost.usableScore,
             remainTimes:this.props.cost.remainTimes,
             masker:null,
@@ -393,8 +392,9 @@ $FW.DOMReady(function () {
       API_PATH + '/mall/api/magic/v1/winnersList.json?activityId=1ead8644a476448e8f71a72da29139ff&num=20',//获奖名单        
   ], function (data) {
         var user = data[0], cost = data[1],prizeList=data[2];
-        console.log(user);console.log(cost);console.log(prizeList.data.list);
+        console.log(user);console.log(cost);console.log(typeof(prizeList.data));console.log(typeof(prizeList.data.length))
         if (typeof(user) == 'undefined' || typeof(cost) == 'undefined' || typeof(prizeList) == 'undefined') $FW.Component.Alert('error: empty data received');
+        console.log(prizeList.data.list);
         ReactDOM.render(<NineActivity user={user.data} cost={cost.data} prizeList={prizeList.data}/>,
         document.getElementById('cnt'));
   }, true);
