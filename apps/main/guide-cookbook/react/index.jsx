c8e32077d50c2b7171e4c8b68d285180fb07109b
delong <w.del@qq.com>
2016-08-29 14:25:26 +0800

    no message

--- apps/main/guide-cookbook/react/index.jsx
+++ apps/main/guide-cookbook/react/index.jsx
@@ -2,9 +2,9 @@ const API_PATH = document.getElementById('api-path').value;
 
 $FW.DOMReady(function () {
     if ($FW.Browser.inApp()) {
-        NativeBridge.setTitle('升级攻略');
+        NativeBridge.setTitle('玩赚攻略');
     } else {
-        ReactDOM.render(<Header title={'升级攻略'}/>, document.getElementById('header'));
+        ReactDOM.render(<Header title={'玩赚攻略'}/>, document.getElementById('header'));
     }
 });
 
