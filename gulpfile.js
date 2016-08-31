cdf0c86aae2919b9803382e3fa98920b23ac5051
delong <w.del@qq.com>
2016-08-31 14:12:41 +0800

    rename

--- gulpfile.js
+++ gulpfile.js
@@ -22,10 +22,10 @@ const MAIN_APP_NAMES = [
     // 徽商相关页面
     'bind-bank-card', // 绑定银行卡
     'open-account', // 原 hui-shang-bank, 新老用户开户页面
-    'hui-shang-user',
-    'hui-shang-settings-password',
-    'hui-shang-succeed',
-    'hui-shang-resultList',
+    'hui-shang-account', // 徽商存管帐户
+    'config-deal-password', // 原 'hui-shang-settings-password', 设置交易密码
+    'open-account-complete', // 原 'hui-shang-succeed' 徽商开户成功页面
+    'hui-shang-cash-flow', // 原 'hui-shang-resultList', // 徽商资金流水列表
     'register-success', // 新用户注册成功后跳转落地页
     'bind-card',
     'change-bank-card',
