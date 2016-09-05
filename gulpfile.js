d9f89a31238814b876b83579cdc4c363913a596c
delong <w.del@qq.com>
2016-09-05 16:27:44 +0800

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -25,29 +25,29 @@ const MAIN_APP_NAMES = [
     'user-level', // 用户等级详情
     'user-contribute', // 用户贡献值
 
-    // // 徽商相关页面
-    // 'bind-bank-card', // 绑定银行卡
-    // 'open-account', // 原 hui-shang-bank, 新老用户开户页面
-    // 'hui-shang-account', // 徽商存管帐户
-    // 'set-deal-password', // 原 'hui-shang-settings-password', 设置交易密码
-    // 'reset-deal-password', //第二次设置交易密码
-    // 'open-account-complete', // 原 'hui-shang-succeed' 徽商开户成功页面
-    // 'hui-shang-cash-flow', // 原 'hui-shang-resultList', // 徽商资金流水列表
-    // 'register-success', // 新用户注册成功后跳转落地页
-    // 'open-account-fail', //开户失败
-    // 'bind-card',
-    // 'change-bank-card',
-    // 'recharge-recording', // 充值记录, 暂未启用
-    // 'recharge', // 给金融工场账户充值
-    // // 'personal-center',
-    // // 'first-page',
-    // 'cash',
-    // 'special-cash',
-    // 'verify-identity',
-    // 'open-bank',
-    // 'hs-topic', //徽商专题页面
-    // 'trusteeship-pact', // 资金托管协议
-    // 'counseling-pact', // 咨询服务协议
+    // 徽商相关页面
+    'bind-bank-card', // 绑定银行卡
+    'open-account', // 原 hui-shang-bank, 新老用户开户页面
+    'hui-shang-account', // 徽商存管帐户
+    'set-deal-password', // 原 'hui-shang-settings-password', 设置交易密码
+    'reset-deal-password', //第二次设置交易密码
+    'open-account-complete', // 原 'hui-shang-succeed' 徽商开户成功页面
+    'hui-shang-cash-flow', // 原 'hui-shang-resultList', // 徽商资金流水列表
+    'register-success', // 新用户注册成功后跳转落地页
+    'open-account-fail', //开户失败
+    'bind-card',
+    'change-bank-card',
+    'recharge-recording', // 充值记录, 暂未启用
+    'recharge', // 给金融工场账户充值
+    // 'personal-center',
+    // 'first-page',
+    'cash',
+    'special-cash',
+    'verify-identity',
+    'open-bank',
+    'hs-topic', //徽商专题页面
+    'trusteeship-pact', // 资金托管协议
+    'counseling-pact', // 咨询服务协议
 ];
 MAIN_APP_NAMES.forEach(function (i) {
 
