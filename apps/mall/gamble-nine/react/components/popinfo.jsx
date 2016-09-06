6e347eea86654a9cc52ed4462ede06603789bbf0
xiangwenlong <xiangwenlong@ucfgroup.com>
2016-09-06 10:15:50 +0800

    1

--- apps/mall/gamble-nine/react/components/popinfo.jsx
+++ apps/mall/gamble-nine/react/components/popinfo.jsx
@@ -11,11 +11,11 @@ const PopInf = React.createClass({
                         </div>
                         <div className="pop-inf-li">
                             <div className="pop-inf-h">2.活动奖品:</div>
-                            <div className="pop-inf-detail">phone6s(64G)、小辣椒手机、经典干红葡萄酒、爱奇艺月卡爱奇艺周卡、5元返现券、10工分、5工分</div>
+                            <div className="pop-inf-detail">iPhone6s(64G)、小辣椒手机、经典干红葡萄酒、爱奇艺月卡、爱奇艺周卡、5元返现券、10工分、5工分</div>
                         </div>
                         <div className="pop-inf-li">
                             <div className="pop-inf-h">3.活动内容:幸运大转盘</div>
-                            <div className="pop-inf-detail">① 普通用户可转动10次/每天;<br/>② V1用户可转动15次/每天;<br/>③ V2用户可转动20次/每天;<br/>④ V3用户可转动25次/每天;<br/>⑤ V4用户可转动30次/每天。</div>
+                            <div className="pop-inf-detail">① 普通会员可转动10次/每天;<br/>② VIP1会员可转动15次/每天;<br/>③ VIP2会员可转动20次/每天;<br/>④ VIP3会员可转动25次/每天;<br/>⑤ VIP4会员可转动30次/每天。</div>
                         </div>
                         <div className="pop-inf-li">
                         	<div className="pop-inf-h">4.活动说明:</div>
