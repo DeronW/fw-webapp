a9ddc6fd898c16c34b074190d0458c2604a635f6
xiangwenlong <xiangwenlong@ucfgroup.com>
2016-09-06 10:55:12 +0800

    1

--- apps/mall/gamble-nine/react/components/popinfo.jsx
+++ apps/mall/gamble-nine/react/components/popinfo.jsx
@@ -7,19 +7,19 @@ const PopInf = React.createClass({
                     <div className="pop-inf-cnt">
                         <div className="pop-inf-li">
                             <div className="pop-inf-h">1.活动时间:</div>
-                            <div className="pop-inf-detail">2016年9月2日-9月8日</div>
+                            <div className="pop-inf-detail">2016年9月5日-9月11日</div>
                         </div>
                         <div className="pop-inf-li">
                             <div className="pop-inf-h">2.活动奖品:</div>
-                            <div className="pop-inf-detail">phone6s(64G)、小辣椒手机、经典干红葡萄酒、爱奇艺月卡爱奇艺周卡、5元返现券、10工分、5工分</div>
+                            <div className="pop-inf-detail">iPhone6s(64G)、小辣椒手机、经典干红葡萄酒、爱奇艺月卡、爱奇艺周卡、5元返现券、10工分、5工分</div>
                         </div>
                         <div className="pop-inf-li">
                             <div className="pop-inf-h">3.活动内容:幸运大转盘</div>
-                            <div className="pop-inf-detail">① 普通用户可转动10次/每天;<br/>② V1用户可转动15次/每天;<br/>③ V2用户可转动20次/每天;<br/>④ V3用户可转动25次/每天;<br/>⑤ V4用户可转动30次/每天。</div>
+                            <div className="pop-inf-detail">① 普通会员可转动10次/每天;<br/>② VIP1会员可转动15次/每天;<br/>③ VIP2会员可转动20次/每天;<br/>④ VIP3会员可转动25次/每天;<br/>⑤ VIP4会员可转动30次/每天。</div>
                         </div>
                         <div className="pop-inf-li">
                         	<div className="pop-inf-h">4.活动说明:</div>
-                            <div className="pop-inf-detail">① 10工分即可参与大转盘抽奖一次，100%有奖；<br/>② 抽奖获得任意奖品，均实时发放（实物奖品以兑换券形式实时发放到工场账户，中奖者到豆哥商城自行兑换）；<br/>③ 所获得5元返现券为全场通用券，不限标的（债权转让除外）；<br/>④ 本活动规则解释权归金融工场所有。<br/></div>
+                            <div className="pop-inf-detail">① 10工分即可参与大转盘抽奖一次，100%有奖；<br/>② 抽奖获得任意奖品，均实时发放（实物奖品以兑换券形式实时发放到工场账户，兑换券有效期30天。请中奖者收到兑换券后30天内到豆哥商城自行兑换，过期按自动放弃领取奖品对待）；<br/>③ 所获得5元返现券为全场通用券，不限标的（债权转让除外）；<br/>④ 本活动规则解释权归金融工场所有。<br/></div>
                         </div>                       
                     </div>
                     <div className="pop-inf-close" onClick={this.props.hidePopInf}></div>
--- apps/mall/gamble-nine/react/index.jsx
+++ apps/mall/gamble-nine/react/index.jsx
@@ -21,7 +21,10 @@ const NineActivity = React.createClass({
     componentDidMount: function () {
         $FW.Ajax({
             url: `${API_PATH}mall/api/magic/v1/winnersList.json?activityId=${ACTIVITY_ID}&num=20`,//获奖名单
-            success: (data) => this.setState({prize_list: data.list})
+            success: (data) =>{
+            	data.list=data.list||[];
+            	this.setState({prize_list: data.list})
+            } 
         });
     },
 
