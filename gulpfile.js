5d22a9f264c4ce7223d99e3bf638472532a62d9d
delong <w.del@qq.com>
2016-09-12 10:14:33 +0800

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,6 @@ const MAIN_APP_NAMES = [
     'open-account-fail', //开户失败
     'recharge', // 给金融工场账户充值
     'cash', // 用户提现
-    'hs-topic', //徽商专题页面
     'protocol-trusteeship', // 资金托管协议
     'protocol-counseling', // 咨询服务协议
 
