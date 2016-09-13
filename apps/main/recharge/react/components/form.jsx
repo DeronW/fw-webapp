a17785d2f493f1fc7f409dda0487b0279ed112b9
彭焱 <pengyan@ucfgroup.com>
2016-09-13 15:20:33 +0800

    修改提示语逻辑

--- apps/main/recharge/react/components/form.jsx
+++ apps/main/recharge/react/components/form.jsx
@@ -65,10 +65,7 @@ const Form = React.createClass({
         if(this.state.money < 1) {
             $FW.Component.Toast("充值金额不能低于1元");
             return false;
-        }
-
-
-        if (!this.state.money) {
+        } else if (!this.state.money) {
             $FW.Component.Alert('请输入充值金额')
         } else if (!this.state.phone) {
             $FW.Component.Alert('请输入银行预留手机号')
