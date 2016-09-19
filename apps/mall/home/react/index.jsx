0b5c38fc754099843497f324bad0975d565cf936
xiangwenlong <xiangwenlong@ucfgroup.com>
2016-09-19 17:32:30 +0800

    改首页new-home为home

--- apps/mall/home/react/index.jsx
+++ apps/mall/home/react/index.jsx
@@ -14,6 +14,22 @@ function gotoHandler(link, need_login) {
 }
 
 const Mall = React.createClass({
+	getInitialState: function () {
+        return {
+            user_score: '--'
+        }
+    },
+    componentDidMount: function () {
+        $FW.Ajax({
+            url: `${API_PATH}/api/v1/user-state.json`,//登录状态及工分
+            success: (data) =>{
+            	if(data.is_login){
+            		this.setState({user_score:data.score})
+            	}
+            	
+            } 
+        });
+    },
     getHeadImages: function () {
         var images = [];
         var bs = this.props.banners;
@@ -35,20 +51,32 @@ const Mall = React.createClass({
             return <ActivityProduct title={i.title} img={i.img} bizNo={i.bizNo}
                                     activity_id={i.activity_id} products={i.products} key={index}/>;
         };
-
+		let backFactory=()=>{
+			return $FW.Browser.inApp() ?<img className="m-logo" src="images/m-logo.png"/>:<a className="back-factory" href="http://m.9888.cn/mpwap/">回到工场</a>
+		};
         return (
-            <div>
+            <div className="head-wrap">
                 {this.props.banners.length ?
                     <BannerGroup className="head-images" images={this.getHeadImages()}
                                  onImageClick={this.onImageClickHandler}/> :
                     <div className="no-banner"></div>}
-
+                <div className="head-items">
+                    {backFactory()}
+                    <a onClick={function(){ gotoHandler("/static/mall/product-list/index.html?searchSourceType=2") }} className="search-bar-a"><input className="search-bar" type="text" value="" placeholder="搜索"/></a>
+                    <img className="search-icon" src="images/search-icon.png"/>
+                    <a className="index-avatar" onClick={function(){ gotoHandler("/static/mall/user/index.html", true) }}><img src="images/profile-icon.png"/></a>
+                </div>
+                <div className="head-user-info">
+                     <div className="my-score"><span className="my-score-icon"></span>我的工分:<span className="score-num">{this.state.user_score}工分</span></div>
+                     <a className="my-exchange" onClick={function(){ gotoHandler("/static/mall/product-list/index.html?searchSourceType=1&title=我可兑换", true) }}><span className="my-exchange-icon"></span>我可兑换</a>
+                     <a className="my-mall" onClick={function(){ gotoHandler("/static/mall/user/index.html", true) }}><span className="my-mall-icon"></span>我的商城</a>
+                     <span className="vertical-gray-line"></span>
+                </div>
                 <div className="header-nav">
-                    <a className="recharge" onClick={function(){ gotoHandler("/static/mall/product-recharge/index.html",true) }}>话费充值</a>
+                    <a className="recharge" onClick={function(){ gotoHandler("/static/mall/product-recharge/index.html",true) }}>充值中心</a>
                     <a className="vip" onClick={function(){ gotoHandler("/static/mall/product-vip-zone/index.html") }}>VIP专区</a>
-                    <a className="play" onClick={function(){ gotoHandler("/static/mall/gamble-nine/index.html?"+(+new Date()), true) }}>玩玩乐</a>
-                    <a className="goods" onClick={function(){ gotoHandler("/static/mall/product-list/index.html") }}>全部商品</a>
-                    <a className="mine" onClick={function(){ gotoHandler("/static/mall/user/index.html", true) }}>我的商城</a>
+                    <a className="goods" onClick={function(){ gotoHandler("/static/mall/gamble-nine/index.html?"+(+new Date()), true) }}>大转盘</a>
+                    <a className="mine" onClick={function(){ gotoHandler("/user", true) }}>品类</a>
                 </div>
                 <div className="index-actList-wrap">
                     { this.props.activities.map(activity) }
@@ -149,10 +177,6 @@ const ProductItem = React.createClass({
 });
 
 $FW.DOMReady(function () {
-
-    // just for dev
-    if(location.pathname == '/') location.href = '/static/mall/home/index.html';
-
     NativeBridge.setTitle('豆哥商城');
     $FW.BatchGet([
         API_PATH + 'mall/api/index/v1/banners.json', // banner轮播图数据
@@ -163,9 +187,4 @@ $FW.DOMReady(function () {
         ReactDOM.render(<Mall banners={banners} activities={activities}/>, document.getElementById('cnt'));
     }, true);
 
-    if ($FW.Utils.shouldShowHeader()) {
-        ReactDOM.render(<Header title_img='images/dg-mall-title.png' show_back_btn={!$FW.Browser.inApp()}
-                                back_handler={ () => location.href = 'http://m.9888.cn' }/>,
-            document.getElementById('header'));
-    }
 });
