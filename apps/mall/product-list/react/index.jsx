94f311cfa59b5684382dc7e6fc47306feb9dc56c
xiangwenlong <xiangwenlong@ucfgroup.com>
2016-09-20 17:23:18 +0800

    no message

--- apps/mall/product-list/react/index.jsx
+++ apps/mall/product-list/react/index.jsx
@@ -37,7 +37,7 @@ const ResultPage = React.createClass({
                     {this.state.products.map((p, index) => <ProductItem {...p} key={index}/>) }
                     {this.state.products.length ?
                         null :
-                        <div className="empty-list">暂无商品</div>}
+                        <div href="http://mmall.9888.cn/" className="empty-list">暂无商品</div>}
                 </div>
     		)
     	}
