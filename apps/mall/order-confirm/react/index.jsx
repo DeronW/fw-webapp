348d50ab9c01a0143ef2351d8016374cf2872b32
xiangwenlong <xiangwenlong@ucfgroup.com>
2016-10-10 14:36:40 +0800

    更改确认商品

--- apps/mall/order-confirm/react/index.jsx
+++ apps/mall/order-confirm/react/index.jsx
@@ -19,7 +19,7 @@ const ConfirmOrder = React.createClass({
             productBizNo: query.productBizNo,
             useTicket: null,
             ticket: [],
-            tokenStr: query.tokenStr,
+            tokenStr: '',
             sms_code: null,
             addressId: this.props.default_address_id,
             vipLevel: this.props.vipLevel,
@@ -32,6 +32,15 @@ const ConfirmOrder = React.createClass({
         }
     },
     componentDidMount: function () {
+        this.refreshTokenStr()
+    },
+    refreshTokenStr: function () {
+        $FW.Ajax({
+            url: API_PATH + '/mall/api/order/v1/getTokenStr.json',
+            success: function (data) {
+                window._form_data.tokenStr = data.tokenStr;
+            }
+        });
     },
     componentDidUpdate: function () {
         this.can_buy(true)
@@ -76,9 +85,10 @@ const ConfirmOrder = React.createClass({
                 url: API_PATH + '/mall/api/order/v1/commit_pay_order.json',
                 enable_loading: true,
                 data: this.FormData,
-                success: function (data) {
+                success: (data) => {
                     if (data.errMsg) {
-                        $FW.Component.Alert(data.errMsg)
+                        $FW.Component.Alert(data.errMsg);
+                        this.refreshTokenStr()
                     } else {
                         location.href = '/order/complete?id=' + data.orderId
                     }
