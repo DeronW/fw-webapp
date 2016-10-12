ec92306c9dae79435ef261d3cffd2d56139684ea
jiabin.xiong <jiabin.xiong@imduoduo.com>
2016-10-12 18:04:25 +0800

    test

--- apps/main/topic-hui-shang/react/index.jsx
+++ apps/main/topic-hui-shang/react/index.jsx
@@ -1,3 +1,10 @@
+$FW.DOMReady(function() {    
+    if($FW.Browser.inApp()) {    
+        document.getElementById("header").style.display = "none";
+        document.getElementById("openUserBtn").style.display = "none";
+    }        
+});
+
 $FW.DOMReady(function () {
     ReactDOM.render(<Header title={'徽商专题'}/>, document.getElementById('header'))
 });
