84065a1b783b0906370874b4ceea3d9d9bc3c751
jiabin.xiong <jiabin.xiong@imduoduo.com>
2016-10-13 11:07:46 +0800

    修改 返回

--- apps/main/open-account/react/index.jsx
+++ apps/main/open-account/react/index.jsx
@@ -991,7 +991,30 @@ $FW.DOMReady(function () {
              } else {
                 ReactDOM.render(<Body activity={data}/>, document.getElementById("cnt"));
              }
-         }
+         },
+         fail: function hander(code, msg, responseText) {
+
+            if (code == 10000 || code == 0) {
+                $FW.Component.Alert('it seems OK');
+            } else if (code == 40101) {
+                if (FinancialWorkspace.Browser.inApp() && NativeBridge) {
+                    NativeBridge.login()
+                } else {
+                    location.href = 'http://m.9888.cn/mpwap/orderuser/toLogin.shtml?redirect_url=' + FinancialWorkspace.getLoginRedirect();
+                }
+            } else if (code == 60000) {
+                $FW.Component.Alert(msg)
+            } else if (code >= 50000 && code < 60000) {
+                // 服务器内部错误
+                $FW.Component.Alert('异常:' + msg)
+            } else {
+                $FW.Component.Alert(msg);
+                console.log(responseText)
+            }
+
+            return true;
+        }
+
      });
 
  });
