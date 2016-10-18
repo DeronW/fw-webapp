c435971b034e8260bf648c02b19cbfd97f7a2bef
delong <w.del@qq.com>
2016-10-18 19:31:54 +0800

    no message

--- apps/main/topic-interest-reward/react/index.jsx
+++ apps/main/topic-interest-reward/react/index.jsx
@@ -2,12 +2,15 @@ const API_PATH = document.getElementById('api-path').value;
 
 $FW.DOMReady(function () {
     if ($FW.Browser.inApp()) {
-        NativeBridge.setTitle('年化加息奖励')
+        NativeBridge.setTitle('VIP等级加息')
     } else {
-        ReactDOM.render(<Header title={'年化加息奖励'}/>, document.getElementById('header'))
+        ReactDOM.render(<Header title={'VIP等级加息'}/>, document.getElementById('header'))
     }
 
     function getCurrentUserInfo(interest_list) {
+        if (interest_list.length == 0) {
+            interest_list = [null, null, {describe: '0.2'}, {describe: '0.4'}, {describe: '0.6'}]
+        }
         $FW.Ajax({
             url: API_PATH + 'mpwap/api/v1/user/level-info.shtml',
             success: (data)=> {
@@ -22,11 +25,17 @@ $FW.DOMReady(function () {
         if (data.code != 10000) throw new Error('接口异常, 无法获取等级反息信息');
 
         var rule = data.data.levelGiftRule, interest_list = [];
-        for (var i = 0; i < rule.length; i++) {
-            var interest = rule[i].addInterest;
-            interest_list.push(rule[i].addInterest);
-            document.getElementById("add-interest-text-" + i).innerHTML = interest ? interest.describe : '无';
+
+        try {
+            for (var i = 0; i < rule.length; i++) {
+                var interest = rule[i].addInterest;
+                interest_list.push(rule[i].addInterest);
+                document.getElementById("add-interest-text-" + i).innerHTML = interest ? interest.describe : '无';
+            }
+        } catch (e) {
+
         }
-        getCurrentUserInfo(interest_list)
+
+        getCurrentUserInfo(interest_list);
     });
 });
