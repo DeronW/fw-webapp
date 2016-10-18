260324cd7463cf7f35fad763619e228cf7ff47d8
delong <w.del@qq.com>
2016-10-18 19:17:36 +0800

    no message

--- apps/main/topic-interest-reward/react/index.jsx
+++ apps/main/topic-interest-reward/react/index.jsx
@@ -7,32 +7,26 @@ $FW.DOMReady(function () {
         ReactDOM.render(<Header title={'年化加息奖励'}/>, document.getElementById('header'))
     }
 
-    $FW.Ajax({
-        url: API_PATH + '/mpwap/api/v1/user/level-info.shtml',
-        success: (data)=> {
-            var level = data.userLevel - 1;
-
-            document.getElementById('level').innerText = level > 1 ? 'VIP' + (level - 1) : '普通会员';
-
-            var interest = '无';
-            if (level == 1) {
-                interest = '0.1'
-            } else if (level == 2) {
-                interest = '0.3'
-            } else if (level == 3) {
-                interest = '0.5'
-            } else if (level == 4) {
-                interest = '0.7'
+    function getCurrentUserInfo(interest_list) {
+        $FW.Ajax({
+            url: API_PATH + 'mpwap/api/v1/user/level-info.shtml',
+            success: (data)=> {
+                var level = data.userLevel - 1;
+                document.getElementById('level').innerText = level > 1 ? 'VIP' + (level - 1) : '普通会员';
+                document.getElementById('interest').innerText = interest_list[level] ? parseFloat(interest_list[level].describe) : '无';
             }
-            document.getElementById('interest').innerText = interest;
-        }
-    })
-});
-
-function gotoInvest() {
-    if ($FW.Browser.inApp()) {
-        NativeBridge.toNative('app_invest_immediately');
-    } else {
-        location.href = '/'
+        });
     }
-}
+
+    $FW.getJSONP('http://www.9888.cn/api/userLevel/v1/giftVO.json', (data)=> {
+        if (data.code != 10000) throw new Error('接口异常, 无法获取等级反息信息');
+
+        var rule = data.data.levelGiftRule, interest_list = [];
+        for (var i = 0; i < rule.length; i++) {
+            var interest = rule[i].addInterest;
+            interest_list.push(rule[i].addInterest);
+            document.getElementById("add-interest-text-" + i).innerHTML = interest ? interest.describe : '无';
+        }
+        getCurrentUserInfo(interest_list)
+    });
+});
