f66a77bd6752ac01d814c893b798e66f54eef38c
sunjiankun <sunjiankun@ucfgroup.com>
2016-10-21 18:02:43 +0800

    1021 1802

--- apps/main/circle-transfer-complete/react/index.jsx
+++ apps/main/circle-transfer-complete/react/index.jsx
@@ -22,18 +22,6 @@ const HeaderTitle = React.createClass({
                             <div className='moneyLeft'> 预期收益 </div>
                             <div className='moneyRight'>￥<span>200.00</span></div>
                         </div>
-                        <div className="money hideMsg">
-                            <div className='moneyLeft'> 使用返现券收益 </div>
-                            <div className='moneyRight'>￥<span>100.00</span></div>
-                        </div>
-                        <div className="money hideMsg">
-                            <div className='moneyLeft'> 使用返息券收益 </div>
-                            <div className='moneyRight'>￥<span>5.00</span> (工豆)</div>
-                        </div>
-                        <div className="money hideMsg">
-                            <div className='moneyLeft'> 会员等级年化加息奖励 </div>
-                            <div className='moneyRight'>￥<span>10.00</span> (工豆)</div>
-                        </div>
                     </div>
         };
 
@@ -71,46 +59,6 @@ const HeaderTitle = React.createClass({
                     }
                 </div>
                 <div className='smallTitle'>
-                    <p>投资奖励</p>
-                </div>
-                <div className='imgMove'>
-                    <ul>
-                        <li>
-                            <div className='imgMoveImg'><img src="./images/move1.png" alt=""/></div>
-                            <div className='popScore'>￥<span>50返现券</span></div>
-                        </li>
-                        <li>
-                            <div className='imgMoveImg'><img src="./images/move2.png" alt=""/></div>
-                            <div className='popScore'><span>5%返息券</span></div>
-                        </li>
-                        <li>
-                            <div className='imgMoveImg'><img src="./images/move3.png" alt=""/></div>
-                            <div className='popScore'><span>2个兑换券</span></div>
-                        </li>
-                        <li>
-                            <div className='imgMoveImg'><img src="./images/move4.png" alt=""/></div>
-                            <div className='popScore'><span>+2000</span>工分</div>
-                        </li>
-                    </ul>
-                </div>
-                <div className='smallTitle reward'>
-                    <p>圈子奖励</p>
-                    <span>
-                        小队数据页面
-                        <img src="./images/arrow.png" alt=""/>
-                    </span>
-                </div>
-                <div className='interest circleReward'>
-                    <div className='money'>
-                        <div className='moneyLeft'> 获得个人任务贡献分</div>
-                        <div className='moneyRight'><span>+3000</span></div>
-                    </div>
-                    <div className='money moneyNoBorder'>
-                        <div className='moneyLeft'> 获得小队奖励工分</div>
-                        <div className='moneyRight'><span>+3000</span></div>
-                    </div>
-                </div>
-                <div className='smallTitle'>
                     <p>会员等级</p>
                 </div>
                 <div className='interest level'>
@@ -121,10 +69,6 @@ const HeaderTitle = React.createClass({
                     <div className='money'>
                         <div className='moneyLeft'> 当前会员等级</div>
                         <div className='moneyRight'><img src="./images/vip.png" alt=""/></div>
-                    </div>
-                    <div className='money'>
-                        <div className='moneyLeft'> 年化加息奖励</div>
-                        <div className='moneyRight'><span className='fontColor'>3.5%</span></div>
                     </div>
                 </div>
                 <div className='smallTitle reward'>
