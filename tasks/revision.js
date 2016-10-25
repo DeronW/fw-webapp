f8cf1269950fcba0f43330f8eeeac50138f80e80
xiangwenlong <xiangwenlong@ucfgroup.com>
2016-10-25 18:14:27 +0800

    no message

--- tasks/revision.js
+++ tasks/revision.js
@@ -1,11 +1,13 @@
 const gulp = require('gulp');
-const RevAll = require('gulp-rev-all');
+const rev_all = require('gulp-rev-all');
 
 
 module.exports = revision = function (src_path, build_path, revAllOptions) {
 
+    var RevAll = new rev_all(revAllOptions);
+
     return gulp.src(src_path)
-        .pipe(RevAll.revision(revAllOptions))
+        .pipe(RevAll.revision())
         .pipe(gulp.dest(build_path))
         .pipe(RevAll.manifestFile())
         .pipe(gulp.dest(build_path))
