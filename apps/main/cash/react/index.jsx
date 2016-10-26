e8a38f957f325858a11a3c8502ca944d3749ec7d
fengxiaoran <fengxiaoran@ucfgroup.com>
2016-10-26 14:40:09 +0800

    提现页面修改完成

--- apps/main/cash/react/index.jsx
+++ apps/main/cash/react/index.jsx
@@ -474,7 +474,7 @@ const Withdrawals = React.createClass({
 				</div>
 
 				<div className="txt-a">
-					<div className="nin">如果您绑定的银行卡暂不支持手机一键支付请联系客服<a href="tel:400-0322-988" className="c-4aa1f9">400-0322-988</a></div>
+					<div className="nin">如果您绑定的银行卡暂不支持手机快捷支付请联系客服<a href="tel:400-0322-988" className="c-4aa1f9">400-0322-988</a></div>
 					<div className="kx">可提现金额(元)：<span style={{fontSize: '38px',color: '#fd4d4c'}}>{this.props.data.accountAmount}</span></div>
 				</div>
 
@@ -542,11 +542,11 @@ const Withdrawals = React.createClass({
 					<div className="hsuo">提现说明</div>
 					<div className="danbi">
 						<div className="atpr"><img className="card-d" src="images/card-d.png"/><span className="online">充值后无投资提现将由第三方平台收取{this.props.data.fee}%手续费。</span></div>
-						<div className="atpr"><img className="card-d" src="images/card-d.png"/><span className="online">{this.props.data.criticalValue} 万及以下提现，7*24小时实时到帐；{this.props.data.criticalValue}万元及以上，工作日{this.props.data.doTime}实时到帐，其余时间及节假日发起提现不予受理；中国银行和南京银行，单笔仅支持5万及以下金额提现。
+						<div className="atpr"><img className="card-d" src="images/card-d.png"/><span className="online">{this.props.data.criticalValue} 万及以下提现，7*24小时实时到帐；{this.props.data.criticalValue}万元以上提现，工作日{this.props.data.doTime}，最快30分钟内到帐，实际到帐时间以发卡行为准，其它时间或节假日发起的提现，不予受理；中国银行和南京银行，单笔仅支持5万及以下金额提现。
 </span></div>
 						<div className="atpr"><img className="card-d" src="images/card-d.png"/><span className="online">单笔提现金额不低于{this.props.data.criticalValue}元。</span></div>
-						<div className="atpr"><img className="card-d" src="images/card-d.png"/><span className="online">{this.props.data.workingDay}个工作日之内到账。</span></div>
-						<div className="atpr"><img className="card-d" src="images/card-d.png"/><span className="online">在双休日和法定节假日期间，也可申请提现。</span></div>
+						{/*<div className="atpr"><img className="card-d" src="images/card-d.png"/><span className="online">{this.props.data.workingDay}个工作日之内到账。</span></div>*/}
+						{/*<div className="atpr"><img className="card-d" src="images/card-d.png"/><span className="online">在双休日和法定节假日期间，也可申请提现。</span></div>*/}
 						<div className="atpr"><img className="card-d" src="images/card-d.png"/><span className="online">填写的提现信息不正确导致提现失败，由此产生的提现费用不予退还。</span></div>
 					</div>
 				</div>
