d5186ba8cfd812c8c43eae1cb80132762e68aef1
彭焱 <pengyan@ucfgroup.com>
2016-11-01 17:52:54 +0800

    修改链接

--- apps/mall/order-list/react/index.jsx
+++ apps/mall/order-list/react/index.jsx
@@ -182,8 +182,8 @@ $FW.DOMReady(function () {
     NativeBridge.setTitle('订单列表');
 
     $FW.Ajax({
-        //url: API_PATH + "mall/api/member/v1/order_list.json",
-        url: "http://localhost/nginx-1.9.12/html/order_list.json",
+        url: API_PATH + "mall/api/member/v1/order_list.json",
+        //url: "http://localhost/nginx-1.9.12/html/order_list.json",
         enable_loading: true,
         success: function (data) {
             console.log(data);
