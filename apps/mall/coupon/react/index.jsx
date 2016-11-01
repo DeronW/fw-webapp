2d489a44a7849c9dc4b8cc6839b1cec95f320ed6
彭焱 <pengyan@ucfgroup.com>
2016-11-01 11:48:46 +0800

    爱奇艺兑换券页面

--- apps/mall/coupon/react/index.jsx
+++ apps/mall/coupon/react/index.jsx
@@ -3,22 +3,30 @@ const API_PATH = document.getElementById('api-path').value;
 
 const Coupon = React.createClass({
     render:function(){
-        return (
-            <div className="coupon">
-                <div className="l-r-text">
-                    <div className="info-block">
-                        <span className="text">券码</span>
-                        <span className="data-text">{data.coupon.cardNum}</span>
-                    </div>
-                    <div className="info-block">
-                        <span className="text">密码</span>
-                        <span className="data-text">{data.coupon.cardPwd}</span>
-                    </div>
-                    <div className="info-block">
-                        <span className="text">有效期</span>
-                        <span className="data-text">{data.coupon.tillDate}</span>
+        let ls = this.props.data.coupon;
+        let coupon = (l,index) => {
+            return (
+                <div className="coupon">
+                    <div className="l-r-text">
+                        <div className="info-block">
+                            <span className="text">券码</span>
+                            <span className="data-text">{ls[index].cardNum}</span>
+                        </div>
+                        <div className="info-block">
+                            <span className="text">密码</span>
+                            <span className="data-text">{ls[index].cardPwd}</span>
+                        </div>
+                        <div className="info-block">
+                            <span className="text">有效期</span>
+                            <span className="data-text">{ls[index].tillDate}</span>
+                        </div>
                     </div>
                 </div>
+            )
+        };
+        return (
+            <div>
+                {ls.map((l, index) => coupon(l, index)) }
             </div>
         )
     }
@@ -29,12 +37,13 @@ $FW.DOMReady(function() {
     NativeBridge.setTitle('爱奇艺兑换券');
     if ($FW.Utils.shouldShowHeader())
         ReactDOM.render(<Header title={"爱奇艺兑换券"} back_handler={back_handler}/>, document.getElementById('header'));
+    var query = $FW.Format.urlQuery();
     $FW.Ajax({
         url: API_PATH + "mall/api/order/v1/viewCardPass.json",
         enable_loading: true,
         data:{
-            bizNo:"56500117085",
-            cardUuid:"12cece4f27b64fa5b907c9b057fa615f"
+            bizNo:query.bizNo,
+            cardUuid:query.cardUuid
         },
         success: function (data) {
             console.log(data);
