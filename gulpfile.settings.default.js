57a578e75ec930c297ff17d38b078cb10e38ab7f
delong <w.del@qq.com>
2016-11-14 15:02:12 +0800

    no message

--- gulpfile.settings.default.js
+++ gulpfile.settings.default.js
@@ -1,9 +1,9 @@
 const CONSTANTS = {
     main: {
-        dev_api_path: 'http://localhost/fake-api/'
+        dev_api_path: 'http://fe.9888.cn/fake-api/'
     },
     mall: {
-        dev_api_path: 'http://localhost/fake-api/'
+        dev_api_path: 'http://fe.9888.cn/fake-api/'
     }
 };
 
