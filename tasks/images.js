a6901a7bd8f3c71c1468b64b2d705423e7dc3d73
delong <w.del@qq.com>
2016-11-21 17:13:19 +0800

    no message

--- tasks/images.js
+++ tasks/images.js
@@ -6,6 +6,6 @@ const changed = require('gulp-changed');
 module.exports = image = function (src_path, build_path) {
     return gulp.src(src_path)
         .pipe(changed(build_path))
-        // .pipe(imagemin()) // 在打包过程中对图片进行的压缩并没有实现太多节省流量的目的, 暂不启用
+        // .pipe(imagemin()) // 图片压缩并没有实现节省流量的目的, 暂不启用
         .pipe(gulp.dest(build_path));
 };
