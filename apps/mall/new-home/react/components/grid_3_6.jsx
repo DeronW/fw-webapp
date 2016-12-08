0815f4b2d76eceda1568b94e07080fb17e1a587d
baoxiao <baoxiao@ucfgroup.com>
2016-12-08 10:08:30 +0800

    10:08

--- apps/mall/new-home/react/components/grid_3_6.jsx
+++ apps/mall/new-home/react/components/grid_3_6.jsx
@@ -19,7 +19,7 @@ const Grid_3_6 = React.createClass({
             return (
                 <a className="theme2-top-product-item" key={index} onClick={ () => gotoHandler('/static/mall/product-detail/index.html?bizNo=' + product.bizNo)}>
                     <div className={"theme2-top-product-title theme2-top-product-title-color"+parseInt(index+1)}>{product.abbreviation}</div>
-                    <div className="theme2-top-product-price">{product.rmbPrice==0.00? null:"&yen;"+product.rmbPrice+"+"}+{product.score}工分</div>
+                    <div className="theme2-top-product-price">{product.rmbPrice==0.00? null:"&yen;"+product.rmbPrice+"+"}{product.score}工分</div>
                     <img className="product-img2" src={product.img}/>
                 </a>
             )
@@ -32,7 +32,7 @@ const Grid_3_6 = React.createClass({
                         <img className ="theme2-product-img" src={product.img}/>
                         <div className="theme2-btm-product-info">
                             <span className="theme2-btm-product-title">{product.abbreviation}</span>
-                            <span className="theme2-btm-product-price">{product.rmbPrice==0.00? null:"&yen;"+product.rmbPrice+"+"}{product.score}工分</span>
+                            <span className="theme2-btm-product-price">{product.rmbPrice==0.00? null:"¥"+product.rmbPrice+"+"}{product.score}工分</span>
                         </div>
                     </div>
                 </a>
