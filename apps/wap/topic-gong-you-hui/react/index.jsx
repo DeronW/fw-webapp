39577c1fbff99fec8f3a6b8cb2fbc400859bbdd6
sunjiankun <sunjiankun@ucfgroup.com>
2016-12-09 15:17:50 +0800

    no message

--- apps/wap/topic-gong-you-hui/react/index.jsx
+++ apps/wap/topic-gong-you-hui/react/index.jsx
@@ -26,6 +26,16 @@ const WorkerBox = React.createClass({
             this.setState({eastImg: data});
         }.bind(this))
     },
+    gotoHandle: function (link, need_login) {
+        if (link.indexOf('://') < 0) {
+            link = location.protocol + '//' + location.hostname + link;
+        }
+        if (navigator.userAgent.indexOf('FinancialWorkshop') >= 0) {
+            NativeBridge.goto(link, need_login)
+        } else {
+            location.href = link;
+        }
+    },
     render: function () {
         var introduction = [
             {
@@ -91,7 +101,7 @@ const WorkerBox = React.createClass({
                             })
                         }
                     </div>
-                    <a href="http://bbs.9888.cn/forum.php?mod=viewthread&tid=5615" className="moreBtn">了解更多现场情况</a>
+                    <a onClick={() => {this.gotoHandle("http://bbs.9888.cn/forum.php?mod=viewthread&tid=5615",true)}} className="moreBtn">了解更多现场情况</a>
                 </div>
                 <div className="northEast">
                     <div className="northEastTitle">大连站、哈尔滨站——走进东北</div>
@@ -103,7 +113,7 @@ const WorkerBox = React.createClass({
                             })
                         }
                     </div>
-                    <a href="http://bbs.9888.cn/forum.php?mod=viewthread&tid=5615" className="moreBtn">了解更多现场情况</a>
+                    <a onClick={() => {this.gotoHandle("http://bbs.9888.cn/forum.php?mod=viewthread&tid=5615",true)}} className="moreBtn">了解更多现场情况</a>
 
                     <div className="northTitle">12月10日，工友汇”第三站走进童话的冰雪之城——哈尔滨。</div>
                     <div className="northImg">
@@ -113,7 +123,7 @@ const WorkerBox = React.createClass({
                             })
                         }
                     </div>
-                    <a href="http://bbs.9888.cn/forum.php?mod=viewthread&tid=5615" className="moreBtn">了解更多现场情况</a>
+                    <a onClick={() => {this.gotoHandle("http://bbs.9888.cn/forum.php?mod=viewthread&tid=5615",true)}} className="moreBtn">了解更多现场情况</a>
                 </div>
                 <div className="aboutMe">
                     <div className="aboutTitle">关于我们</div>
