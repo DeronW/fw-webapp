f9adf09ed94581d1b7346dfaaa7f560bb966e2d9
xiangwenlong <xiangwenlong@ucfgroup.com>
2016-12-14 10:57:06 +0800

    2016-12-20app上线，更改首页app返回金融工场

--- apps/mall/home/react/index.jsx
+++ apps/mall/home/react/index.jsx
@@ -53,13 +53,17 @@ const Mall = React.createClass({
         }
         link ? gotoHandler(link) : console.log('no link set');
     },
+    backNativeApp:function(){
+        NativeBridge.close();
+    },
     render: function () {
         let activity = (i, index) => {
             return <ActivityProduct title={i.title} img={i.img} bizNo={i.bizNo}
                                     activity_id={i.activity_id} products={i.products} key={index}/>;
         };
         let backFactory = ()=> {
-            return $FW.Browser.inApp() ? <img className="m-logo" src="images/m-logo.png"/> :
+            return $FW.Browser.inApp() ? <a className="back-factory" onClick={this.backNativeApp}><img
+                src="images/wap_shop_gong_logo.png"/></a> :
                 <a className="back-factory" href="http://m.9888.cn/mpwap/"><img
                     src="images/wap_shop_gong_logo.png"/></a>
         };
