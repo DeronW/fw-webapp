a912bab7d486450f318bcb8bbef25700d1c990aa
xiangwenlong <xiangwenlong@ucfgroup.com>
2016-12-15 11:20:44 +0800

    no message

--- apps/mall/new-game/react/index.jsx
+++ apps/mall/new-game/react/index.jsx
@@ -93,6 +93,22 @@ const GameCenter = React.createClass({
         return images;
     },
     render: function(){
+        let head=this.state.isLogin?<div className="game-head-login">
+            <div className="avatar"><img src={this.state.avatar} onClick={()=>{this.popNickName(true)}}/></div>
+            <div className="name-box">
+                <div className="nickname-title">游戏昵称</div>
+                <div className="nickname">{this.state.nameNick}</div>
+            </div>
+            <div className="login-btn">设置昵称</div>
+        </div>:
+            <div className="game-head-unlogin">
+                <div className="avatar"><img src="images/头像00.png" /></div>
+                <div className="name-box">
+                    <div className="nickname-title">游戏昵称</div>
+                    <div className="nickname">未登录</div>
+                </div>
+                <div className="unlogin-btn">登录设置昵称</div>
+            </div>
         return(
             <div className="game-center">
                 {head}
