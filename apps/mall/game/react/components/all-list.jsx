128a0618794d85d064e334289345f340c36f540b
xiangwenlong <xiangwenlong@ucfgroup.com>
2016-12-15 19:28:07 +0800

    no message

--- apps/mall/game/react/components/all-list.jsx
+++ apps/mall/game/react/components/all-list.jsx
@@ -1,10 +1,11 @@
 const GameCenter_AllGame=React.createClass({
     render:function(){
         let allList=(list,index)=>{
+            let link=(list.game_url==''||list.game_url=='#')?null:href=list.game_url
             return(
                 <div className="all-game-li" key={index}>
                     <div className="game-img"><img src={list.logo}/></div>
-                    <a className="game-up-img" href={list.game_url}></a>
+                    <a className="game-up-img" {link}></a>
                     <div className="game-name">{list.game_name}</div>
                     <div className="game-desc">{list.tag}</div>
                 </div>
--- apps/mall/game/react/components/new-list.jsx
+++ apps/mall/game/react/components/new-list.jsx
@@ -1,9 +1,11 @@
 const GameCenter_NewGame = React.createClass({
     render:function(){
+
         let newList=(list,index)=>{
+            let link=(list.game_url==''||list.game_url=='#')?null:href=list.game_url
             return(
                 <div className="new-game-li" key={index}>
-                    <a href={list.game_url} className="new-game-a"><img src={list.icon}/></a>
+                    <a {link} className="new-game-a"><img src={list.icon}/></a>
                     <div className="new-game-text">{list.game_name}</div>
                 </div>
             )
