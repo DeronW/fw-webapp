212bb0cc98da2e48118e35376053e21da4e1e841
xiangwenlong <xiangwenlong@ucfgroup.com>
2016-12-20 10:27:29 +0800

    足迹完成等接口，进行联调

--- apps/mall/footprint/react/index.jsx
+++ apps/mall/footprint/react/index.jsx
@@ -1,60 +1,105 @@
 const FootPrint = React.createClass({
     getInitialState: function () {
-
         return {
             list: [],
             hasNextPage:true,
             page:0
-
         }
     },
     componentDidMount: function () {
+        this.loadMoreProductHandler();
         $FW.Event.touchBottom(this.loadMoreProductHandler);
     },
     loadMoreProductHandler: function (done) {
-        this.state.hasData ?
-            Filter.search({page: this.state.page + 1}, (data)=> {
-                this.appendProducts(data);
+        // let data={"list":[{"date":"1月3日","score":0,"bizNo":"B0000002884","img":"http://mmall.9888.cn/images/20161008/B0000002884_title1475916515730_M.jpg","price":23213,"title":"jd积分大姐夫哈市第","id":"sadasdd"},
+        //     {"date":"1月3日","score":1,"bizNo":"B0000002884","img":"http://mmall.9888.cn/images/20161008/B0000002884_title1475916515730_M.jpg","price":23213,"title":"jd积分大姐夫哈市第","id":"sadasdd"},
+        //     {"date":"1月4日","score":2,"bizNo":"B0000002884","img":"http://mmall.9888.cn/images/20161008/B0000002884_title1475916515730_M.jpg","price":23213,"title":"jd积分大姐夫哈市第","id":"sadasdd"},
+        //     {"date":"1月3日","score":3,"bizNo":"B0000002884","img":"http://mmall.9888.cn/images/20161008/B0000002884_title1475916515730_M.jpg","price":0,"title":"jd积分大姐夫哈市第","id":"sadasdd"},{"date":"1月3日","score":0,"bizNo":"B0000002884","img":"http://mmall.9888.cn/images/20161008/B0000002884_title1475916515730_M.jpg","price":23213,"title":"jd积分大姐夫哈市第","id":"sadasdd"},
+        //     {"date":"1月3日","score":1,"bizNo":"B0000002884","img":"http://mmall.9888.cn/images/20161008/B0000002884_title1475916515730_M.jpg","price":23213,"title":"jd积分大姐夫哈市第","id":"sadasdd"},
+        //     {"date":"1月4日","score":2,"bizNo":"B0000002884","img":"http://mmall.9888.cn/images/20161008/B0000002884_title1475916515730_M.jpg","price":23213,"title":"jd积分大姐夫哈市第","id":"sadasdd"},
+        //     {"date":"1月3日","score":3,"bizNo":"B0000002884","img":"http://mmall.9888.cn/images/20161008/B0000002884_title1475916515730_M.jpg","price":0,"title":"jd积分大姐夫哈市第","id":"sadasdd"},{"date":"1月3日","score":0,"bizNo":"B0000002884","img":"http://mmall.9888.cn/images/20161008/B0000002884_title1475916515730_M.jpg","price":23213,"title":"jd积分大姐夫哈市第","id":"sadasdd"},
+        //     {"date":"1月3日","score":1,"bizNo":"B0000002884","img":"http://mmall.9888.cn/images/20161008/B0000002884_title1475916515730_M.jpg","price":23213,"title":"jd积分大姐夫哈市第","id":"sadasdd"},
+        //     {"date":"1月4日","score":2,"bizNo":"B0000002884","img":"http://mmall.9888.cn/images/20161008/B0000002884_title1475916515730_M.jpg","price":23213,"title":"jd积分大姐夫哈市第","id":"sadasdd"},
+        //     {"date":"1月3日","score":3,"bizNo":"B0000002884","img":"http://mmall.9888.cn/images/20161008/B0000002884_title1475916515730_M.jpg","price":0,"title":"jd积分大姐夫哈市第","id":"sadasdd"}],"hasNextPage":false};
+
+        this.state.hasNextPage?
+            AjaxList(this.state.page + 1, (data)=> {
+               this.appendProducts(data);
                 this.setState({
                     page: this.state.page + 1,
-                    hasData: data.hasData
+                    hasNextPage: data.hasNextPage
                 });
                 done && done()
             }) : null;
     },
     appendProducts: function (data) {
-        var list = this.state.products.slice();
-        var newList = list.concat(data.products || []);
-        this.setState({products: newList})
+        var list = this.state.list.slice();
+        var newList = list.concat(data.list || []);
+        this.setState({list: newList})
     },
-
+    clearAllHandler:function(){
+        $FW.Ajax({
+            url: `${API_PATH}mall/api/v1/footPrintDel.json`,//足迹删除
+            data:{
+                type:'all',
+                id:''
+            },
+            method:'post',
+            success: function(){
+                this.setState({list:[]});
+            }
+        });
+    },
+    delHandler:function(id,key){
+        $FW.Ajax({
+            url: `${API_PATH}mall/api/v1/footPrintDel.json`,//足迹删除
+            data:{
+                type:'one',
+                id:id
+            },
+            method:'post',
+            success: function(){
+                let list=this.state.list
+                list.splice(key,1)
+                this.setState({list:list});
+            }
+        });
+    },
     render: function(){
-
-        return (
-           <div className="foot-print">
-                <div className="footPrint-clear">清空</div>
-                <div className="footPrint-ul">
-                   <div className="footPrint-data">11月18日</div>
-                   <div className="footPrint-li">
-                       <a href="#" className="footPrint-a">
-                           <img src="" className="footPrint-img"/>
-                           <div className="footPrint-detail">
-                               <div className="footPrint-title">Apple / 苹果   iPad Air2 128G WIFI 64g 金色</div>
-                               <div className="footPrint-pay"><span className="footPrint-price"><span>¥</span>2,199</span><span className="footPrint-plus">+</span><span className="footPrint-score">22233320工分</span></div>
-                           </div>
-                       </a>
-                       <div className="footPrint-del"></div>
-                   </div>
+        let myList=(list,key)=>{
+            let date=key>0&&(this.state.list[key-1].date==list.date)?null:<div className="footPrint-data">{this.state.list[key].date}</div>;
+            let price=list.price?<span className="footPrint-price"><span>¥</span>{$FW.Format.currency(list.price)}</span>:null;
+            let score=list.score?<span className="footPrint-score">{list.score}工分</span>:null;
+            let priceBox=()=>{
+                return(
+                    <div className="footPrint-pay" >
+                        {price}
+                        {list.price&&list.score?<span className="footPrint-plus">+</span>:null}
+                        {score}
+                    </div>
+                )
+            };
+            return(
+                <div key={key}>
+                    {date}
                     <div className="footPrint-li">
                         <a href="#" className="footPrint-a">
-                            <img src="" className="footPrint-img"/>
+                            <img src={list.img} className="footPrint-img"/>
                             <div className="footPrint-detail">
-                                <div className="footPrint-title">Apple / 苹果   iPad Air2 128G WIFI 64g 金色</div>
-                                <div className="footPrint-pay"><span className="footPrint-price"><span>¥</span>2,199</span><span className="footPrint-plus">+</span><span className="footPrint-score">22233320工分</span></div>
+                                <div className="footPrint-title">{list.title}</div>
+                                    {priceBox()}
                             </div>
                         </a>
-                        <div className="footPrint-del"></div>
+                        <div className="footPrint-del" onClick={()=>{this.delHandler(list.id,key)}}></div>
                     </div>
+                </div>
+                )
+        };
+        return (
+           <div className="foot-print">
+                <div className="footPrint-clear" style={{"zIndex":10}} onClick={this.clearAllHandler}>清空</div>
+                <div className="footPrint-ul">
+                    {this.state.list.map(myList)}
                </div>
            </div>
       )
@@ -65,15 +110,20 @@ $FW.DOMReady(function(){
     if ($FW.Utils.shouldShowHeader()){
         ReactDOM.render(<Header title={"足迹"}/>, document.getElementById('header'));
     }
+    ReactDOM.render(<FootPrint />, document.getElementById('cnt'));
 
-    $FW.Ajax({
-        url: `${API_PATH}mall/api/index/v1/activities.json`,//热门活动
-        success: (data) => {
-            ReactDOM.render(<FootPrint data={data} />, document.getElementById('cnt'));
-        }
-    });
 });
-
+function AjaxList(page,callback){
+    $FW.Ajax({
+        url: `${API_PATH}mall/api/v1/footPrintList.json`,//足迹
+        data:{
+            num:10,
+            pageNo:page
+        },
+        method:'post',
+        success: data => callback(data)
+    });
+}
 function backward(){
     $FW.Browser.inApp() ? NativeBridge.close() : location.href = '/'
 }
