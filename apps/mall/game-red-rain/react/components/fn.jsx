2b130ee88beb0d1fe5d2fbf6a2c8310f920166e2
xiangwenlong <xiangwenlong@ucfgroup.com>
2017-01-03 16:47:15 +0800

    1

--- apps/mall/game-red-rain/react/components/fn.jsx
+++ apps/mall/game-red-rain/react/components/fn.jsx
@@ -3,15 +3,15 @@
  */
 let redRain = {
     totalRed: 10,
-    maxTime: 1000,
-    minTime: 500,
+    maxTime: 3500,
+    minTime: 2000,
     idNum: 0,
     getRed: 0,
     nowNum: 0,
     redImg: ["redBag0.png", "redBag1.png", "redBag2.png", "redBag3.png", "redBag4.png", "redBag5.png"],
-    time:500,
-    x:720,
-    y:700,
+    time: 500,
+    x: 720,
+    y: 700,
 };
 //倒计时准备开始
 let fnReady = (callback)=> {
@@ -24,57 +24,61 @@ let fnReady = (callback)=> {
             callback && callback();
         }
     }, 1000)
-}
+};
 //单个红包
-let Redbag = ()=> {};
+let Redbag = ()=> {
+};
 Redbag.prototype = {
-    fnCreateNode: ()=> {
-            redRain.idNum = redRain.idNum + 1;
-            var box = document.getElementById("red-rain");
-            var newNode = document.createElement("div");
-            newNode.className="bag-li";
-            newNode.id='li' + redRain.idNum;
-            newNode.style.left=Math.random() *redRain.x+'px';
-            newNode.style.top=Math.random() *redRain.y+'px';
-            newNode.innerHTML = '<div class="bag-text">+1</div><div class="bag-img"><img src="images/' + redRain.redImg[parseInt(redRain.redImg.length * Math.random())] + '"></div>';
-            box.appendChild(newNode);
-        return document.getElementById('li' + redRain.idNum);
-    },
-    fnAnimation: (node,callback)=> {
-        console.log(node);
-        var step = 0.1;
-        node.setAttribute("scale",0);
-        node.timer = setInterval(()=> {
-            node.setAttribute("scale",node.getAttribute("scale")+step);
-            if (node.getAttribute("scale") >= 1) {
-                node.setAttribute("scale",1);
-                step = -step;
-                requestAnimationFrame(()=> {
-                    node.style.Transform = scale(node.getAttribute("scale") + step);
-                });
-            } else if (node.getAttribute("scale") <= 0) {
-                node.setAttribute("scale",0);
-                clearInterval(node.timer);
-                callback && callback();
-                node.fnRemoveNode();
-            } else {
-                requestAnimationFrame(()=> {
-                    node.style.Transform = scale(node.getAttribute("scale") + step);
-                });
-            }
-        }), parseInt(redRain.minTime + (redRain.maxTime - redRain.minTime).random())
-    },
-    fnRemoveNode: (time)=> {
+    fnRemoveNode: (node, time)=> {
         var time = time || 0;
-        if (this != null) {
+        clearInterval(node.timer);
+        console.log(node.timer);
+        if (node) {
             setTimeout(()=> {
-                this.parentNode.removeChild(this);
-                if(redRain.nowNum>0){
+                node.parentNode.removeChild(node);
+                if (redRain.nowNum > 0) {
                     redRain.nowNum--;
                 }
             }, time)
         }
     },
+    fnCreateNode: ()=> {
+        redRain.idNum = redRain.idNum + 1;
+        var box = document.getElementById("red-rain");
+        var newNode = document.createElement("div");
+        newNode.className = "bag-li";
+        newNode.id = 'li' + redRain.idNum;
+        newNode.style.left = Math.random() * redRain.x + 'px';
+        newNode.style.top = Math.random() * redRain.y + 'px';
+        newNode.innerHTML = '<div class="bag-text">+1</div><div class="bag-img"><img src="images/' + redRain.redImg[parseInt(redRain.redImg.length * Math.random())] + '"></div>';
+        box.appendChild(newNode);
+        return document.getElementById('li' + redRain.idNum);
+    },
+    fnAnimation: (node, callback)=> {
+        var step = 0.1;
+        node.setAttribute("scale", 0);
+        node.timer = setInterval(()=> {
+            node.setAttribute("scale", (parseFloat(node.getAttribute("scale")) + step));
+            if (node.getAttribute("scale") >= 1) {
+                node.setAttribute("scale", 1);
+                step = -step;
+                requestAnimationFrame(()=> {
+                    node.style.transform = node.style.webkitTransform = "scale(1)";
+                });
+            } else if (node.getAttribute("scale") <= 0) {
+                node.setAttribute("scale", 0);
+                requestAnimationFrame(()=> {
+                    node.style.transform = node.style.webkitTransform = "scale(0)";
+                });
+                callback && callback();
+            } else {
+                requestAnimationFrame(()=> {
+                    node.style.transform = node.style.webkitTransform = "scale(" + (parseFloat(node.getAttribute("scale")) + step) + ")";
+                });
+            }
+        }), parseInt(redRain.minTime + (redRain.maxTime - redRain.minTime) * Math.random())
+    },
+
     fnClickElement: ()=> {
         if (!this.checked) {
             redRain.getRed++;
--- apps/mall/game-red-rain/react/index.jsx
+++ apps/mall/game-red-rain/react/index.jsx
@@ -3,13 +3,15 @@ $FW.DOMReady(function () {
     //倒计时准备开始
     fnReady();
     var box = document.getElementById("red-rain");
-    console.log(window.innerHeight);
     box.style.height=window.innerHeight+'px';
     let fnRedBag=setInterval(()=>{
+        console.log()
         if (redRain.nowNum < redRain.totalRed) {
+            redRain.nowNum++;
             let newBag=new Redbag();
             let node=newBag.fnCreateNode();
-            newBag.fnAnimation(node);
+            newBag.fnAnimation(node,newBag.fnRemoveNode(node));
+
         }
     },redRain.time);
 
