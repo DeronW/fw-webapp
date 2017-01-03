eecd4427a2049649dc9baa3552a4061791900297
xiangwenlong <xiangwenlong@ucfgroup.com>
2017-01-03 14:25:52 +0800

    no message

--- apps/mall/game-red-rain/react/index.jsx
+++ apps/mall/game-red-rain/react/index.jsx
@@ -2,12 +2,14 @@ $FW.DOMReady(function () {
     $FW.Event.cancelSlideDownRefresh();
     //倒计时准备开始
     fnReady();
+    var box = document.getElementById("red-rain");
+    console.log(window.innerHeight);
+    box.style.height=window.innerHeight+'px';
     let fnRedBag=setInterval(()=>{
         if (redRain.nowNum < redRain.totalRed) {
             let newBag=new Redbag();
             let node=newBag.fnCreateNode();
-            console.log(node);
-            node.fnAnimation();
+            newBag.fnAnimation(node);
         }
     },redRain.time);
 
