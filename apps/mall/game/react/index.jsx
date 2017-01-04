3f7726fc2d10f98d5f716963dca830a38bc4b341
delong <w.del@qq.com>
2017-01-04 09:48:19 +0800

    enable game center for new iOS version app

--- apps/mall/game/react/index.jsx
+++ apps/mall/game/react/index.jsx
@@ -2,17 +2,17 @@ const GameCenter = React.createClass({
     getInitialState: function () {
         return {
             allList: [],
-            newList:[],
-            bannerList:[],
-            avatarList:[],
-            isLogin:this.props.data.login_status==1?true:false,
-            popNameNick:false,
-            nameValue:'',
-            popAvatar:false,
-            popError:false,
-            avatar:this.props.data.login_status==1&&this.props.data.list.user_icon?this.props.data.list.user_icon:'images/default-avatar.png',
-            nameNick:this.props.data.login_status==1?this.props.data.list.nickname:'',
-            errorMessage:''
+            newList: [],
+            bannerList: [],
+            avatarList: [],
+            isLogin: this.props.data.login_status == 1 ? true : false,
+            popNameNick: false,
+            nameValue: '',
+            popAvatar: false,
+            popError: false,
+            avatar: this.props.data.login_status == 1 && this.props.data.list.user_icon ? this.props.data.list.user_icon : 'images/default-avatar.png',
+            nameNick: this.props.data.login_status == 1 ? this.props.data.list.nickname : '',
+            errorMessage: ''
         }
     },
     componentDidMount: function () {
@@ -20,50 +20,50 @@ const GameCenter = React.createClass({
             url: `${location.protocol}//game.9888.cn/index.php?r=polymerization/gamelist&fr=shop`,//游戏中心所有列表
             withCredentials: true,
             success: (data) => {
-                this.setState({allList:data.list});
+                this.setState({ allList: data.list });
             }
         });
         $FW.Ajax({
             url: `${location.protocol}//game.9888.cn/index.php?r=polymerization/newst`,//游戏中心最新游戏
             withCredentials: true,
             success: (data) => {
-                this.setState({newList:data.list});
+                this.setState({ newList: data.list });
             }
         });
         $FW.Ajax({
             url: `${location.protocol}//game.9888.cn/index.php?r=polymerization/gamebanner&fr=shop&tag=tag1`,//banner
             withCredentials: true,
             success: (data) => {
-                this.setState({bannerList:data.list});
+                this.setState({ bannerList: data.list });
             }
         });
     },
-    clickAvatarHandler:function(){
+    clickAvatarHandler: function () {
         $FW.Ajax({
             url: `${location.protocol}//game.9888.cn/index.php?r=polymerization/uicons`,//获取头像列表
             withCredentials: true,
             success: (data) => {
                 this.setState({
-                    popAvatar:true,
-                    avatarList:data
+                    popAvatar: true,
+                    avatarList: data
                 });
             }
         });
     },
-    popNickName:function(a){
+    popNickName: function (a) {
         this.setState({
-            popNameNick:a
+            popNameNick: a
         });
     },
 
-    setAvatar:function(state){
+    setAvatar: function (state) {
         this.setState({
-            avatar:state
+            avatar: state
         });
     },
-    setPopAvatar:function(state){
+    setPopAvatar: function (state) {
         this.setState({
-            popAvatar:state
+            popAvatar: state
         });
     },
     onImageClickHandler: function (index) {
@@ -72,21 +72,21 @@ const GameCenter = React.createClass({
         for (var i = 0; i < bs.length; i++) {
             if (i == index) link = bs[i].link;
         }
-        link ?location.href =link: console.log('no link set');
+        link ? location.href = link : console.log('no link set');
     },
-    popError:function(a){
+    popError: function (a) {
         this.setState({
-            popError:a
+            popError: a
         });
     },
-    setNameNick:function(a){
-    this.setState({
-        nameNick:a
-    });
-},
-    setErrorMessage:function(msg){
+    setNameNick: function (a) {
         this.setState({
-            errorMessage:msg
+            nameNick: a
+        });
+    },
+    setErrorMessage: function (msg) {
+        this.setState({
+            errorMessage: msg
         });
     },
     getHeadImages: function () {
@@ -97,15 +97,15 @@ const GameCenter = React.createClass({
         }
         return images;
     },
-    render: function(){
-        let head=this.state.isLogin?<div className="game-head-login">
-            <div className="avatar" onClick={this.clickAvatarHandler}><img src={this.state.avatar}/></div>
+    render: function () {
+        let head = this.state.isLogin ? <div className="game-head-login">
+            <div className="avatar" onClick={this.clickAvatarHandler}><img src={this.state.avatar} /></div>
             <div className="name-box">
                 <div className="nickname-title">游戏昵称</div>
                 <div className="nickname">{this.state.nameNick}</div>
             </div>
-            <div className="login-btn" onClick={()=>{this.popNickName(true)}}>修改昵称</div>
-        </div>:
+            <div className="login-btn" onClick={() => { this.popNickName(true) } }>修改昵称</div>
+        </div> :
             <div className="game-head-unlogin">
                 <div className="avatar"><img src="images/default-avatar.png" /></div>
                 <div className="name-box">
@@ -114,47 +114,47 @@ const GameCenter = React.createClass({
                 </div>
                 <div className="unlogin-btn" onClick={$FW.Utils.loginMall}>登录设置昵称</div>
             </div>
-        return(
+        return (
             <div className="game-center">
                 {head}
-                {this.state.bannerList.length>0?<BannerGroup className="game-banner"
-                                                       images={this.getHeadImages()}
-                                                       onImageClick={this.onImageClickHandler}/>:null}
-                {this.state.newList.length>0?<GameCenter_NewGame data={this.state.newList}/>:null}
-                {this.state.allList.length>0?<GameCenter_AllGame data={this.state.allList}/>:null}
-                {this.state.popError?<GameCenter_popNickNameError data={this.state.errorMessage} popNickName={this.popNickName} popError={this.popError}/>:null}
-                {this.state.popNameNick?<GameCenter_popNickname setNameNick={this.setNameNick} popNickName={this.popNickName} popError={this.popError} popErrorMessage={this.setErrorMessage} />:null}
-                {this.state.popAvatar?<GameCenter_popAvatar setAvatar={this.setAvatar} setPopAvatar={this.setPopAvatar} data={this.state.avatarList}/>:null}
+                {this.state.bannerList.length > 0 ? <BannerGroup className="game-banner"
+                    images={this.getHeadImages()}
+                    onImageClick={this.onImageClickHandler} /> : null}
+                {this.state.newList.length > 0 ? <GameCenter_NewGame data={this.state.newList} /> : null}
+                {this.state.allList.length > 0 ? <GameCenter_AllGame data={this.state.allList} /> : null}
+                {this.state.popError ? <GameCenter_popNickNameError data={this.state.errorMessage} popNickName={this.popNickName} popError={this.popError} /> : null}
+                {this.state.popNameNick ? <GameCenter_popNickname setNameNick={this.setNameNick} popNickName={this.popNickName} popError={this.popError} popErrorMessage={this.setErrorMessage} /> : null}
+                {this.state.popAvatar ? <GameCenter_popAvatar setAvatar={this.setAvatar} setPopAvatar={this.setPopAvatar} data={this.state.avatarList} /> : null}
             </div>
         );
     }
 });
-var gameToken='';
-$FW.DOMReady(function(){
-    if($FW.Browser.inIOS()&&$FW.Browser.appVersion()>="2.3.40"){
-        location.href="/static/mall/waiting/index.html";
+var gameToken = '';
+$FW.DOMReady(function () {
+    if ($FW.Browser.inIOS() && $FW.Browser.appVersion() >= "2.3.50") {
+        location.href = "/static/mall/waiting/index.html";
     }
     NativeBridge.setTitle('游戏中心');
     var ua = window.navigator.userAgent.toLowerCase();
-    var wxBrower=ua.match(/MicroMessenger/i) == 'micromessenger'?true:false;
-    if($FW.Format.urlQuery().mallHead=="true"&&!wxBrower){
+    var wxBrower = ua.match(/MicroMessenger/i) == 'micromessenger' ? true : false;
+    if ($FW.Format.urlQuery().mallHead == "true" && !wxBrower) {
         if ($FW.Utils.shouldShowHeader())
-            ReactDOM.render(<Header title={"游戏中心"} back_handler={backward}/>, document.getElementById
-            ('header'));
+            ReactDOM.render(<Header title={"游戏中心"} back_handler={backward} />, document.getElementById
+                ('header'));
     }
     $FW.Ajax({
         url: `${location.protocol}//game.9888.cn/index.php?r=polymerization/getuinfo`,//判断登录状态
         withCredentials: true,
         success: (data) => {
-            if(data.login_status ==1){
-                gameToken=data.token;
+            if (data.login_status == 1) {
+                gameToken = data.token;
             }
             ReactDOM.render(<GameCenter data={data} />, document.getElementById('cnt'));
         }
     });
 });
 
-function backward(){
+function backward() {
     $FW.Browser.inApp() ? NativeBridge.close() : location.href = '/static/mall/home/index.html'
 }
 function str_length(str) {
