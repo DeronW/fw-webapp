64434ad9a03ecdd4c81f0528f6c1470eddc6c663
baoxiao <baoxiao@ucfgroup.com>
2017-01-04 15:55:07 +0800

    15:55

--- apps/mall/shopping-cart/react/index.jsx
+++ apps/mall/shopping-cart/react/index.jsx
@@ -1,8 +1,8 @@
-function getNextElement(node){
-    if(node.nextSibling.nodeType == 1){    //判断下一个节点类型为1则是“元素”节点
+function getNextElement(node) {
+    if (node.nextSibling.nodeType == 1) {    //判断下一个节点类型为1则是“元素”节点
         return node.nextSibling;
     }
-    if(node.nextSibling.nodeType == 3){      //判断下一个节点类型为3则是“文本”节点  ，回调自身函数
+    if (node.nextSibling.nodeType == 3) {      //判断下一个节点类型为3则是“文本”节点  ，回调自身函数
         return getNextElement(node.nextSibling);
     }
     return null;
@@ -28,7 +28,7 @@ const ShoppingCart = React.createClass({
             //changeAll:true
         }
     },
-    componentDidMount:function(){
+    componentDidMount: function () {
         //var ps = this.state.products;
         //for(var i=0;i<ps.length;i++){
         //    if(!ps[i].isChecked){
@@ -36,25 +36,27 @@ const ShoppingCart = React.createClass({
         //    }
         //}
     },
-    payHandler:function(){
-        let prds=[];
-        document.querySelectorAll('.checked-circle').map((checkDom, index) => {
+    payHandler: function () {
+        let prds = [];
+        let _checkDom = document.querySelectorAll('.checked-circle');
+        _checkDom = Array.prototype.slice.call(_checkDom);
+        _checkDom.map((checkDom, index) => {
             prds.push(getNextElement(checkDom).value);
         });
-        gotoHandler("/static/mall/order-confirm/index.html?cartFlag=true&prds="+prds)
+        gotoHandler("/static/mall/order-confirm/index.html?cartFlag=true&prds=" + prds)
 
     },
     checkHandler: function (index) {
         var ps = this.state.products;
         var _this = this;
         $FW.Ajax({
-            url:`${API_PATH}mall/api/cart/v1/isChecked.json`,
-            data:{
-                flag:ps[index].isChecked = !ps[index].isChecked,
-                productBizNo:ps[index].productBizno
+            url: `${API_PATH}mall/api/cart/v1/isChecked.json`,
+            data: {
+                flag: ps[index].isChecked = !ps[index].isChecked,
+                productBizNo: ps[index].productBizno
             },
-            success:function(data){
-                 _this.setState({products: ps});
+            success: function (data) {
+                _this.setState({products: ps});
                 //for(var i=0;i<ps.length;i++){
                 //    if(!ps[i].checked){
                 //        _this.setState({changeAll:false});
@@ -63,25 +65,25 @@ const ShoppingCart = React.createClass({
                 //    }
                 //}
             }
-       });
+        });
 
     },
     deleteHandler: function (index) {
         let ps = this.state.products;
         var _this = this;
         $FW.Ajax({
-                url:`${API_PATH}mall/api/cart/v1/deleteCartProduct.json`,
-            data:{
-                productId:ps[index].productId
+            url: `${API_PATH}mall/api/cart/v1/deleteCartProduct.json`,
+            data: {
+                productId: ps[index].productId
             },
             enable_loading: true,
-            success:function(data){
+            success: function (data) {
                 _this.setState({products: ps});
             }
         });
-        ps.splice(index,1);
+        ps.splice(index, 1);
     },
-    allChoseHandler:function(){
+    allChoseHandler: function () {
         //let products=this.state.products;
         //let newChangeAll=!this.state.changeAll;
         //for(var i= 0;i<this.state.products.length;i++){
@@ -92,58 +94,61 @@ const ShoppingCart = React.createClass({
         //    changeAll:newChangeAll
         //});
         $FW.Ajax({
-            url:`${API_PATH}mall/api/cart/v1/isChecked.json`,
-            data:{
-                flag:ps[index].isChecked = !ps[index].isChecked,
-                productBizNo:ps[index].productBizno
+            url: `${API_PATH}mall/api/cart/v1/isChecked.json`,
+            data: {
+                flag: ps[index].isChecked = !ps[index].isChecked,
+                productBizNo: ps[index].productBizno
             },
-            success:()=>{
+            success: ()=> {
                 this.setState({products: ps});
             }
         });
     },
-    updateCount: function (index,newAmount) {
+    updateCount: function (index, newAmount) {
         var ps = this.state.products;
         var _this = this;
         var c = newAmount;
         c = parseInt(c) || 1;
         if (c < 1) c = 1;
         if (c > ps[index].productStock) c = ps[index].productStock;
-        ps[index].productNumber=c;
+        ps[index].productNumber = c;
         $FW.Ajax({
-            url:`${API_PATH}mall/api/cart/v1/updateCartNumber.json`,
-            data:{
-                buyNum:ps[index].productNumber,
-                productBizNo:ps[index].productBizno
+            url: `${API_PATH}mall/api/cart/v1/updateCartNumber.json`,
+            data: {
+                buyNum: ps[index].productNumber,
+                productBizNo: ps[index].productBizno
             },
-            success:function(data){
+            success: function (data) {
                 _this.setState({products: ps});
             }
         });
     },
-    changeMinus:function(index){
+    changeMinus: function (index) {
         let ps = this.state.products;
-        this.updateCount(index,ps[index].productNumber - 1);
+        this.updateCount(index, ps[index].productNumber - 1);
     },
-    changePlus:function(index){
+    changePlus: function (index) {
         let ps = this.state.products;
-        this.updateCount(index,ps[index].productNumber + 1);
+        this.updateCount(index, ps[index].productNumber + 1);
     },
     render: function () {
         let {products} = this.state;
 
-        let product_item =  (product, index) => {
+        let product_item = (product, index) => {
             return (
                 <div className="shopping-item" key={index}>
                     <div className="checked-icon" onClick={()=>this.checkHandler(index)}>
                         <span className={product.isChecked ? "checked-circle" : "unchecked-circle"}></span>
-                        <input type="hidden" className="checked-bizNo" value={product.isChecked ? product.productBizno : null}/>
+                        <input type="hidden" className="checked-bizNo"
+                               value={product.isChecked ? product.productBizno : null}/>
                     </div>
                     <div className="product-img"><img src={product.img}/></div>
                     <div className="product-item">
                         <div className="product-info">
                             <div className="product-name">{product.productName}</div>
-                            <div className="product-price">¥{product.productPrice*product.productNumber}+{product.beanPrice*product.productNumber}工分</div>
+                            <div className="product-price">
+                                ¥{product.productPrice * product.productNumber}+{product.beanPrice * product.productNumber}工分
+                            </div>
                             <div className="detail-num-change">
                                 <div className="minus" onClick={()=>this.changeMinus(index)}></div>
                                 <div className="input-num">{product.productNumber}</div>
@@ -155,11 +160,11 @@ const ShoppingCart = React.createClass({
                 </div>
             )
         };
-        let total_price=0;
-        let total_score=0;
-        let total=(product, index)=>{
-            product.isChecked ? total_price+=product.productPrice*product.productNumber:total_price;
-            product.isChecked ? total_score+=product.beanPrice*product.productNumber:total_score;
+        let total_price = 0;
+        let total_score = 0;
+        let total = (product, index)=> {
+            product.isChecked ? total_price += product.productPrice * product.productNumber : total_price;
+            product.isChecked ? total_score += product.beanPrice * product.productNumber : total_score;
         };
         this.state.products.map((product, index) => total(product, index));
 
@@ -181,30 +186,37 @@ const ShoppingCart = React.createClass({
         //    }
         //}
 
-        let checkAllCN =  products.reduce((a, b) => a && b.isChecked, true) ?
+        let checkAllCN = products.reduce((a, b) => a && b.isChecked, true) ?
             "total-checked-circle" :
             'total-unchecked-circle';
 
         return (
             <div className="shopping-cart">
                 <div className="cart-header">
-                    {this.props.products.length !=0 ? <div className="all-chosen" onClick={this.allChoseHandler}>
+                    {this.props.products.length != 0 ? <div className="all-chosen" onClick={this.allChoseHandler}>
                         <span className={checkAllCN}></span>
                         <span className="chosenTip">全选</span></div> : null}
                     <div className="cart-title">购物车</div>
                 </div>
-                {this.props.products.length !=0  ? this.state.products.map((product, index) => product_item(product, index)) : <div className="empty-cart-icon"></div>}
-                {this.props.products.length !=0  ?  <div className="pay-bar">
-                    <div className="all-price">合计：<span className="total-price">¥{total_price}+{total_score}工分</span></div>
+                {this.props.products.length != 0 ? this.state.products.map((product, index) => product_item(product, index)) :
+                    <div className="empty-cart-icon"></div>}
+                {this.props.products.length != 0 ? <div className="pay-bar">
+                    <div className="all-price">合计：<span className="total-price">¥{total_price}+{total_score}工分</span>
+                    </div>
                     <a className="pay-btn"
                        onClick={this.payHandler}>结算</a>
                 </div> : null}
                 <div className="fixed-nav">
-                    <a className="fixed-nav-link fixed-nav-link1" onClick={ () => gotoHandler("/static/mall/home/index.html") }></a>
-                    <a className="fixed-nav-link fixed-nav-link2" onClick={ () => gotoHandler("/static/mall/product-category/index.html") }></a>
-                    <a className="backToIndex" onClick={ () => $FW.Browser.inApp() ? NativeBridge.close() : location.href = 'http://m.9888.cn'}></a>
-                    <a className="fixed-nav-link fixed-nav-link3 active" onClick={ () => gotoHandler("/static/mall/shopping-cart/index.html", true) }></a>
-                    <a className="fixed-nav-link fixed-nav-link4" onClick={ () => gotoHandler("/static/mall/user/index.html", true) }></a>
+                    <a className="fixed-nav-link fixed-nav-link1"
+                       onClick={ () => gotoHandler("/static/mall/home/index.html") }></a>
+                    <a className="fixed-nav-link fixed-nav-link2"
+                       onClick={ () => gotoHandler("/static/mall/product-category/index.html") }></a>
+                    <a className="backToIndex"
+                       onClick={ () => $FW.Browser.inApp() ? NativeBridge.close() : location.href = 'http://m.9888.cn'}></a>
+                    <a className="fixed-nav-link fixed-nav-link3 active"
+                       onClick={ () => gotoHandler("/static/mall/shopping-cart/index.html", true) }></a>
+                    <a className="fixed-nav-link fixed-nav-link4"
+                       onClick={ () => gotoHandler("/static/mall/user/index.html", true) }></a>
                 </div>
             </div>
         )
@@ -214,7 +226,7 @@ $FW.DOMReady(function () {
     NativeBridge.setTitle('购物车');
     $FW.Ajax({
         //url: "http://localhost/nginx-1.9.12/html/shoppingcart.json",
-        url:`${API_PATH}mall/api/cart/v1/shoppingCart.json`,
+        url: `${API_PATH}mall/api/cart/v1/shoppingCart.json`,
         enable_loading: true
     }).then(data => ReactDOM.render(<ShoppingCart products={data.cartList}/>, CONTENT_NODE));
 });
