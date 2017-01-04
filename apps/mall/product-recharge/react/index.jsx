508b6b1ed2a307195101b4620e22df2e3c17d4ac
baoxiao <baoxiao@ucfgroup.com>
2017-01-04 10:58:33 +0800

    10:58

--- apps/mall/product-recharge/react/index.jsx
+++ apps/mall/product-recharge/react/index.jsx
@@ -15,7 +15,10 @@ const Recharge = React.createClass({
         }
     },
     componentDidMount: function () {
-        this.switchTabHandler('fee');
+        var query = $FW.Format.urlQuery();
+        var tab= query.tab;
+        if(tab==1){this.switchTabHandler('fee');}
+        else{ this.switchTabHandler('net'); }
     },
 
     switchTabHandler: function (tab) {
