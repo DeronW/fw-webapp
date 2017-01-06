36d300fad79790314098ee09ef008dd85272eb18
xiangwenlong <xiangwenlong@ucfgroup.com>
2017-01-06 16:25:49 +0800

    no message

--- apps/mall/game-red-rain/react/index.jsx
+++ apps/mall/game-red-rain/react/index.jsx
@@ -5,7 +5,7 @@ $FW.DOMReady(function () {
     let startRandom=parseInt(Math.random()*100000+1000);
     let startTime=new Date().getTime();
     let startToken=hex_md5(startRandom+''+startTime);
-    console.log(startToken);
+    console.log(startRandom+''+startTime);
     $FW.Ajax({
         url:`${location.protocol}//game.9888.cn/index.php?r=redrain/trig`,
         data:{
