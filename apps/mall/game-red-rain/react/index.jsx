4c747c8f852d83e56f38eeffcf7016969f57de33
xiangwenlong <xiangwenlong@ucfgroup.com>
2017-01-06 16:15:33 +0800

    c

--- apps/mall/game-red-rain/react/index.jsx
+++ apps/mall/game-red-rain/react/index.jsx
@@ -1,21 +1,22 @@
 //取消下拉刷新
 $FW.DOMReady($FW.Event.cancelSlideDownRefresh);
-
+let Browsertype=$FW.Browser.inIOS()?3:4;
 $FW.DOMReady(function () {
     let startRandom=parseInt(Math.random()*100000+1000);
     let startTime=new Date().getTime();
     let startToken=hex_md5(startRandom+''+startTime);
+    console.log(startToken);
     $FW.Ajax({
         url:`${location.protocol}//game.9888.cn/index.php?r=redrain/trig`,
         data:{
             nonce:startRandom,
             time:startTime,
             token:startToken,
-            client_type:$FW.Browser.inIOS()?3:4,
+            client_type:Browsertype
         },
         method:'POST',
         success:data=>{
-            console.log(startToken);
+            //console.log(startToken);
         }
     });
     //倒计时准备开始
