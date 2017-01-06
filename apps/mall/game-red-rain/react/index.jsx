a95ea4e3311ae650b5ba2511bd3dfef22612ac4d
xiangwenlong <xiangwenlong@ucfgroup.com>
2017-01-06 13:41:46 +0800

    添加md5

--- apps/mall/game-red-rain/react/index.jsx
+++ apps/mall/game-red-rain/react/index.jsx
@@ -2,6 +2,22 @@
 $FW.DOMReady($FW.Event.cancelSlideDownRefresh);
 
 $FW.DOMReady(function () {
+    let startRandom=parseInt(Math.random()*100000+1000);
+    let startTime=new Date().getTime();
+    let startToken=hex_md5(startRandom+''+startTime);
+    $FW.Ajax({
+        url:`${location.protocol}//game.9888.cn/index.php?r=redrain/trig`,
+        data:{
+            rand:startRandom,
+            time:startTime,
+            token:startToken,
+            trig:'',
+        },
+        method:'POST',
+        success:data=>{
+            console.log(startToken);
+        }
+    });
     //倒计时准备开始
     readyCounting(
         () => fnStartRedbag(
