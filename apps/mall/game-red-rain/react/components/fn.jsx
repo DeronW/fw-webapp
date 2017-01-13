a610fa5c973974ece8091dec9f379f21a3877cb3
xiangwenlong <xiangwenlong@ucfgroup.com>
2017-01-13 16:09:23 +0800

    返回close

--- apps/mall/game-red-rain/react/components/fn.jsx
+++ apps/mall/game-red-rain/react/components/fn.jsx
@@ -64,10 +64,13 @@ function fnShowResult(num) {
                         G('success-btn').onclick = function () {
                             if (mygetprize == 1) {
                                 NativeBridge.toNative('app_scores');
+                                NativeBridge.close();
                             } else if(mygetprize == 2) {
                                 NativeBridge.toNative('app_coupon');
+                                NativeBridge.close();
                             }else if(mygetprize==3) {
                                 NativeBridge.toNative('app_fanxiCoupon');
+                                NativeBridge.close();
                             }
                         };
                     }
@@ -78,6 +81,6 @@ function fnShowResult(num) {
         });
 };
 G('fail-btn').onclick = function () {
-    NativeBridge.close()
+    NativeBridge.close();
 };
 
