d49a079736d94ba99ea01002bade7deccdd6ab91
xiangwenlong <xiangwenlong@ucfgroup.com>
2017-01-17 14:05:57 +0800

    game-year-end

--- apps/wap/game-year-end/react/index.jsx
+++ apps/wap/game-year-end/react/index.jsx
@@ -1,70 +1,86 @@
 $FW.DOMReady(function () {
     let str;
-    let login=true;
-    $("#p4-3-2").attr("src","images/p4-text"+parseInt(Math.random()*3+1)+".png");
-    $FW.Ajax(`${API_PATH}/mpwap/api/v1/getAllRegistCount.shtml`).then((data)=>{
-        $(".p1-num").html(data.allUserCount);
+    let login = true;
+    $("#p4-3-2").attr("src", "images/p4-text" + parseInt(Math.random() * 3 + 1) + ".png");
+    $FW.Ajax(`${API_PATH}/mpwap/api/v1/getAllRegistCount.shtml`).then((data) => {
+        let allUserCount = data.allUserCount.split("");
+        let s = "";
+        for (var i = 0; i < allUserCount.length; i++) {
+            s += "<span class='p1-num" + i + "' style='transition-delay:" + 0.2 * i + "s ;'>" + allUserCount[i] + "</span>";
+        }
+        $("#p1-num").html(s);
     });
     $FW.Ajax({
         url: `${API_PATH}/mpwap/api/v1/getUserRanking.shtml`,//第二三页
         fail: () => true,
         complete: (data) => {
-            if(data.code==10000){
+            if (data.code == 10000) {
                 swiper.lockSwipeToNext();
                 $(".p2-0-1").html(data.userName);
                 $(".p2-2-2").html(data.registDate);
                 $("#p2-2-3").html(data.myRank);
                 $(".p1-2").hide();
-                $FW.Ajax(`${API_PATH}/mpwap/api/v1/getUserIncome.shtml`).then((data)=>{
+                $FW.Ajax(`${API_PATH}/mpwap/api/v1/getUserIncome.shtml`).then((data) => {
                     $("#p3-2").html(data.investTimesAll);
                     $("#p3-3").html(data.investAmountAll);
                     $("#p3-5").html(data.allReceipt);
                     $("#smoke-num").html(data.pipeCount);
                     $("#cloth-num").html(data.windbreakCount);
-                    $FW.Ajax(`${API_PATH}/mpwap/api/v1/getUserHabitMess.shtml`).then((data)=>{
+
+                    $("#li-r1").html(data.receivedInterest);
+                    $("#li-r2").html(data.waitedInterest);
+                    $("#li-r3").html(data.cashCoupon);
+                    $("#li-r4").html(data.beanCount);
+                    $("#li-r5").html(data.balanceProfit);
+
+                    $FW.Ajax(`${API_PATH}/mpwap/api/v1/getUserHabitMess.shtml`).then((data) => {
                         $("#p4-0").html(data.investTimesYear);
                         $("#p4-1").html(data.monthName);
                         $("#p4-2").html(data.investTimeSlot);
-                        $("#p5-1").html(data.investAmountYear);
-                        if(data.monthName=="实习豆尔摩斯"){
-                            $("#p5-2").attr("src","images/p5-level-1.png");
-                        }else if(data.monthName=="初级豆尔摩斯"){
-                            $("#p5-2").attr("src","images/p5-level-2.png");
-                        }else if(data.monthName=="精英豆尔摩斯"){
-                            $("#p5-2").attr("src","images/p5-level-3.png");
-                        }else if(data.monthName=="特级豆尔摩斯"){
-                            $("#p5-2").attr("src","images/p5-level-4.png");
-                        }else if(data.monthName=="王牌豆尔摩斯"){
-                            $("#p5-2").attr("src","images/p5-level-5.png");
+                        let investAmountYear = (data.investAmountYear + "").split("");
+                        let p5 = "";
+                        for (var j = 0; j < investAmountYear.length; j++) {
+                            p5 += "<span class='p5-1-" + j + "' style='transition-delay:" + 0.2 * j + "s ;'>" + investAmountYear[j] + "</span>";
+                        }
+                        $("#p5-1").html(p5);
+                        if (data.monthName == "实习豆尔摩斯") {
+                            $("#p5-2").attr("src", "images/p5-level-1.png");
+                        } else if (data.monthName == "初级豆尔摩斯") {
+                            $("#p5-2").attr("src", "images/p5-level-2.png");
+                        } else if (data.monthName == "精英豆尔摩斯") {
+                            $("#p5-2").attr("src", "images/p5-level-3.png");
+                        } else if (data.monthName == "特级豆尔摩斯") {
+                            $("#p5-2").attr("src", "images/p5-level-4.png");
+                        } else if (data.monthName == "王牌豆尔摩斯") {
+                            $("#p5-2").attr("src", "images/p5-level-5.png");
                         }
                     });
                 });
-            }else if(data.code==40101){
-                login=false;
+            } else if (data.code == 40101) {
+                login = false;
                 swiper.lockSwipeToNext();
                 $(".p1-2").show();
-            }else{
+            } else {
                 $FW.Component.Alert(code.message);
             }
-
         }
     });
     music();
-    //load();
+    load();
     $(".swiper-container").css("width", $(window).width());
     $(".swiper-container").css("height", $(window).height());
     //分享弹层
-    $(".pop-share").on("touchstart",function(){
+    $(".pop-share").on("touchstart", function () {
         $(".pop-share").hide();
     });
-    $(".p6-8").on("touchstart",function(){
+    $(".p6-8").on("touchstart", function () {
         $(".pop-share").show();
     });
     //查看收益弹层
-    $(".pop-close").on("touchstart",function(){
+    $(".pop-close").on("touchstart", function () {
         $(".pop-detail-box").hide();
     });
-    $("#p3-5-img").on("touchstart",function(){
+    $("#p3-5-img").on("touchstart", function () {
         $(".pop-detail-box").show();
     });
 
@@ -72,24 +88,24 @@ $FW.DOMReady(function () {
         loop: false,
         direction: 'vertical',
         onSlideChangeEnd: function (swiper) {
-            if(swiper.isEnd||!login&&swiper.activeIndex==1){
+            if (swiper.isEnd || !login && swiper.activeIndex == 1) {
                 $(".up").hide();
-            }else{
+            } else {
                 $(".up").show();
             }
         },
-        onTouchStart: function(swiper,even){
+        onTouchStart: function (swiper, even) {
             swiper.unlockSwipeToPrev();
             swiper.unlockSwipeToNext();
-            if(!login&&swiper.activeIndex==1||swiper.isEnd){
+            if (!login && swiper.activeIndex == 1 || swiper.isEnd) {
                 swiper.lockSwipeToNext();
             }
-            if(swiper.isEnd||!login&&swiper.activeIndex==1){
+            if (swiper.isEnd || !login && swiper.activeIndex == 1) {
                 $(".up").hide();
-            }else{
+            } else {
                 $(".up").show();
             }
-            if(swiper.activeIndex==0){
+            if (swiper.activeIndex == 0) {
                 swiper.lockSwipeToPrev();
             }
         }
@@ -110,8 +126,8 @@ $FW.DOMReady(function () {
         });
     }
 
-    var gurl = `${API_PATH}/static/wap/user-evaluate/index.html`;
-    var iurl = `${API_PATH}/static/wap/user-evaluate/images/share-ico.png`;
+    var gurl = `${API_PATH}/static/wap/game-year-end/index.html`;
+    var iurl = `${API_PATH}/static/wap/game-year-end/images/share-ico.png`;
 
     function setShareFriend() {
         wx.onMenuShareAppMessage({
@@ -128,6 +144,7 @@ $FW.DOMReady(function () {
             }
         });
     }
+
     function setShareFriendQuan() {
         wx.onMenuShareTimeline({
             title: '豆尔摩斯年度大揭秘，快来查看你的组织身份！', // 分享标题
@@ -143,13 +160,13 @@ $FW.DOMReady(function () {
     }
 
 
- $FW.Ajax({
-     url:`${location.protocol}//game.9888.cn/index.php?r=games/getshare`,
-     data:{url:location.href},
-     success:(data)=>{
-         setWxConfig(false, data.appId, data.timestamp, data.nonceStr, data.signature);
-         setShareFriend();
-         setShareFriendQuan();
-     }
- })
+    $FW.Ajax({
+        url: `${location.protocol}//game.9888.cn/index.php?r=games/getshare`,
+        data: {url: location.href},
+        success: (data) => {
+            setWxConfig(false, data.appId, data.timestamp, data.nonceStr, data.signature);
+            setShareFriend();
+            setShareFriendQuan();
+        }
+    })
 });
