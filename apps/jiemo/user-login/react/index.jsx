f7a1c38764df8e8684717565bc0e15417a5b7b6d
jiabin.xiong <jiabin.xiong@imduoduo.com>
2017-01-19 15:13:50 +0800

    修改

--- apps/jiemo/user-login/react/index.jsx
+++ apps/jiemo/user-login/react/index.jsx
@@ -2,7 +2,7 @@ const Register = React.createClass({
 	getInitialState() {
 		return {
 			pwdVal: ''
-		}	
+		}
 	},
 	changePwd(e) {
 		this.setState({
@@ -20,18 +20,19 @@ const Register = React.createClass({
 				mobile: location.search.split("=")[1],
 				password: _this.state.pwdVal,
 				sourceType: 3
-			},			
+			},
 			success: function (data) {
 				localStorage.userGid = data.userLogin.userGid;
 				localStorage.userId = data.userLogin.userId;
 				localStorage.userToken = data.userLogin.userToken;
+				localStorage.userStatus = data.userLogin.userStatus;
 
-				location.href = `${location.protocol}\/\/${location.host}/static/apply-loan/index.html`;
+				//location.href = `${location.protocol}\/\/${location.host}/static/apply-loan/index.html`;
 			},
 			fail: function(code, mes) {
 
 			}
-		})	
+		})
 	},
 	render() {
 		return (
@@ -39,34 +40,34 @@ const Register = React.createClass({
 				<div className="top">
 					<span className="icon"></span>
 					<span className="title">登录</span>
-				</div>	
+				</div>
 
 				<div className="logo">
-					<img src="images/logo.png" />	
+					<img src="images/logo.png" />
 				</div>
 
 				<div className="get-name-phone">
-					<span className="phone-text">{location.search.split("=")[1]}</span>欢迎登录现金贷!	
+					<span className="phone-text">{location.search.split("=")[1]}</span>欢迎登录现金贷!
 				</div>
-				
+
 				<div className="from-cnt">
 					<div className="from">
 						<div className="icon"></div>
 
 						<div className="input">
-							<input 
+							<input
 								type="passwold"
-		   						placeholder="请输入登录密码" 
+		   						placeholder="请输入登录密码"
 								onChange={this.changePwd}
 							/>
 						</div>
-							
+
 						<div className="pwd-icon">
-								
+
 						</div>
 					</div>
 				</div>
-						
+
 
 				<div className="register-login-btn">
 					<div className="ui-btn" onClick={this.loadingBtn}>下一步</div>
@@ -76,7 +77,7 @@ const Register = React.createClass({
 					<a href="">忘记密码?</a>
 				</div>
 
-			</div>	
+			</div>
 		)
 	}
 });
--- apps/jiemo/user-set-cash-card/react/index.jsx
+++ apps/jiemo/user-set-cash-card/react/index.jsx
@@ -28,42 +28,91 @@ function space(str) {
     return str.replace(/ /g, "");
 }
 
+const WithholdServer = React.createClass({
+	handlerBack(ble) {
+		this.props.getWithholdServerPop(false);
+	},
+	render() {
+		return (
+			<div className="withhold-server">
+				<div className="title-nav">
+					<div className="icon" onClick={this.handlerBack}></div>
+					<div className="title-text">
+						代扣服务协议
+					</div>
+				</div>
+
+				<div className="withhold-server-cnt">
+					<div className="title">
+						借款协议
+					</div>
+
+					<div className="text">
+						欢迎阅读金融工场（以下简称本公司）用户协议。本协议将详述您在域名为 www.9888.cn 的互联网网站（备案号为：京ICP证130046号，以下简称金融工场）使用本公司服务所须遵守的条款和条件。
+					</div>
+					<div className="text">
+						您成为金融工场用户前，必须阅读、同意并接受本协议中所含的所有条款和条件，包括以下明示载明的及因被提及而纳入的文件、条款和条件。本公司强烈建议：您阅读本协议时，也应阅读本协议所提及的其他网页中所包含的资料，因为其可能包含对作为金融工场用户的您适用的进一步条款和条件。请注意：点击划有底线的词句即可链接到相应网页。
+					</div>
+					<div className="text">
+			您申请注册为金融工场用户，表明您已经充分阅读、理解并无任何附加条件的接受了本协议中含有的所有条款和条件，包括本协议中载明的及因被提及而纳入的所有文件、条款和条件。
+					</div>
+
+					<div className="text">
+						<div className="q">
+							一、用户资格
+						</div>
+						<div className="a">
+							1.1 本公司的服务仅向适用法律规定的能够签订有法律约束力文件的个人、企业及其他组织提供并由其使用。
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+});
+
 const SetCashCard = React.createClass({
 	getInitialState() {
 		return {
 			name: '',
 			id: '',
 			bankNum: '',
+            bankName: '',
 			phone: '',
 			cardinfoBankName: '',
-			cardinfoLogoUrl: ''
+			cardinfoLogoUrl: '',
+            cardType: '',
+            selectClause: false,
+			withholdServerPop: false,
+            loading: false
 		}
 	},
 	changeName(e) {
-		let val = e.target.value;	
-		
-		this.setState({
-			name: space(val) 
-		});		
-	},
-	changeId(e) {
-		let val = e.target.value;	
+		let val = e.target.value;
 
 		this.setState({
-			id: val
+			name: space(val)
+		});
+	},
+	changeId(e) {
+		let val = e.target.value;
+
+		this.setState({
+			id: space(val)
 		});
 	},
 	changeBankNum(e) {
-		let val = e.target.value;	
-		
+		let val = e.target.value;
+
 		this.setState({
 			bankNum: numberFormat.format(val)
-		});	
+		});
 	},
-	blurBankNum() {
+	blurBankNum(e) {
+
 		if(!space(this.state.bankNum).length > 19 || !space(this.state.bankNum).length < 16) {
 			$FW.Ajax({
-				url: `${API_PATH}api/bankcard/v1/cardinfo.json`,	
+				url: `${API_PATH}api/bankcard/v1/cardinfo.json`,
             	method: "POST",
 				data: {
 					bankCardNo: space(this.state.bankNum),
@@ -75,60 +124,114 @@ const SetCashCard = React.createClass({
 			}).then((data) => {
 				this.setState({
 					cardinfoBankName: data.cardInfo.bankName,
-					cardinfoLogoUrl: data.cardInfo.logoUrl
-				});	
+					cardinfoLogoUrl: data.cardInfo.logoUrl,
+                    cardType: data.cardInfo.cardType,
+                    bankName: data.cardInfo.bankName
+
+				});
 			}, (error) => {
-				
+
 			})
 		} else {
-			$FW.Component.Toast("储蓄卡格式不对");	
+			$FW.Component.Toast("储蓄卡格式不对");
 		}
 	},
 	changePhone (e) {
-		let val = e.target.value;	
+		let val = e.target.value;
 
 		if(verificationNum(val)) {
 			if(val.length <= 11) {
 				this.setState({
-					phone: space(val)  
+					phone: space(val)
 				});
-			}		
+			}
 		}
 	},
-
+	handlerClause() {
+		this.setState({
+			selectClause: !this.state.selectClause
+		});
+	},
+	handlerWithholdServer() {
+		this.setState({
+			withholdServerPop: true
+		});
+	},
+	callbackWithholdServerPop(ble) {
+		this.setState({
+			withholdServerPop: ble
+		});
+	},
 	handlerNext() {
-		if(this.state.name == '') {
+        console.log(this.state.bankNum);
+        // console.log(this.state.name);
+        // console.log(this.state.id);
+        // console.log(this.state.bankName);
+        // console.log(this.state.bankNum);
+        // console.log(this.state.phone);
+        // console.log(localStorage.userStatus);
+
+        if(this.state.name == '') {
 			$FW.Component.Toast("姓名不能为空");
-		} else if (this.state.id == '') {
+		} else if(this.state.name.length > 20) {
+			$FW.Component.Toast("姓名不能超过20个字符");
+		}else if (this.state.id == '') {
 			$FW.Component.Toast("身份证不能为空");
 		} else if(!isCardNo(this.state.id)) {
 			$FW.Component.Toast("身份证格式不对");
-		} else if (this.state.bankNum) {
+		} else if (this.state.bankNum == '') {
 			$FW.Component.Toast("储蓄卡不能为空");
 		} else if(space(this.state.bankNum).length > 19 || space(this.state.bankNum).length < 16) {
-			$FW.Component.Toast("储蓄卡格式不对");	
+			$FW.Component.Toast("储蓄卡格式不对");
 		} else if (this.state.phone == '') {
 			$FW.Component.Toast("手机号不能为空");
 		} else if (!isMobilePhone(this.state.phone)) {
 			$FW.Component.Toast("手机号格式不对");
+		} else if(!this.state.selectClause) {
+			$FW.Component.Toast("请勾选代扣服务协议");
 		} else {
-				
-		}	
-	},	
+			$FW.Ajax({
+				url: `${API_PATH}api/bankcard/v1/commitinfo.json`,
+				method: 'POST',
+                data: {
+                    bankName: this.state.bankName,
+                    cardHolderName: this.state.name,
+                    cardNo: space(this.state.bankNum),
+                    cardType: this.state.cardType,
+                    idCard: this.state.id,
+                    mobile: this.state.phone,
+                    operatorType: localStorage.userStatus,
+                    token: localStorage.userToken,
+                    userGid: localStorage.userGid,
+                    userId: localStorage.userId,
+                    sourceType: 3
+                }
+
+			}).then((data) => {
+                console.log(data);
+            },(error) => {
+                console.log(error);
+            });
+		}
+	},
 	render() {
+
 		return (
 			<div className="set-cash-card-cnt">
+				{
+					this.state.withholdServerPop ? <WithholdServer getWithholdServerPop={this.callbackWithholdServerPop} /> : null
+				}
 				<div className="ui-froms">
 					<div className="list">
 						<span className="text">姓名</span>
 						<div className="input">
-							<input onChange={this.changeName} type="text" placeholder="请输入姓名" />
+							<input onChange={this.changeName} value={this.state.name} type="text" placeholder="请输入姓名" />
 						</div>
 					</div>
 					<div className="list">
 						<span className="text">身份证号</span>
 						<div className="input">
-							<input onChange={this.changeId} type="text" placeholder="请输入身份证号码" />
+							<input onChange={this.changeId} value={this.state.Id} type="text" placeholder="请输入身份证号码" />
 						</div>
 					</div>
 				</div>
@@ -146,9 +249,10 @@ const SetCashCard = React.createClass({
 								<img src="images/prompt-icon.png" />
 							</span>
 							{
-								this.state.cardinfoBankName != '' ? <span className="bank"><img className="logo-icon" src={this.state.cardinfoLogoUrl} />{this.state.cardinfoBankName}</span> : null
+								this.state.cardinfoBankName != '' ?
+                                    <span className="bank"><img className="logo-icon" src={this.state.cardinfoLogoUrl} />{this.state.cardinfoBankName}</span> : null
 							}
-						</div>	
+						</div>
 					</div>
 				</div>
 
@@ -162,20 +266,21 @@ const SetCashCard = React.createClass({
 				</div>
 
 				<div className="clause">
-					<span className="icon"></span>
+					<span className={"icon " + (this.state.selectClause ? "select-icon" : "icon")} onClick={this.handlerClause}></span>
 					<span className="text">
 						同意
-						<a href="">《代扣服务协议》</a>
-					</span>	
+						<span onClick={this.handlerWithholdServer}>《代扣服务协议》</span>
+					</span>
 				</div>
 
 				<div className="next-btn">
 					<div onClick={this.handlerNext} className="ui-btn">下一步</div>
 				</div>
-			</div>		
+			</div>
 		)
 	}
 });
+
 
 
 ReactDOM.render(<Header title={"设置提现卡"}/>, document.getElementById('header'));
