bea99cf67e133f137639f1fb3800823e3130469f
彭焱 <pengyan@ucfgroup.com>
2017-02-07 17:54:23 +0800

    底部导航公共组件链接修改

--- lib/components/loan/bottom-nav-bar.jsx
+++ lib/components/loan/bottom-nav-bar.jsx
@@ -31,7 +31,7 @@ const BottomNavBar = React.createClass({
 
         return (
             <div className="jiemo_style_footer_fixed" style={jiemo_style_footer_fixed}>
-                <a className={this.props.index == 1 ? "jiemo_style_footer_item1 active" : "jiemo_style_footer_item1"} onClick={() => this.gotoHandler("/static/loan/apply-loan/index.html")}></a>
+                <a className={this.props.index == 1 ? "jiemo_style_footer_item1 active" : "jiemo_style_footer_item1"} onClick={() => this.gotoHandler("/static/loan/home/index.html")}></a>
                 <a className={this.props.index == 2 ? "jiemo_style_footer_item2 active" : "jiemo_style_footer_item2"} onClick={() => this.gotoHandler("/static/loan/bill/index.html")}></a>
                 <a className={this.props.index == 3 ? "jiemo_style_footer_item3 active" : "jiemo_style_footer_item3"} onClick={() => this.gotoHandler("/static/loan/user/index.html")}></a>
             </div>
