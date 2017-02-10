5d22796564812f7341b0e81ed9593050e025d111
delong <w.del@qq.com>
2017-02-10 10:49:32 +0800

    refactory user-entry, fix input error

--- apps/loan/user-entry/react/index.jsx
+++ apps/loan/user-entry/react/index.jsx
@@ -1,54 +1,41 @@
-function verificationNum(val) {
-    var reg = new RegExp("^[0-9]*$");
-    return reg.test(val)
-}
-
 function isMobilePhone(phone) {
     return /^1(3|4|5|7|8)\d{9}$/.test(phone)
 }
 
 const Register = React.createClass({
     getInitialState() {
-        return {
-            val: ''
-        }
+        return { phone: '' }
     },
-    changeVal(e) {
-        if (verificationNum(e.target.value)) {
-            this.setState({
-                val: e.target.value
-            });
-        }
+    changeHandler(e) {
+        let v = e.target.value;
+        console.log(v)
+        this.setState({ phone: parseInt(v) || 1 })
     },
 
     handleGetCode() {
-        var _this = this;
+        let phone = this.state.phone;
 
-        if (!isMobilePhone(this.state.val)) {
+        if (!isMobilePhone(phone)) {
             $FW.Component.Toast("手机号格式不对");
-        } else {
-            $FW.Ajax({
-                url: API_PATH + "api/userBase/v1/sendVerifyCode.json",
-                method: "POST",
-                data: {
-                    mobile: _this.state.val,
-                    userOperationType: 3,
-                    sourceType: 3
-                },
-                success: function (data) {
-                    location.href = location.protocol + "//" + location.host + "/static/loan/user-register/index.html?codeToken=" + data.codeToken + "&phone=" + _this.state.val;
-
-                    localStorage.phone = _this.state.val;
-                },
-                fail: function (code, mes) {
-                    if (code == 1029) {
-                        location.href = location.protocol + "//" + location.host + "/static/loan/user-login/index.html?phone=" + _this.state.val;
-
-                        localStorage.phone = _this.state.val;
-                    }
-                }
-            })
+            return;
         }
+
+        $FW.Post(`${API_PATH}api/userBase/v1/sendVerifyCode.json`, {
+            mobile: phone,
+            userOperationType: 3,
+            sourceType: 3
+        }).then(data => {
+            localStorage.phone = phone;
+            location.href = `/static/loan/user-register/index.html?codeToken=${data.codeToken}&phone=${phone}`;
+        }, (code, msg) => {
+            if (code === 1029) {
+                localStorage.phone = phone;
+                location.href = `/static/loan/user-login/index.html?phone=${phone}`;
+            } else {
+                $FW.Component.Alert(msg)
+            }
+        })
+
     },
 
     render() {
@@ -58,29 +45,24 @@ const Register = React.createClass({
                     <span className="title">现金贷</span>
                 </div>
 
-                <div className="logo">
-                    <img src="images/logo.png" />
-                </div>
+                <div className="logo"> <img src="http://placehold.it/214x214" /> </div>
 
                 <div className="register-login-cnt">
                     <div className="from-cnt">
                         <div className="from">
                             <div className="icon"></div>
-
                             <div className="input">
-                                <input type="text" value={this.state.val} onChange={this.changeVal} placeholder="请输入手机号进行注册登录" />
+                                <input type="text" value={this.state.phone}
+                                    onChange={this.changeHandler}
+                                    placeholder="请输入手机号进行注册登录" />
                             </div>
-
-                            <div className="pwd-icon">
-
-                            </div>
+                            <div className="pwd-icon"> </div>
                         </div>
                     </div>
-
                 </div>
 
                 <div className="register-login-btn">
-                    <div className="ui-btn" onClick={() => this.handleGetCode()}>下一步</div>
+                    <div className="ui-btn" onClick={this.handleGetCode}>下一步</div>
                 </div>
 
             </div>
