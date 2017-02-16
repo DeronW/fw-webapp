01c17b40163c5f7c6957bbbdcddf03ddca4658e5
baoxiao <baoxiao@ucfgroup.com>
2017-02-16 11:03:39 +0800

    11：03

--- apps/mall/product-recharge/react/index.jsx
+++ apps/mall/product-recharge/react/index.jsx
@@ -71,7 +71,7 @@ const Recharge = React.createClass({
         console.log('load net', code)
 
         $FW.Ajax({
-            url: API_PATH + 'api/v1/phone/net/recharge.json',
+            url: API_PATH + 'mall/api/v1/phone/net/recharge.json',
             enable_loading: 'mini',
             data: {operator: code},
             success: function (data) {
