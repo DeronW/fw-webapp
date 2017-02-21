51e0c1b63be9f09d09c6766902b0706528208381
delong <w.del@qq.com>
2017-02-21 12:46:11 +0800

    add Object.assign polyfill & format common javascripts dir

--- tasks/index.js
+++ tasks/index.js
@@ -17,10 +17,11 @@ let COMMON_JAVASCRIPTS_TASK = {};
 
 function get_common_javascript_files(lib_path, extend_files, debug) {
     let files = [
-        `${lib_path}request-animation-frame-0.0.23.js`,
-        `${lib_path}promise-2.0.2.min.js`,
+        `${lib_path}javascripts/request-animation-frame-0.0.23.js`,
+        `${lib_path}javascripts/promise-2.0.2.min.js`,
+        `${lib_path}javascripts/object-assign-4.1.1.js`,
         `${lib_path}fw-1.7.1.js`,
-        `${lib_path}native-bridge-0.4.0.js`
+        `${lib_path}javascripts/native-bridge-0.4.0.js`
     ];
 
     if (debug) {
