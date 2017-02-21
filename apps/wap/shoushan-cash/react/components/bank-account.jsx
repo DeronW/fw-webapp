4f8ca2e2ab0603dddd3cf10581f4d2434757e727
xjb710 <xiongjiabin710@gmail.com>
2017-02-21 10:57:16 +0800

    测试

--- apps/wap/shoushan-cash/react/components/bank-account.jsx
+++ apps/wap/shoushan-cash/react/components/bank-account.jsx
@@ -107,6 +107,8 @@ var BankAccount = React.createClass({
                         <img className="false" onClick={this.handleClear} src="images/false.jpg" />
                     </div>
 
+                    <div >{this.state.bankList.length}</div>
+
                     {this.state.bankList.length && list()}
 
                     {
