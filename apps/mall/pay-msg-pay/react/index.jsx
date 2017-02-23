ecf6d55fa58503650ba865b05c4c8d4efba96a89
baoxiao <baoxiao@ucfgroup.com>
2017-02-23 22:02:10 +0800

    22:02

--- apps/mall/pay-msg-pay/react/index.jsx
+++ apps/mall/pay-msg-pay/react/index.jsx
@@ -151,6 +151,7 @@ const SendCode = React.createClass({
         }).then(data=> {
             $FW.Component.showAjaxLoading('mini');
             if (data.responseResult.resCode == "20005") {//短信校验失败
+                FW.Component.hideAjaxLoading();
                 $FW.Component.Alert(data.responseResult.resMessage);
             } else if (data.responseResult.resCode == "00000") {
                 setTimeout(() => {
@@ -160,6 +161,7 @@ const SendCode = React.createClass({
                     this.queryState('final');
                 }, 6000);
             } else {
+                FW.Component.hideAjaxLoading();
                 $FW.Component.Alert(data.responseResult.resMessage);
             }
             /* setTimeout(() => {
