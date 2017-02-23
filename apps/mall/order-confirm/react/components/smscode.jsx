270df7145c3591529abf6ac7d58495444835323d
delong <w.del@qq.com>
2017-02-23 14:43:29 +0800

    update

--- apps/mall/order-confirm/react/components/smscode.jsx
+++ apps/mall/order-confirm/react/components/smscode.jsx
@@ -1,9 +1,9 @@
 const SMSCode = React.createClass({
     getInitialState: function () {
-        return {value: '', remain: 0}
+        return { value: '', remain: 0 }
     },
     changeValueHandler: function (e) {
-        this.setState({value: e.target.value});
+        this.setState({ value: e.target.value });
         this.props.update_sms_code_handler(e.target.value);
     },
     getSmsCodeHandler: function () {
@@ -20,17 +20,17 @@ const SMSCode = React.createClass({
                         $FW.Component.Alert('原来你在测试, 那就直接告诉你验证码\n ' + data.validateCode);
                 },
                 fail: function () {
-                    _this.setState({remain: 0});
+                    _this.setState({ remain: 0 });
                 }
             })
         }
     },
     countingDown: function () {
         if (this.state.remain <= 1) window.clearInterval(this._timer);
-        this.setState({remain: this.state.remain - 1});
+        this.setState({ remain: this.state.remain - 1 });
     },
     tick: function () {
-        this.setState({remain: 60});
+        this.setState({ remain: 60 });
         this._timer = setInterval(this.countingDown, 1000);
     },
     render: function () {
@@ -39,10 +39,10 @@ const SMSCode = React.createClass({
                 <div className="test-cnt">
                     <div className="test-input">
                         <input type="text" value={this.state.value} onChange={this.changeValueHandler}
-                               placeholder="请输入验证码"/>
+                            placeholder="请输入验证码" />
                     </div>
-                    <div className={this.state.remain>0 ? "btn-countdown" : "btn-test-blue"}
-                         onClick={this.getSmsCodeHandler}>
+                    <div className={this.state.remain > 0 ? "btn-countdown" : "btn-test-blue"}
+                        onClick={this.getSmsCodeHandler}>
                         {this.state.remain > 0 ? this.state.remain + 's' : '获取验证码'}
                     </div>
                 </div>
