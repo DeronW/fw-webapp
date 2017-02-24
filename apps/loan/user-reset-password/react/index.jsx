1c90f93a07c100c78a08cc7c787a834a278f04b4
delong <w.del@qq.com>
2017-02-24 12:20:24 +0800

    debug

--- apps/loan/user-reset-password/react/index.jsx
+++ apps/loan/user-reset-password/react/index.jsx
@@ -88,8 +88,7 @@ const Register = React.createClass({
                     gid: dict.userGid,
                     status: dict.userStatus
                 })
-
-                // window.location.href = "/static/loan/home/index.html"
+                window.location.href = "/static/loan/home/index.html"
             }, e => $FW.Component.Toast(e.message))
     },
     render() {
