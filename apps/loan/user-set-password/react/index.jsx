559e67fcbe046ae9deb8d25f0f41492e091ba4e4
彭焱 <pengyan@ucfgroup.com>
2017-02-24 15:24:06 +0800

    update

--- apps/loan/user-set-password/react/index.jsx
+++ apps/loan/user-set-password/react/index.jsx
@@ -31,7 +31,7 @@ const SetPassword = React.createClass({
     },
     changeCode(e) {
         let v = e.target.value;
-        v.length == 4 && isNum(v) && this.setState({ code: v });
+        v.length <= 4 && !isNaN(v) && this.setState({ code: v });
     },
     changePasswordHandler(e) {
         let v = e.target.value;
@@ -69,7 +69,7 @@ const SetPassword = React.createClass({
         let err, {password, code, checked, codeToken} = this.state;
 
         if (code == '') err = "验证码不能为空";
-        if (code.length != 4) err = "验证码为四位数字";
+        if (code.length > 4) err = "验证码为四位数字";
         if (!isNum(code)) err = "验证码为数字";
         if (password == '') err = "密码不能为空";
         if (password.length < 8) err = "密码不能少于8位";
@@ -118,7 +118,7 @@ const SetPassword = React.createClass({
                     <div className="list code-list">
                         <span className="icon"></span>
                         <div className="input">
-                            <input type="number" onChange={this.changeCode}
+                            <input type="number" onChange={this.changeCode} value={this.state.code}
                                 placeholder="输入手机验证码" />
                         </div>
                         {btnSMSCode}
