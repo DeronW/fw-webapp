0ede9aafd9859c5e01b428f40d0e6ae48d5cdef5
baoxiao <baoxiao@ucfgroup.com>
2017-03-14 16:54:02 +0800

    16：53

--- apps/mall/product-detail/react/index.jsx
+++ apps/mall/product-detail/react/index.jsx
@@ -204,13 +204,12 @@ const PlusMinus = React.createClass({
 
         let _this = this;
         let bizNo = $FW.Format.urlQuery().bizNo;
-
         if (this.props.is_login == 0) {
             $FW.Ajax({
                 url: `${API_PATH}mall/api/cart/v1/shoppingCart.json`,
                 enable_loading: 'mini'
             }).then(data => {
-
+                //  location.href = linkLogin
             });
         } else {
             $FW.Ajax({
@@ -279,7 +278,7 @@ const PlusMinus = React.createClass({
             gotoHandler(link);
         } else {
             if (!isCanBuy) {
-                 $FW.Ajax({
+                $FW.Ajax({
                     url: `${API_PATH}mall/api/cart/v1/shoppingCart.json`,
                     enable_loading: 'mini'
                 }).then(data => {
