16aad881aa7574d0dac047405c6bcb09d0a7d0b1
baoxiao <baoxiao@ucfgroup.com>
2017-03-14 15:55:18 +0800

    15：55

--- apps/mall/product-detail/react/index.jsx
+++ apps/mall/product-detail/react/index.jsx
@@ -29,6 +29,7 @@ const Product = React.createClass({
 
     componentDidMount: function () {
         document.title = this.props.data.title;
+
     },
     render: function () {
         let topBuyCart = `_style_buy_cart ${$FW.Browser.inIOSApp() && '_top_buy_cart'}`;
@@ -206,10 +207,12 @@ const PlusMinus = React.createClass({
         let bizNo = $FW.Format.urlQuery().bizNo;
 
         if (this.props.is_login == 0) {
+
             $FW.Ajax({
                 url: `${API_PATH}mall/api/cart/v1/shoppingCart.json`,
                 enable_loading: 'mini'
             }).then(data => {
+                
             });
         } else {
             $FW.Ajax({
