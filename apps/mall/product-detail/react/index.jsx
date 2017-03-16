45a951f80960bff639e570fddea98467c6d99712
baoxiao <baoxiao@ucfgroup.com>
2017-03-16 11:19:38 +0800

    11：19

--- apps/mall/product-detail/react/index.jsx
+++ apps/mall/product-detail/react/index.jsx
@@ -46,6 +46,8 @@ const Product = React.createClass({
     render: function () {
         let topBuyCart = `_style_buy_cart ${$FW.Browser.inIOSApp() && '_top_buy_cart'}`;
 
+        if (!$FW.Browser.inWeixin()) document.querySelector('#cnt').style.marginTop = "-100px";
+
         if ($FW.Browser.inIOSApp()) document.querySelector('._style_header_arrow').style.top = "22px";
 
         let data = this.props.data;
