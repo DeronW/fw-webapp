c4f92dd71d01be228a89248fb74b04b561ecbffe
delong <w.del@qq.com>
2017-03-17 11:42:59 +0800

    update

--- scripts/gen-loan-differential-compile.js
+++ scripts/gen-loan-differential-compile.js
@@ -1,4 +1,4 @@
-import 'fs'
+let fs = require('fs')
 
 // const sourceF = '/tmp/webapp.loan.git.diff'
 const targetF = 'differential.compile.tmp.sh'
@@ -8,5 +8,16 @@ const sourceF = './test.diff'
 
 fs.readFile(sourceF, (err, data) => {
     if (err) throw err;
-    console.log(data);
+    let lines = data.toString().split('\n');
+    let r = {
+        lib: false,
+        pages: {}
+    }
+
+    lines.forEach(line => {
+        if (line.startsWith('lib')) r.lib = true;
+        let m = line.match(/apps\/loan\/([-\w]+)\//);
+        if (m) r.pages[m[0]] = true;
+    })
+    console.log(JSON.stringify(r))
 });
