b9a5c0612f84fbb7ef2c83160b1e6eef3be35894
jiabin.xiong <jiabin.xiong@imduoduo.com>
2017-03-20 16:21:05 +0800

    微商

--- apps/wap/register-success/react/index.jsx
+++ apps/wap/register-success/react/index.jsx
@@ -51,7 +51,6 @@ var Btn = React.createClass({
 
 var PromptBlock = React.createClass({
     render: function () {
-		console.log(this.props.resDetails);
         return (
             <div className="ui-prompt">
                 <div className="img">
@@ -63,7 +62,7 @@ var PromptBlock = React.createClass({
                 </div>
 
 				{
-					this.props.resDetails != '' ?  this.props.resDetails.map((data, index) => {
+					this.props.resDetails.resDetails != '' ?  this.props.resDetails.resDetails.map((data, index) => {
 						<div className="ui-prompt-text">
 							<span className="number-text">{data.giftAmount}</span>
 							data.giftType == 0 ? '元返现券礼包已经转入您的账户中' : '返息券已经转入您的账户中'
@@ -126,7 +125,7 @@ var AccountSucceedBody = React.createClass({
                 <PromptBlock imgUrl={"images/succeed-1.png"} title={"注册成功"}
                     text={"元返现券已经转入您的账户中"}
                     numberText={this.state.registResultData.resvalue}
-                    resDetails={this.state.registResultData.resDetails}
+                    resDetails={this.state.registResultData}
                 />
                 <Btn btnText={"马上开通徽商账户"} Fun={this.clickHandler} />
 
