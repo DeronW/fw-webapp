57e2b3cb4fb93e04a5e79437d82e7a440f4be736
jiabin.xiong <jiabin.xiong@imduoduo.com>
2017-03-20 16:31:01 +0800

    修改

--- apps/wap/register-success/react/index.jsx
+++ apps/wap/register-success/react/index.jsx
@@ -64,15 +64,15 @@ var PromptBlock = React.createClass({
                     {this.props.title}
                 </div>
 
-				/*{
-					this.props.resDetails.resDetails != '' ?  this.props.resDetails.resDetails.map((data, index) => {
+				{
+					/*this.props.resDetails.resDetails != '' ?  this.props.resDetails.resDetails.map((data, index) => {
 						<div className="ui-prompt-text">
 							<span className="number-text">{data.giftAmount}</span>
 							data.giftType == 0 ? '元返现券礼包已经转入您的账户中' : '返息券已经转入您的账户中'
 						</div>
 						
-					}) : null	
-				}*/
+					}) : null	*/
+				}
             </div>
         );
     }
