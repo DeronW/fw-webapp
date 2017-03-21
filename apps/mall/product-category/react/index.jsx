aa00ebbf9f44ce6cdbf6b2a9d164c7b062d5f116
baoxiao <baoxiao@ucfgroup.com>
2017-03-21 19:12:30 +0800

    19:12

--- apps/mall/product-category/react/index.jsx
+++ apps/mall/product-category/react/index.jsx
@@ -4,7 +4,7 @@ const SearchPage = React.createClass({
         let category_link_name = ['workshop', 'fantasy', 'virtualCard', 'diet', 'living', 'automobile', 'outdoor', 'mobileDigital', 'maternalInfantEducation', 'qualityLife', ''];
 
         let category_item = category.map((name, index) => {
-            let category_link = 'http://mmall.9888.cn/static/mall/product-list/index.html?searchSourceType=0&category=' + category_link_name[index] + '&title=' + category[index];
+            let category_link = '/static/mall/product-list/index.html?searchSourceType=0&category=' + category_link_name[index] + '&title=' + category[index];
             return (
                 <a href={category_link} key={index}><img className="category-img"
                                                          src={"images/icon-" + (index + 1) + ".png"}/><span
