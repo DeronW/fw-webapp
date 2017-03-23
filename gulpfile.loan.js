8720a0936b37992cd45c40bb191e373e395b6e9a
liyanzhu <liyanzhu@ucfgroup.com>
2017-03-23 16:47:28 +0800

    Add: register-successfully-page through wx

--- gulpfile.loan.js
+++ gulpfile.loan.js
@@ -10,7 +10,9 @@ let APP_NAMES = [
 
 // 用户模块
 const USER_PAGES = [
-    'fxh-register', // 放心花注册页面
+    'outside-register', // 放心花注册页面
+    'outside-register-success-wx', // 放心花注册页面
+    'outside-register-success-app', // 放心花注册页面
     'user-entry', // 用户入口, 并非登录或注册, 而是用户的进入入口
     'user-register', //注册入口
     'user-login', //登录入口
