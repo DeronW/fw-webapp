ccf013af3247bdd71a90c923b659a26b596b3dea
彭焱 <pengyan@ucfgroup.com>
2017-03-27 18:29:34 +0800

    update

--- apps/loan/fxh-jrgc-invite/react/index.jsx
+++ apps/loan/fxh-jrgc-invite/react/index.jsx
@@ -19,7 +19,7 @@ $FW.DOMReady(function () {
         NativeBridge.share({
             title:'掌上钱包，随用随取' , // 标题
             image: 'https://static.9888.cn/images/loan/invitation.jpg', // 图标
-            link: `https://m.easyloan888.com/static/loan/outside-register/index.html?code=${code}`, // 链接
+            link: `https://m.easyloan888.com/static/loan/outside-register/index.html?channelCode=JRGC&invitationCode=${code}`, // 链接
             desc:'缺钱不用愁，注册放心花，借款神器，急速到账'  // 描述
         });
     });
