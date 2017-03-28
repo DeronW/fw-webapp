95e08a656ca98a6c93b75ade0076ff2a28036cdd
liyanzhu <liyanzhu@ucfgroup.com>
2017-03-28 18:07:03 +0800

    Modify: ui

--- apps/loan/outside-register-success-app/react/index.jsx
+++ apps/loan/outside-register-success-app/react/index.jsx
@@ -43,6 +43,9 @@ class DownloadAppWrap extends React.Component {
         location.href = 'https://itunes.apple.com/cn/app/%E6%94%BE%E5%BF%83%E8%8A%B1-%E6%8E%8C%E4%B8%8A%E5%B0%8F%E9%A2%9D%E7%8E%B0%E9%87%91%E5%80%9F%E6%AC%BE/id1208062782?mt=8';
         break;
       case 'android':
+        if ($FW.inIOS) {
+          return;
+        }
         var channel = $FW.Format.urlQuery().name;
         $FW.Ajax(`${API_PATH}api/v1/download.json?name=${channel}`).then((data) => {
             location.href = data.url;
