4e7fcd3418848adc9217d22bb345a0c35eeb8cbf
baoxiao123 <a928926694@163.com>
2017-04-01 10:51:28 +0800

    10:51

--- apps/mall/user-coupon-list/react/index.jsx
+++ apps/mall/user-coupon-list/react/index.jsx
@@ -314,7 +314,7 @@ const ConfAlert = React.createClass({
 $FW.DOMReady(function () {
     ReactDOM.render(<Header title={"优惠券"} back_handler={back_handler}/>, HEADER_NODE);
     $FW.Ajax({
-        url: `${API_PATH}mall/api/cheap/v1/order_list.json`,//queryAllcheap.json
+        url: `${API_PATH}mall/api/cheap/v1/queryAllcheap.json`,//queryAllcheap.json
         enable_loading: true
     }).then(data => {
         ReactDOM.render(<CouponMain orders={data.orders}/>, CONTENT_NODE);
