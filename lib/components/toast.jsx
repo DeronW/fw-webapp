1bdd04d35fe463471d64f903febb210b88ab5a0c
delong <w.del@qq.com>
2017-04-01 10:40:46 +0800

    fix

--- lib/components/toast.jsx
+++ lib/components/toast.jsx
@@ -29,7 +29,7 @@ const GlobalToast = React.createClass({
             top: "50%",
             left: "50%",
             transform: "translate(-50%,-50%)",
-            webkitTransform: "translate(-50%,-50%)",
+            WebkitTransform: "translate(-50%,-50%)",
             padding: "18px 28px",
             color: "#fff",
             fontSize: "28px",
