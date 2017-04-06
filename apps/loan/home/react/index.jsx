ac49c806332fd2eb4812418eca8c0da37e4acea1
彭焱 <pengyan@ucfgroup.com>
2017-04-06 09:56:44 +0800

    update

--- apps/loan/home/react/index.jsx
+++ apps/loan/home/react/index.jsx
@@ -190,7 +190,7 @@ const ApplyLoan = React.createClass({
         return (
             <div className="apply-loan">
                 {!$FW.Browser.inApp() && <div className="header">放心花</div>}
-                {false && $FW.Browser.inApp() && <div className="ad">
+                {$FW.Browser.inApp() && <div className="ad">
                     <a onClick={()=>gotoHandler("https://m.easyloan888.com/static/loan/fxh-jrgc-invite/index.html")}><img src="images/banner.png"/></a>
                 </div>}
                 <div className={$FW.Browser.inApp()? "app-loan-num":"loan-num"}>
