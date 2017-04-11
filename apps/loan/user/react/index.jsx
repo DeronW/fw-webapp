619fab12b0791e3e98c52f9cac309ba869999c39
liyanzhu <liyanzhu@ucfgroup.com>
2017-04-11 16:13:51 +0800

    Add: user's homepage with avatar card

--- apps/loan/user/react/index.jsx
+++ apps/loan/user/react/index.jsx
@@ -1,163 +1,34 @@
-function phoneMosaic(val) {
-    let frontNum = val.slice(0, 3);
-    let lastNum = val.slice(val.length - 4, val.length);
-
-    if (val == undefined) {
-        return '';
-    } else {
-        return `${frontNum}****${lastNum}`
-    }
-
-}
-
-const MyCnt = React.createClass({
-    getInitialState() {
-        return {
-            overdueCount: 0,
-            loanCount: 0,
-            totalLoanAmout: 0,
-            indexloadpageData: '',
-            baseinfoData: ''
-        }
-    },
-    componentDidMount() {
-        Promise.all([
-            $FW.Ajax({
-                url: `${API_PATH}/api/oriole/v1/indexloadpage.json`,
-                method: "POST",
-                enable_loading: "mini",
-                data: {
-                    token: USER.token,
-                    userGid: USER.gid,
-                    userId: USER.id,
-                    sourceType: SOURCE_TYPE
-                }
-            }),
-            $FW.Ajax({
-                url: `${API_PATH}/api/loan/v1/baseinfo.json`,
-                method: "POST",
-                enable_loading: "mini",
-                data: {
-                    token: USER.token,
-                    userGid: USER.gid,
-                    userId: USER.id,
-                    productId: 1,
-                    sourceType: SOURCE_TYPE
-                }
-            })
-        ]).then(data => {
-            this.setState({
-                indexloadpageData: data[0],
-                baseinfoData: data[1]
-            });
-        })
-    },
-    clickHandler() {
-        let userStatus = this.state.baseinfoData.borrowBtnStatus;
-        if (userStatus == 101) $FW.Component.Toast("设置提现卡申请处理中，请稍等");
-    },
+class AvatarCard extends React.Component {
     render() {
-        let userStatus = this.state.baseinfoData.borrowBtnStatus;
-
-        let creditUrl = () => {
-            if (userStatus == 1) {
-                return "/static/loan/user-card-set/index.html";
-            } else if (userStatus >= 2) {
-                return `/api/credit/v1/creditlist.shtml?sourceType=${SOURCE_TYPE}&token=${USER.token}&userId=${USER.id}`
-            }
-        }
-
-        let bankUrl = () => {
-            if (userStatus == 1) {
-                return "/static/loan/user-card-set/index.html";
-            } else if (userStatus >= 2) {
-                return '/static/loan/user-card-management/index.html'
-            }
-        }
-
-        let title_text = phoneMosaic($FW.Store.get('phone', ''));
-        if($FW.Store.get('phone') == 'phone:not-exist') {
-            title_text = '未实名'
-        }
-
+        let avatarSrc = this.props.src || 'images/avatar_default.png';
+        console.log(this.props.phoneNum);
+        console.log(this.props.phoneNum.match(/\d{4}(?=^\d{3})/));
+        let maskedPhoneNum = this.props.phoneNum.replace(/\d{4}(?=^\d{3})/, /\*\*\*\*/);
         return (
-            <div className="my-cnt">
-                <div className="my-nav">
-                    <span className="text">{title_text}</span>
+            <div className="avator-card">
+                <div className="avatar-container">
+                    <img src={avatarSrc} alt="user avatar"/>
                 </div>
-
-                <div className="my-info">
-                    <div className="my-info-cnt">
-                        <div className="loan-sum-text">
-                            <div className="num-text">
-                                {
-                                    this.state.indexloadpageData.totalLoanAmout == 0 ? "--" : this.state.indexloadpageData.totalLoanAmout
-                                }
-                            </div>
-                            <div className="text">累计借款(元)</div>
-                        </div>
-                        <div className="loan-info">
-                            <div className="info info1">
-                                <span className="num-text">{this.state.indexloadpageData.loanCount}</span>
-                                <span className="text">借款次数</span>
-                            </div>
-                            <div className="info info2">
-                                <span className="num-text">{this.state.indexloadpageData.overdueCount}</span>
-                                <span className="text">逾期次数</span>
-                            </div>
-                            <div className="vertical-line"></div>
-                        </div>
-                    </div>
-
+                <div className="masked-phone-num">
+                    {maskedPhoneNum}
                 </div>
-
-                <div className="my-settings">
-                    <div className="list">
-                        <div className="list-cnt" onClick={this.clickHandler}>
-                            <a href={creditUrl()}>
-                                <span className="icon credit-icon"></span>
-                                <span className="text">信用额度</span>
-                                <span className="arrow-r-icon"></span>
-                            </a>
-                        </div>
-                        <div className="list-cnt" onClick={this.clickHandler}>
-                            <a href={bankUrl()}>
-                                <span className="icon back-icon"></span>
-                                <span className="text">银行卡</span>
-                                <span className="arrow-r-icon"></span>
-                            </a>
-                        </div>
-                        <div className="list-cnt">
-                            <a href="/static/loan/weixin-invite/index.html">
-                                <span className="icon invite-icon"></span>
-                                <span className="text">邀请好友</span>
-                                <span className="arrow-r-icon"></span>
-                            </a>
-                        </div>
-                        <div className="list-cnt">
-                            <a href="/static/loan/user-settings/index.html">
-                                <span className="icon more-icon"></span>
-                                <span className="text">更多</span>
-                                <span className="arrow-r-icon"></span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
             </div>
-
-
         )
     }
-});
+}
 
-const USER = $FW.Store.getUserDict();
+class UserInfoWrap extends React.Component {
+    render() {
+        return (
+            <div className="user-info-wrap">
+                <AvatarCard phoneNum="18900001234"/>
+            </div>
+        )
+    }
+}
 
+// render ReactDom
 $FW.DOMReady(() => {
-    ReactDOM.render(<MyCnt />, CONTENT_NODE);
-    ReactDOM.render(<BottomNavBar index={3} />, BOTTOM_NAV_NODE);
+    ReactDOM.render(
+        <UserInfoWrap/>, CONTENT_NODE)
 })
