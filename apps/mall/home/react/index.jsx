54d5b32cc05ec188da9c94c2ca6863b370cba9a5
LiuTaotao123 <lttcz0604@163.com>
2017-04-14 10:34:18 +0800

    支付宝支付

--- apps/mall/home/react/index.jsx
+++ apps/mall/home/react/index.jsx
@@ -11,7 +11,7 @@ function productLink(bizNo) {
 }
 
 const Mall = React.createClass({
-    getInitialState: function () {
+    getInitialState: function() {
         return {
             background: "transparent",
             logoImage: "images/logo.png",
@@ -19,7 +19,7 @@ const Mall = React.createClass({
             borderBottom: "none"
         }
     },
-    getHeadImages: function () {
+    getHeadImages: function() {
         var images = [];
         var bs = this.props.banners;
         for (var i = 0; i < bs.length; i++) {
@@ -27,7 +27,7 @@ const Mall = React.createClass({
         }
         return images;
     },
-    onImageClickHandler: function (index) {
+    onImageClickHandler: function(index) {
         var link = null;
         var bs = this.props.banners;
         for (var i = 0; i < bs.length; i++) {
@@ -35,8 +35,8 @@ const Mall = React.createClass({
         }
         link && gotoHandler(link)
     },
-    componentDidMount: function () {
-        window.addEventListener('scroll', function () {
+    componentDidMount: function() {
+        window.addEventListener('scroll', function() {
             var scrollTop = document.documentElement.scrollTop + document.body.scrollTop;
 
             let style = scrollTop > 400 ? {
@@ -44,24 +44,25 @@ const Mall = React.createClass({
                 logoImage: "images/m-logo.png",
                 avatarImage: "images/m-list-icon.png"
             } : {
-                    background: "transparent",
-                    logoImage: "images/logo.png",
-                    avatarImage: "images/list-icon.png"
-                }
+                background: "transparent",
+                logoImage: "images/logo.png",
+                avatarImage: "images/list-icon.png"
+            }
 
             this.setState(style);
         }.bind(this), false);
     },
-    render: function () {
+    render: function() {
         let banner;
         let iOSApp = $FW.Browser.inApp() && $FW.Browser.inIOS();
 
         if (this.props.banners.length) {
-            banner = <BannerGroup className={iOSApp ? "head-images head-images-ios" : "head-images"}
-                images={this.getHeadImages()}
-                onImageClick={this.onImageClickHandler} />
+            banner = < BannerGroup className = { iOSApp ? "head-images head-images-ios" : "head-images" }
+            images = { this.getHeadImages() }
+            onImageClick = { this.onImageClickHandler }
+            />
         } else {
-            banner = <div className="no-banner"></div>
+            banner = < div className = "no-banner" > < /div>
         }
 
         var head_nav_wrap = {
@@ -72,65 +73,116 @@ const Mall = React.createClass({
             paddingBottom: "20px",
             transition: "1s all"
         };
-        let Charge_Nav = <div className="charge-nav">
-            <div className="charge-bill"><img src="images/charge-bill.png" /></div>
-            <div className="charge-flow"><img src="images/charge-flow.png" /></div>
-        </div>
+        let Charge_Nav = < div className = "charge-nav" >
+            <
+            div className = "charge-bill" > < img src = "images/charge-bill.png" / > < /div> <
+        div className = "charge-flow" > < img src = "images/charge-flow.png" / > < /div> < /
+            div >
 
-        return (
-            <div className="head-wrap">
-                {banner}
-                <div className={iOSApp ? "head-items head-images-ios" : "head-items"}>
-                    <div style={head_nav_wrap} className="head_nav_wrap">
-                        <img className="m-logo" src={this.state.logoImage} />
-                        <a href="/static/mall/product-list/index.html?searchSourceType=2"
-                            className="search-bar-a">
-                            <img className="search-icon" src="images/search-icon.png" />
-                            <div className="search-bar">请您输入关键字。。。</div>
-                        </a>
-                        <a className="index-avatar" href="/static/mall/user/index.html">
-                            <img src={this.state.avatarImage} /></a>
-                    </div>
-                </div>
-                <div className="head-nav">
-                    <a href="/static/mall/product-vip-zone/index.html">
-                        <img src="images/nav-1.png" /><span>VIP专区</span></a>
-                    <a href="/static/mall/product-list/index.html?searchSourceType=0&category=fantasy&title=2">
-                        <img src="images/nav-2.png" /><span>豆哥周边</span></a>
-                    <a href="/static/mall/product-list/index.html?searchSourceType=0&category=workshop&title=3">
-                        <img src="images/nav-3.png" /><span>工场券</span></a>
-                    <a href="/static/mall/product-hot-activity/index.html">
-                        <img src="images/nav-4.png" /><span>热门活动</span></a>
-                    <a href="/static/mall/product-recharge/index.html?tab=1">
-                        <img src="images/nav-5.png" /><span>充话费</span></a>
-                    <a href="/static/mall/product-recharge/index.html?tab=2">
-                        <img src="images/nav-6.png" /><span>充流量</span>
-                        <span className="hot-tag"></span></a>
-                    <a href="/static/mall/game/index.html?mallHead=true">{/*static/mall/waiting/index.html*/}
-                        <img src="images/nav-7.png" /><span>游戏中心</span></a>
+            return ( <
+                div className = "head-wrap" > { banner } <
+                div className = { iOSApp ? "head-items head-images-ios" : "head-items" } >
+                <
+                div style = { head_nav_wrap }
+                className = "head_nav_wrap" >
+                <
+                img className = "m-logo"
+                src = { this.state.logoImage }
+                /> <
+                a href = "/static/mall/product-list/index.html?searchSourceType=2"
+                className = "search-bar-a" >
+                <
+                img className = "search-icon"
+                src = "images/search-icon.png" / >
+                <
+                div className = "search-bar" > 请您输入关键字。。。 < /div> < /
+                a > <
+                a className = "index-avatar"
+                href = "/static/mall/user/index.html" >
+                <
+                img src = { this.state.avatarImage }
+                /></a >
+                <
+                /div> < /
+                div > <
+                div className = "head-nav" >
+                <
+                a href = "/static/mall/product-vip-zone/index.html" >
+                <
+                img src = "images/nav-1.png" / > < span > VIP专区 < /span></a >
+                <
+                a href = "/static/mall/product-list/index.html?searchSourceType=0&category=fantasy&title=2" >
+                <
+                img src = "images/nav-2.png" / > < span > 豆哥周边 < /span></a >
+                <
+                a href = "/static/mall/product-list/index.html?searchSourceType=0&category=workshop&title=3" >
+                <
+                img src = "images/nav-3.png" / > < span > 工场券 < /span></a >
+                <
+                a href = "/static/mall/product-hot-activity/index.html" >
+                <
+                img src = "images/nav-4.png" / > < span > 热门活动 < /span></a >
+                <
+                a href = "/static/mall/product-recharge/index.html?tab=1" >
+                <
+                img src = "images/nav-5.png" / > < span > 充话费 < /span></a >
+                <
+                a href = "/static/mall/product-recharge/index.html?tab=2" >
+                <
+                img src = "images/nav-6.png" / > < span > 充流量 < /span> <
+                span className = "hot-tag" > < /span></a >
+                <
+                a href = "/static/mall/game/index.html?mallHead=true" > { /*static/mall/waiting/index.html*/ } <
+                img src = "images/nav-7.png" / > < span > 游戏中心 < /span></a >
 
-                    <a href="/static/mall/product-list/index.html?searchSourceType=1">
-                        <img src="images/nav-8.png" /><span>我可兑换</span></a>
-                </div>
-                <HotProducts bizNo={'TJ0000022'} count={10} />
-                <NewProducts bizNo={'TJ0000060'} count={10} />
-                <Grid_1 bizNo={'TJ0000042'} count={10} />
-                <Grid_2 bizNo={'TJ0000044'} count={10} />
-                <Grid_3 bizNo={'TJ0000046'} count={10} />
-                <Grid_7 bizNo={'TJ0000054'} count={10} />
-                <Grid_6 bizNo={'TJ0000052'} count={10} />
-                <Grid_5 bizNo={'TJ0000050'} count={10} />
-                <Grid_4 bizNo={'TJ0000048'} count={10} />
-                <Grid_8 bizNo={'TJ0000056'} count={10} />
+                <
+                a href = "/static/mall/product-list/index.html?searchSourceType=1" >
+                <
+                img src = "images/nav-8.png" / > < span > 我可兑换 < /span></a >
+                <
+                /div> <
+                HotProducts bizNo = { 'TJ0000022' }
+                count = { 10 }
+                /> <
+                NewProducts bizNo = { 'TJ0000060' }
+                count = { 10 }
+                /> <
+                Grid_1 bizNo = { 'TJ0000042' }
+                count = { 10 }
+                /> <
+                Grid_2 bizNo = { 'TJ0000044' }
+                count = { 10 }
+                /> <
+                Grid_3 bizNo = { 'TJ0000046' }
+                count = { 10 }
+                /> <
+                Grid_7 bizNo = { 'TJ0000054' }
+                count = { 10 }
+                /> <
+                Grid_6 bizNo = { 'TJ0000052' }
+                count = { 10 }
+                /> <
+                Grid_5 bizNo = { 'TJ0000050' }
+                count = { 10 }
+                /> <
+                Grid_4 bizNo = { 'TJ0000048' }
+                count = { 10 }
+                /> <
+                Grid_8 bizNo = { 'TJ0000056' }
+                count = { 10 }
+                />
 
-                <HotSales />
-            </div>
-        )
+                <
+                HotSales / >
+                <
+                /div>
+            )
     }
 });
 
-$FW.DOMReady(function () {
-    ReactDOM.render(<BottomNavBar />, BOTTOM_NAV_NODE);
-    $FW.Ajax(`${API_PATH}/mall/api/index/v1/banners.json`)
-        .then(data => ReactDOM.render(<Mall banners={data.banners} />, CONTENT_NODE));
-});
+$FW.DOMReady(function() {
+            ReactDOM.render( < BottomNavBar / > , BOTTOM_NAV_NODE);
+            $FW.Ajax(`${API_PATH}/mall/api/index/v1/banners.json`)
+                .then(data => ReactDOM.render( < Mall banners = { data.banners }
+                    />, CONTENT_NODE));
+                });
