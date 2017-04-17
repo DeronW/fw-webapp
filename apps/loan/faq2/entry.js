f732e22813ff6a0ccf67aa8df5081465e0fe2401
delong <w.del@qq.com>
2017-04-17 09:53:07 +0800

    fix

--- apps/loan/faq2/entry.js
+++ apps/loan/faq2/entry.js
@@ -1,26 +1,9 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-
-// import RaisedButton from 'material-ui/RaisedButton';
-
-import injectTapEventPlugin from 'react-tap-event-plugin';
-
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
-
-import { List, ListItem } from 'material-ui/List';
-import ActionGrade from 'material-ui/svg-icons/action/grade';
-import ContentInbox from 'material-ui/svg-icons/content/inbox';
-import ContentDrafts from 'material-ui/svg-icons/content/drafts';
-import ContentSend from 'material-ui/svg-icons/content/send';
-import Subheader from 'material-ui/Subheader';
-import Toggle from 'material-ui/Toggle';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {observer} from "mobx-react";
 
 import { QUESTIONS } from './components/questions'
+
 
 const STYLE = {
     question: {
@@ -36,27 +19,8 @@ const STYLE = {
     }
 }
 
-class FAQ extends React.Component {
-
-    render() {
-
-        let list_item = (item, index) => {
-            return <ListItem style={STYLE.question} key={index} primaryText={item.q}
-                primaryTogglesNestedList={true}
-                nestedItems={[
-                    <ListItem key={1} style={STYLE.answer} primaryText={item.a} />
-                ]} />
-        }
-
-        return (
-            <MuiThemeProvider>
-                <List>
-                    <Subheader style={{ fontSize: 32 }}>FAQ</Subheader>
-                    {QUESTIONS.map(list_item)}
-                </List>
-            </MuiThemeProvider>
-        );
-    }
+class ToggleList {
+    @observable open_list = []
 }
 
 ReactDOM.render(<FAQ />, document.getElementById('app'));
