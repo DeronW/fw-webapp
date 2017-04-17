012f1bf98c0fa6094b6b68f2a5c109047e62b167
彭焱 <pengyan@ucfgroup.com>
2017-04-17 15:56:49 +0800

    update

--- apps/loan/weixin-download/react/index.jsx
+++ apps/loan/weixin-download/react/index.jsx
@@ -19,7 +19,7 @@ $FW.DOMReady(function () {
 
     if (ConcertUtilBrowser.versions.weixin) {
         $("ios-download").addEventListener("click", function () {
-            $("ios-download").setAttribute("href","http://a.app.qq.com/o/simple.jsp?pkgname=com.ucf.jrgc.cfinance");
+            $("ios-download").setAttribute("href","https://itunes.apple.com/cn/app/%E6%94%BE%E5%BF%83%E8%8A%B1-%E6%8E%8C%E4%B8%8A%E5%B0%8F%E9%A2%9D%E7%8E%B0%E9%87%91%E5%80%9F%E6%AC%BE/id1208062782?mt=8");
         })
         $("android-download").addEventListener("click", function () {
             $("android-download").setAttribute("href","http://a.app.qq.com/o/simple.jsp?pkgname=com.ucf.jrgc.cfinance");
