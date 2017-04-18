f98790672839d301976bedcae01bc251c85d50a5
彭焱 <pengyan@ucfgroup.com>
2017-04-18 16:32:38 +0800

    update

--- apps/loan/apply-du-miao/react/index.jsx
+++ apps/loan/apply-du-miao/react/index.jsx
@@ -8,10 +8,20 @@ const TITLE = [
 
 const INFO = [
     [
-        '贷款机构收取的贷款服务费,按月收取。',
-        '贷款机构收取的贷款服务费,按月收取贷款机构收取的贷款服务费,按月收取取。',
-        '贷款机构收取的,按月收取。'
+        '中国大陆公民，年龄22-55周岁；手机号实名认证且在网时间超过6个月；有正常使用的信用卡；芝麻信用分600分以上。'
     ],
+    [
+        '用户基本信息、信用卡、手机号、芝麻信用分。'
+    ],
+    [
+        '手机运营商、银行卡、芝麻信用分。'
+    ],
+    [
+        '纯线上审核。'
+    ],
+    [
+        '用户可选择自动扣款，也可主动还款。'
+    ]
 ]
 
 class BorrowMoneyDatailList extends React.Component {
@@ -32,16 +42,55 @@ class BorrowMoneyDatailList extends React.Component {
 }
 
 class BorrowMoney extends React.Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
-            product: {}
+            product: {},
+            borrowStatus:'',
+            ableEnter:'',
+            detailPopShow:false,
+            dumiaoEnterPopShow:false,
+            tryOtherLoanPopShow:false,
+            tryOtherLoanMsg:''
         }
+        this.imgClickHandler = this.imgClickHandler.bind(this);
+        this.detailClickHandler = this.detailClickHandler.bind(this);
+        this.dumiaoCloseHandler = this.dumiaoCloseHandler.bind(this);
+        this.tryOtherLoanCloseHandler = this.tryOtherLoanCloseHandler.bind(this);
     }
     componentDidMount = () => {
         let pid = $FW.Format.urlQuery().id;
         $FXH.Post(`${API_PATH}/api/product/v1/productDetail.json?productId=${pid}`)
-            .then(data => this.setState({ product: data }))
+            .then(data => this.setState({ product: data }));
+        $FXH.Post(`${API_PATH}/api/loan/v1/baseinfo.json`)
+            .then(data=>this.setState({borrowStatus:data.borrowBtnStatus}));
+        $FXH.Post(`${API_PATH}/api/loan/v1/dmStatus.json`)
+            .then(data=>this.setState({ableEnter:data.code,tryOtherLoanMsg:data.message}, err=>this.setState({ableEnter:data,code, tryOtherLoanMsg:data.message})));
+    }
+    clickHandler(){
+        let borrowStatus = this.state.borrowStatus;
+        let ableEnter = this.state.ableEnter;
+        if(borrowStatus == 1 || borrowStatus == 101){
+            location.href = '/static/loan/user-card-set/index.html';
+        }else if(borrowStatus == 2 || borrowStatus == 3 || borrowStatus == 5 || ableEnter == 10000){
+            location.href = '/static/loan/apply-du-miao/index.html';
+        }else if(ableEnter == 1001002){
+             this.setState({dumiaoEnterPopShow:true});
+        }else{
+            this.setState({tryOtherLoanPopShow:true});
+        }
+    }
+    imgClickHandler(){
+        this.setState({detailPopShow:true});
+    }
+    detailClickHandler(){
+        this.setState({detailPopShow:false})
+    }
+    dumiaoCloseHandler(){
+        this.setState({dumiaoEnterPopShow:false})
+    }
+    tryOtherLoanCloseHandler(){
+        this.setState({tryOtherLoanPopShow:false})
     }
     render() {
         return (
@@ -52,7 +101,7 @@ class BorrowMoney extends React.Component {
                         <div className="info">
                             <div className="t">
                                 <span className="title-text">读秒</span>
-                                <div className="text"> 借款范围（500 - 10万） </div>
+                                <div className="text"> 借款范围（{this.state.product.amountStr}） </div>
                             </div>
 
                             <div className="b">
@@ -67,37 +116,69 @@ class BorrowMoney extends React.Component {
 
                     <div className="borrow-money-detail-data">
                         <div className="list">
-                            <div className="name-text">6.25%</div>
+                            <div className="name-text">{this.state.product.monthRateStr}</div>
                             <div className="data-text">
                                 每月费用
-							<img src="images/icon-6.png" />
+							<img src="images/icon-6.png" onClick={this.imgClickHandler}/>
                             </div>
                         </div>
                         <div className="list">
-                            <div className="name-text">5~28天/个月</div>
+                            <div className="name-text">{this.state.product.termRangeStr}个月</div>
                             <div className="data-text">
                                 期限范围
 						</div>
                         </div>
                         <div className="list">
-                            <div className="name-text">56秒/分钟/天</div>
+                            <div className="name-text">{this.state.product.fastLoanValue}日</div>
                             <div className="data-text">
                                 最快放款
 						</div>
                         </div>
                     </div>
                 </div>
-
                 <BorrowMoneyDatailList product={this.props.product} />
-
                 <div className="footer">
-                    <Nav className="btn" href="/static/loan/apply-borrow-money/index.html">马上拿钱</Nav>
+                    <Nav className="btn" onClick={this.clickHandler}>马上拿钱</Nav>
                 </div>
+                {this.state.detailPopShow  && <div className="mask" style={{zIndex:100}}>
+                    <div className="detail-pop">
+                        <div className="pop-title">费用说明</div>
+                        <div className="pop-close" onClick={this.detailClickHandler}></div>
+                        <div className="pop-item">
+                            <div className="pop-item-wrap"><span className="pop-item-name">月利率</span><span className="pop-item-num">{this.state.product.loanRateStr}</span></div>
+                            <div className="pop-item-desc">贷款机构收取的贷款利率,按月收取</div>
+                        </div>
+                        <div className="pop-item">
+                            <div className="pop-item-wrap"><span className="pop-item-name">月服务利率</span><span className="pop-item-num">{this.state.product.serviceRateStr}</span></div>
+                            <div className="pop-item-desc">贷款机构收取的贷款服务费,按月收取</div>
+                        </div>
+                        <div className="pop-item">
+                            <div className="pop-item-wrap"><span className="pop-item-name">一次性手续费</span><span className="pop-item-num">{this.state.product.commRateStr}</span></div>
+                            <div className="pop-item-desc">贷款机构收取的一次性费用</div>
+                        </div>
+                        <a className="know-btn" onClick={this.detailClickHandler}>知道了</a>
+                    </div>
+                </div>}
+                {this.state.dumiaoEnterPopShow && <div className="mask" style={{zIndex:100}}>
+                    <div className="detail-pop">
+                        <div className="pop-close" onClick={this.dumiaoCloseHandler}></div>
+                        <div className="pop-tip">有一笔借款正在申请中</div>
+                        <a className="know-btn" href={`api/order/v1/jump.shtml?sourceType=${SOURCE_TYPE}&token=${USER.token}&uid=${USER.uid}&userGid=${USER.gid}&userId=${USER.id}`}>进入读秒查看</a>
+                    </div>
+                </div>}
+                {this.state.tryOtherLoanPopShow && <div className="mask" style={{zIndex:100}}>
+                    <div className="detail-pop">
+                        <div className="pop-close" onClick={this.tryOtherLoanCloseHandler}></div>
+                        <div className="pop-tip">{this.state.tryOtherLoanMsg}</div>
+                        <a className="know-btn">尝试其他借款</a>
+                    </div>
+                </div>}
             </div>
         )
     }
 }
-
+const USER = $FW.Store.getUserDict();
 $FW.DOMReady(() => {
+    ReactDOM.render(<Header title={"读秒"} />, HEADER_NODE);
     ReactDOM.render(<BorrowMoney />, CONTENT_NODE)
 })
