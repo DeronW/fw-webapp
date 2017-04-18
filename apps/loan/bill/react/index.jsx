f5b1203e1a986669a1ef542798f71dba0037b907
delong <w.del@qq.com>
2017-04-18 16:01:29 +0800

    update

--- apps/loan/bill/react/index.jsx
+++ apps/loan/bill/react/index.jsx
@@ -1,39 +1,32 @@
 class Content extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
             current_type: '1',
-            tab_list: [
-                { type: '1', name: '申请中', page_no: 1 },
-                { type: '2', name: '还款中', page_no: 1 },
-                { type: '3', name: '未通过', page_no: 1 },
-                { type: '4', name: '已还款', page_no: 1 }
-            ],
-            order_list: { '1': [], '2': [], '3': [], '4': [] }
+            tab: {
+                '1': { name: '申请中', page_no: 1, order_list: [] },
+                '2': { name: '还款中', page_no: 1, order_list: [] },
+                '3': { name: '未通过', page_no: 1, order_list: [] },
+                '4': { name: '已还款', page_no: 1, order_list: [] }
+            }
         }
+    }
 
-    }
-    get_current_tab = () => {
-        let { tab_list, current_type } = this.state;
-        return tab_list.find(i => i.type === current_type) || {}
-    }
     loadMoreHandler = (done) => {
-        let { order_list, current_type, tab_list } = this.state;
-        let current_tab = this.get_current_tab()
-        if (current_tab.page_no === 0) return;
+        let { current_type, tab } = this.state, current_tab = tab[current_type];
+        if (current_tab.page_no === 0) return done && done();
 
         $FXH.Post(`${API_PATH}/api/order/v1/orderList.json`, {
             pageSize: 10,
             pageIndex: current_tab.page_no,
             loanStatus: current_type
         }).then(data => {
-            order_list[current_type].push(...data.resultList)
-            this.setState({ order_list: order_list })
-            let ind = tab_list.findIndex(i => i.type === current_type)
+            tab[current_type].order_list.push(...data.resultList)
             current_tab.page_no < data.totalPage ?
-                tab_list[ind].page_no++ :
-                tab_list[ind].page_no = 0;
-            this.setState({ tab_list: tab_list })
+                tab[current_type].page_no++ :
+                tab[current_type].page_no = 0;
+            this.setState({ tab: tab });
+
             done && done();
         })
     }
@@ -45,17 +38,17 @@ class Content extends React.Component {
         this.setState({ current_type: type }, this.loadMoreHandler);
     }
     render() {
-        let { current_type, tab_list, order_list } = this.state;
+        let { current_type, tab } = this.state;
 
-        let tab = (tab, index) => {
-            let cn = tab.type === current_type ? "ui-tab-li ui-select-li" : "ui-tab-li";
-            return <Nav key={index} className={cn} onClick={() => { this.switchTabHandler(tab.type); }}>
-                <span className="text">{tab.name}</span>
+        let btn_tab = (type, index) => {
+            let cn = `ui-tab-li ${type === current_type && 'ui-select-li'}`
+            return <Nav key={index} className={cn}
+                onClick={() => this.switchTabHandler(type)}>
+                <span className="text">{tab[current_type].name}</span>
             </Nav>
         }
 
         let order_item = (order, index) => {
-            let statusColor = `bill-${current_type}-color`;
             let link = order.productId == 1 ?
                 `/static/loan/bill-detail/index.html?uuid=${order.loanGid}` : `/static/loan/bill-detail-dumiao/index.html?uuid=${order.uuid}`;
             let logo = order.productId == 1 ? "images/fxh-logo.png" : "images/dumiao-logo.png";
@@ -68,23 +61,23 @@ class Content extends React.Component {
                 </div>
                 <div className="apply-status-wrap">
                     <div className="apply-status">
-                        <span className={statusColor}>{this.get_current_tab().name}</span></div>
+                        <span className={`bill-${current_type}-color`}>
+                            {tab[current_type].name}</span></div>
                     <div className="apply-time">{order.loanTime}</div>
                 </div>
             </Nav>
         }
 
         let empty = <span className="no-data"></span>
+        let current_tab = this.state.tab[this.state.current_type]
 
-        return (
-            <div className="billContent">
-                <div className="bill-header"> {tab_list.map(tab)} </div>
-                <div className="billContainer">
-                    {order_list[current_type].map(order_item)}
-                    {order_list[current_type].length === 0 && empty}
-                </div>
+        return <div className="billContent">
+            <div className="bill-header"> {['1', '2', '3', '4'].map(btn_tab)} </div>
+            <div className="billContainer">
+                {current_tab.order_list.map(order_item)}
+                {current_tab.order_list.length === 0 && empty}
             </div>
-        )
+        </div>
     }
 }
 
