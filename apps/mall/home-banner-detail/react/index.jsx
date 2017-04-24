ebe276a45f1eda9ab140718224ad5d6031198310
LiuTaotao123 <lttcz0604@163.com>
2017-04-24 13:23:51 +0800

    home-banner-detail

--- apps/mall/home-banner-detail/react/index.jsx
+++ apps/mall/home-banner-detail/react/index.jsx
@@ -1,19 +1,34 @@
 const BannerDetail = React.createClass({
-    render: function () {
-    	let iOSApp = $FW.Browser.inApp() && $FW.Browser.inIOS();
-    	return <div>
-			<div className={iOSApp ? "head-images head-images-ios" : "head-images"}>
-        		<img src="images/education-theme-img.png"/>
-        	</div>
-        <p>vheivhidvhdhvid</p>
-    	</div>
-        
-    }
+	render: function () {
+		let iOSApp = $FW.Browser.inApp() && $FW.Browser.inIOS();
+		return (<div>
+					<div className={iOSApp ? "head-images head-images-ios" : "head-images"}>
+						<img src="https://mmall.9888.cn/images/banner_20170412/1491965487785_.jpg"/>
+					</div>
+					<div  className="display">
+					<div>
+					<h2>
+					关于谎称豆哥商城供货商电话诈骗公告
+					</h2>
+					</div>
+
+					<p>近期有个别客户反映，接到电话自称豆哥商城供货商，谎称商品质量有问题，主动给客户退钱退货事宜。请大家不要轻易相信，此类多是诈骗电话。</p>
+					<p>豆哥保证豆哥商城全场正品行货，如有质量问题，退换货请主动联系豆哥商城官方客服人员。</p>
+					<p>请大家提高警惕，谨防上当！</p>
+					<div className="date">
+						<span>豆哥商城</span>
+						<p>2017年4月10日</p>
+					</div>
+					</div>
+
+				</div>)
+
+	}
 });
 
 
 $FW.DOMReady(function () {
-			ReactDOM.render(<Header title={"公告"}/>, HEADER_NODE);
-            ReactDOM.render(<BannerDetail />, CONTENT_NODE);
+	ReactDOM.render(<Header title={"公告"}/>, HEADER_NODE);
+	ReactDOM.render(<BannerDetail />, CONTENT_NODE);
 });
 
