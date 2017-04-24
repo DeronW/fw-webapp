1db10088497df5f9e3df0806a8eb914bdc026a12
LiuTaotao123 <lttcz0604@163.com>
2017-04-24 15:58:14 +0800

    home banner

--- apps/mall/home-banner-detail/react/index.jsx
+++ apps/mall/home-banner-detail/react/index.jsx
@@ -9,16 +9,15 @@ const BannerDetail = React.createClass({
 					<div  className="display">
 					<div>
 					<h2>
-					关于谎称豆哥商城供货商电话诈骗公告
+					豆哥商城防诈骗公告
 					</h2>
 					</div>
 
 					<p className="notice">近期有个别客户反映，接到电话自称豆哥商城供货商，谎称商品质量有问题，主动给客户退钱退货事宜。请大家不要轻易相信，此类多是诈骗电话。</p>
 					<p className="notice">豆哥保证豆哥商城全场正品行货，如有质量问题，退换货请主动联系豆哥商城官方客服人员。</p>
 					<p className="notice">请大家提高警惕，谨防上当！</p>
-					
-						<p className="signature">豆哥商城</p>
-						<p className="last-time">2017年4月10日</p>
+					<p className="signature">豆哥商城</p>
+					<p className="last-time">2017年4月10日</p>
 					
 					</div>
 
