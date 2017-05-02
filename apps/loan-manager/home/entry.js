4a70e5cbb1cf600cfdd0bfc7d20410ab0a590a8f
delong <w.del@qq.com>
2017-05-02 15:48:49 +0800

    update

--- apps/loan-manager/home/entry.js
+++ apps/loan-manager/home/entry.js
@@ -8,6 +8,10 @@ class Todo {
     id = Math.random();
     @observable title = "";
     @observable finished = false;
+
+    constructor(title){
+        this.title = title
+    }
 }
 
 class TodoList {
