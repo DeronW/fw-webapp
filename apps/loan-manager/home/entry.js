75475c62c52d22adcb280a1b30a3bde5f87f5226
彭焱 <pengyan@ucfgroup.com>
2017-05-02 16:11:22 +0800

    update

--- apps/loan-manager/home/entry.js
+++ apps/loan-manager/home/entry.js
@@ -1,13 +1,16 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
+// import ReactDOM from 'react-dom'
+// import React from 'react'
 
-import * as $FW from '../../../es7-lib/javascripts'
+//import * as $FW from '../../../es7-lib/javascripts'
 
-$FW.DOMReady(() => {
-    $FW.Request('http://fe.9888.cn/fake-api/api/fail.json').then(
-        d => console.log(d),
-        e => console.log('self handler: ', e.message)
-    )
-})
+import Browser from '../../../es7-lib/javascripts/core/browser'
+console.log(Browser.inMobile);
+
+// $FW.DOMReady(() => {
+//     $FW.Request('http://fe.9888.cn/fake-api/api/fail.json').then(
+//         d => console.log(d),
+//         e => console.log('self handler: ', e.message)
+//     )
+// })
 
 // import css from './less/index.less'
