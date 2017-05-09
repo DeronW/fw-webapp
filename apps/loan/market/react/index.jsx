5b064d7cc264ea2604bf0fe7e12e2ffd94029120
彭焱 <pengyan@ucfgroup.com>
2017-05-09 10:37:13 +0800

    update

--- apps/loan/market/react/index.jsx
+++ apps/loan/market/react/index.jsx
@@ -2,7 +2,7 @@ function gotoHandler(link, need_login) {
     if (link.indexOf('://') < 0) {
         link = location.protocol + '//' + location.hostname + link;
     }
-    if ($FW.Browser.inApp()) {
+    if ($FW.Browser.inFXHApp()) {
         NativeBridge.goto(link, need_login)
     } else {
         location.href = encodeURI(link);
