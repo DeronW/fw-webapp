76d1ae405b69adb2ffcb1f7c778b46639fb8fdb4
delong <w.del@qq.com>
2017-05-10 15:23:46 +0800

    fix

--- apps/loan/dumiao/react/index.jsx
+++ apps/loan/dumiao/react/index.jsx
@@ -57,164 +57,180 @@ class BorrowMoney extends React.Component {
         super(props)
         this.state = {
             product: {},
-            borrowStatus:'',
-            ableEnter:'',
-            detailPopShow:false,
-            dumiaoEnterPopShow:false,
-            tryOtherLoanPopShow:false,
-            canMessageShow:false,
-            tryOtherLoanMsg:'',
-            canStatus:'',
-            canMessage:'',
-            loanUuid:null
+            borrowStatus: '',
+            ableEnter: '',
+            detailPopShow: false,
+            dumiaoEnterPopShow: false,
+            tryOtherLoanPopShow: false,
+            canMessageShow: false,
+            tryOtherLoanMsg: '',
+            canStatus: '',
+            canMessage: '',
+            loanUuid: null
         }
-        this.clickHandler = this.clickHandler.bind(this);
-        this.imgClickHandler = this.imgClickHandler.bind(this);
-        this.detailClickHandler = this.detailClickHandler.bind(this);
-        this.dumiaoCloseHandler = this.dumiaoCloseHandler.bind(this);
-        this.canMessageCloseHandler = this.canMessageCloseHandler.bind(this);
-        this.tryOtherLoanCloseHandler = this.tryOtherLoanCloseHandler.bind(this);
     }
     componentDidMount = () => {
         let pid = $FW.Format.urlQuery().pid;
+
         $FXH.Post(`${API_PATH}/api/product/v1/productDetail.json?productId=${pid}`)
-            .then(data => this.setState({ product: data}));
-        $FXH.Post(`${API_PATH}/api/loan/v1/baseinfo.json`,{
-            productId:pid
-        }).then(data=>this.setState({borrowStatus:data.borrowBtnStatus}));
+            .then(data => this.setState({ product: data }));
+
+        $FXH.Post(`${API_PATH}/api/loan/v1/baseinfo.json`, {
+            productId: pid
+        }).then(data => this.setState({ borrowStatus: data.borrowBtnStatus }));
+
         $FXH.Post(`${API_PATH}/api/loan/v1/dmStatus.json`)
-            .then(data=>{
-                if(data.canStatus == 0){
-                    this.setState({canStatus:0,canMessage:data.canMessage,loanUuid:data.loanUuid});
-                }else if(data.canStatus == 1){
-                    this.setState({canStatus:1,loanUuid:data.loanUuid,canMessage:data.canMessage});
-                }else if(data.canStatus == 2){
-                    this.setState({canStatus:2});
+            .then(data => {
+                if (data.canStatus == 0) {
+                    this.setState({
+                        canStatus: 0, canMessage: data.canMessage, loanUuid: data.loanUuid
+                    });
+                } else if (data.canStatus == 1) {
+                    this.setState({
+                        canStatus: 1, loanUuid: data.loanUuid, canMessage: data.canMessage
+                    });
+                } else if (data.canStatus == 2) {
+                    this.setState({ canStatus: 2 });
                 }
-            }, err=>{
-                this.setState({ableEnter:err.code, tryOtherLoanMsg:err.message})
-            }
-         );
+            }, err => {
+                this.setState({ ableEnter: err.code, tryOtherLoanMsg: err.message })
+            });
     }
-    clickHandler(){
-        let borrowStatus = this.state.borrowStatus;
-        let canStatus = this.state.canStatus;
-        if(borrowStatus == 1 || borrowStatus == 101){
+    clickHandler = () => {
+        let { canStatus, borrowStatus } = this.state;
+
+        // 初始化数据没有完成, 稍后再试
+        if (canStatus === null) return;
+
+        if (borrowStatus == 1 || borrowStatus == 101) {
             location.href = '/static/loan/user-card-set/index.html';
-        }else if(canStatus == 2){
+        } else if (canStatus == 2) {
             location.href = '/static/loan/dumiao-put-in/index.html?pid=' + $FW.Format.urlQuery().pid;
-        }else if(canStatus == 0){
-             this.setState({dumiaoEnterPopShow:true});
-        }else if(canStatus == 1){
-            this.setState({canMessageShow:true});
-        }else{
-            this.setState({tryOtherLoanPopShow:true});
+        } else if (canStatus == 0) {
+            this.setState({ dumiaoEnterPopShow: true });
+        } else if (canStatus == 1) {
+            this.setState({ canMessageShow: true });
+        } else {
+            this.setState({ tryOtherLoanPopShow: true });
         }
     }
-    imgClickHandler(){
-        this.setState({detailPopShow:true});
+    imgClickHandler = () => {
+        this.setState({ detailPopShow: true });
     }
-    detailClickHandler(){
-        this.setState({detailPopShow:false})
+    detailClickHandler = () => {
+        this.setState({ detailPopShow: false })
     }
-    dumiaoCloseHandler(){
-        this.setState({dumiaoEnterPopShow:false})
+    dumiaoCloseHandler = () => {
+        this.setState({ dumiaoEnterPopShow: false })
     }
-    tryOtherLoanCloseHandler(){
-        this.setState({tryOtherLoanPopShow:false})
+    tryOtherLoanCloseHandler = () => {
+        this.setState({ tryOtherLoanPopShow: false })
     }
-    canMessageCloseHandler(){
-        this.setState({canMessageShow:false})
+    canMessageCloseHandler = () => {
+        this.setState({ canMessageShow: false })
     }
     render() {
         let labelList = this.state.product.productLabelList;
-        return (
-            <div className="">
-                <div className="">
-                    <div className="borrow-money-list">
-                        <div className="icon-block"> <img src={this.state.product.productLogo}/> </div>
-                        <div className="info">
-                            <div className="t">
-                                <span className="title-text">{ this.state.product.productName }</span>
-                                <div className="text"> 借款范围（{ this.state.product.amountStr }） </div>
-                            </div>
-                            <div className="b">
-                                <div className="tag" >
-                                    {labelList && labelList.map((data, index) => {
-                                        return  <img src={ "images/tag-"+ data.labelType  +".png"} key={ index } />
-                                    })}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="borrow-money-detail-data">
-                        <div className="list">
-                            <div className="name-text">{this.state.product.monthRateStr}</div>
-                            <div className="data-text">
-                                每月费用
-							<img src="images/icon-6.png" onClick={this.imgClickHandler}/>
-                            </div>
-                        </div>
-                        <div className="list">
-                            <div className="name-text">{this.state.product.termRangeStr}个月</div>
-                            <div className="data-text">
-                                期限范围
-						</div>
-                        </div>
-                        <div className="list">
-                            <div className="name-text">{this.state.product.fastLoanValue}</div>
-                            <div className="data-text">
-                                最快放款
-						</div>
-                        </div>
-                    </div>
+
+        let goDumiao = () => {
+            return <div className="mask" style={{ zIndex: 100 }}>
+                <div className="detail-pop">
+                    <div className="pop-close" onClick={this.dumiaoCloseHandler}></div>
+                    <div className="pop-tip">{this.state.canMessage}</div>
+                    <a className="know-btn" href={`${API_PATH}/api/order/v1/jump.shtml?sourceType=${SOURCE_TYPE}&token=${USER.token}&uid=${USER.uid}&userGid=${USER.gid}&userId=${USER.id}&loanUuid=${this.state.loanUuid == null ? '' : this.state.loanUuid}`}>
+                        进入读秒查看</a>
                 </div>
-                <BorrowMoneyDatailList product={this.props.product} />
-                <div className="footer">
-                    <Nav className="btn" onClick={this.clickHandler}>马上拿钱</Nav>
-                </div>
-                {this.state.detailPopShow  && <div className="mask" style={{zIndex:100}}>
-                    <div className="detail-pop">
-                        <div className="pop-title">费用说明</div>
-                        <div className="pop-close" onClick={this.detailClickHandler}></div>
-                        <div className="pop-item">
-                            <div className="pop-item-wrap"><span className="pop-item-name">月利率</span><span className="pop-item-num">{this.state.product.loanRateStr}</span></div>
-                            <div className="pop-item-desc">贷款机构收取的贷款利率,按月收取</div>
-                        </div>
-                        <div className="pop-item">
-                            <div className="pop-item-wrap"><span className="pop-item-name">月服务利率</span><span className="pop-item-num">{this.state.product.serviceRateStr}</span></div>
-                            <div className="pop-item-desc">贷款机构收取的贷款服务费,按月收取</div>
-                        </div>
-                        <div className="pop-item">
-                            <div className="pop-item-wrap"><span className="pop-item-name">一次性手续费</span><span className="pop-item-num">{this.state.product.commRateStr}</span></div>
-                            <div className="pop-item-desc">贷款机构收取的一次性费用</div>
-                        </div>
-                        <a className="know-btn" onClick={this.detailClickHandler}>知道了</a>
-                    </div>
-                </div>}
-                {this.state.dumiaoEnterPopShow && <div className="mask" style={{zIndex:100}}>
-                    <div className="detail-pop">
-                        <div className="pop-close" onClick={this.dumiaoCloseHandler}></div>
-                        <div className="pop-tip">{this.state.canMessage}</div>
-                        <a className="know-btn" href={`${API_PATH}/api/order/v1/jump.shtml?sourceType=${SOURCE_TYPE}&token=${USER.token}&uid=${USER.uid}&userGid=${USER.gid}&userId=${USER.id}&loanUuid=${this.state.loanUuid == null ? '' : this.state.loanUuid}`}>进入读秒查看</a>
-                    </div>
-                </div>}
-                {this.state.canMessageShow && <div className="mask" style={{zIndex:100}}>
-                    <div className="detail-pop">
-                        <div className="pop-close" onClick={this.canMessageCloseHandler}></div>
-                        <div className="pop-tip">{this.state.canMessage}</div>
-                        <a className="know-btn" onClick={()=>$FW.Browser.inApp()?NativeBridge.close():gotoHandler("/static/loan/home/index.html")}>尝试其他借款</a>
-                    </div>
-                </div>}
-                {this.state.tryOtherLoanPopShow && <div className="mask" style={{zIndex:100}}>
-                    <div className="detail-pop">
-                        <div className="pop-close" onClick={this.tryOtherLoanCloseHandler}></div>
-                        <div className="pop-tip">{this.state.tryOtherLoanMsg}</div>
-                        <a className="know-btn" onClick={()=>$FW.Browser.inApp()?NativeBridge.close():gotoHandler("/static/loan/home/index.html")}>尝试其他借款</a>
-                    </div>
-                </div>}
             </div>
-        )
+        }
+
+        return <div className="">
+            <div className="">
+                <div className="borrow-money-list">
+                    <div className="icon-block">
+                        <img src={this.state.product.productLogo} /> </div>
+                    <div className="info">
+                        <div className="t">
+                            <span className="title-text">{this.state.product.productName}</span>
+                            <div className="text"> 借款范围（{this.state.product.amountStr}） </div>
+                        </div>
+                        <div className="b">
+                            <div className="tag" >
+                                {labelList && labelList.map((data, index) => {
+                                    return <img src={"images/tag-" + data.labelType + ".png"} key={index} />
+                                })}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="borrow-money-detail-data">
+                    <div className="list">
+                        <div className="name-text">{this.state.product.monthRateStr}</div>
+                        <div className="data-text">
+                            每月费用
+							<img src="images/icon-6.png" onClick={this.imgClickHandler} />
+                        </div>
+                    </div>
+                    <div className="list">
+                        <div className="name-text">{this.state.product.termRangeStr}个月</div>
+                        <div className="data-text">
+                            期限范围
+						</div>
+                    </div>
+                    <div className="list">
+                        <div className="name-text">{this.state.product.fastLoanValue}</div>
+                        <div className="data-text">
+                            最快放款
+						</div>
+                    </div>
+                </div>
+            </div>
+            <BorrowMoneyDatailList product={this.props.product} />
+            <div className="footer">
+                <Nav className="btn" onClick={this.clickHandler}>马上拿钱</Nav>
+            </div>
+            {this.state.detailPopShow && <div className="mask" style={{ zIndex: 100 }}>
+                <div className="detail-pop">
+                    <div className="pop-title">费用说明</div>
+                    <div className="pop-close" onClick={this.detailClickHandler}></div>
+                    <div className="pop-item">
+                        <div className="pop-item-wrap"><span className="pop-item-name">月利率</span><span className="pop-item-num">{this.state.product.loanRateStr}</span></div>
+                        <div className="pop-item-desc">贷款机构收取的贷款利率,按月收取</div>
+                    </div>
+                    <div className="pop-item">
+                        <div className="pop-item-wrap"><span className="pop-item-name">月服务利率</span><span className="pop-item-num">{this.state.product.serviceRateStr}</span></div>
+                        <div className="pop-item-desc">贷款机构收取的贷款服务费,按月收取</div>
+                    </div>
+                    <div className="pop-item">
+                        <div className="pop-item-wrap"><span className="pop-item-name">一次性手续费</span><span className="pop-item-num">{this.state.product.commRateStr}</span></div>
+                        <div className="pop-item-desc">贷款机构收取的一次性费用</div>
+                    </div>
+                    <a className="know-btn" onClick={this.detailClickHandler}>知道了</a>
+                </div>
+            </div>}
+            {this.state.dumiaoEnterPopShow && goDumiao()}
+            {this.state.canMessageShow && <div className="mask" style={{ zIndex: 100 }}>
+                <div className="detail-pop">
+                    <div className="pop-close" onClick={this.canMessageCloseHandler}></div>
+                    <div className="pop-tip">{this.state.canMessage}</div>
+                    <a className="know-btn" onClick={
+                        () => $FW.Browser.inApp() ?
+                            NativeBridge.close() :
+                            gotoHandler("/static/loan/home/index.html")}>
+                        尝试其他借款</a>
+                </div>
+            </div>}
+            {this.state.tryOtherLoanPopShow && <div className="mask" style={{ zIndex: 100 }}>
+                <div className="detail-pop">
+                    <div className="pop-close" onClick={this.tryOtherLoanCloseHandler}></div>
+                    <div className="pop-tip">{this.state.tryOtherLoanMsg}</div>
+                    <a className="know-btn" onClick={
+                        () => $FW.Browser.inApp() ?
+                            NativeBridge.close() :
+                            gotoHandler("/static/loan/home/index.html")}>
+                        尝试其他借款</a>
+                </div>
+            </div>}
+        </div>
     }
 }
 
