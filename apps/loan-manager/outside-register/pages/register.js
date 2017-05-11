5eccb51e2cd73063b1156ea6d02933a2348c1c54
liyanzhu <liyanzhu@ucfgroup.com>
2017-05-11 17:43:49 +0800

    update

--- apps/loan-manager/outside-register/pages/register.js
+++ apps/loan-manager/outside-register/pages/register.js
@@ -3,7 +3,12 @@ import CSSModules from 'react-css-modules'
 import styles from '../css/register.css'
 import { observer, inject } from 'mobx-react'
 
-@inject('registerInfo', 'uiStores') @observer @CSSModules(styles)
+@inject(['registerInfo', 'uiStores']) @observer @CSSModules(styles)
 export default class Register extends Component {
 
+    state =
+
+    render() {
+
+    }
 }
