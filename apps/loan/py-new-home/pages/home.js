003321e72e92b87ae679835cde4db0df1fd00d79
彭焱 <pengyan@ucfgroup.com>
2017-05-17 10:43:27 +0800

    update

--- apps/loan/py-new-home/pages/home.js
+++ apps/loan/py-new-home/pages/home.js
@@ -11,7 +11,7 @@ export default class Home extends React.Component {
     render(){
         return (
             <div>
-                <div>11111111111111111111111</div>
+
                 <BottomNav />
             </div>
         )
--- apps/loan/py-new-home/stores/home.js
+++ apps/loan/py-new-home/stores/home.js
@@ -1,3 +1,15 @@
+import {extendObservable} from 'mobx'
+
 export default class Home{
+    constructor(request,state={}){
+        this.request = request
+    }
+
+    getProductList(params){
+        params = {}
+        return this.request('api/product/productList',params).then( data => {
+            ReactDOM.render(<Home products={data.resultList} otherProducts={data.extList}/>, CONTENT_NODE)
+        })
+    }
 
 }
