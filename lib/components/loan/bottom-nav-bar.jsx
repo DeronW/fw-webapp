b2d84475e2da1c86c59fffa6acd96ca18bd0b745
liyanzhu <liyanzhu@ucfgroup.com>
2017-05-22 15:03:23 +0800

    Modify: home

--- lib/components/loan/bottom-nav-bar.jsx
+++ lib/components/loan/bottom-nav-bar.jsx
@@ -101,7 +101,7 @@ class BottomNavBar extends React.Component {
             <div style={easyloan_style_footer_fixed}>
                 <a style={_style_tab_a} href={_href_a}>
                     <i style={_style_tab_a_icon}></i>
-                    借钱
+                    首页
                 </a>
                 <a style={_style_tab_bill} href={_href_bill}>
                     <i style={_style_tab_bill_icon}></i>
