93a8f65c118993442fd7e92157a56493dfce7008
liyanzhu <liyanzhu@ucfgroup.com>
2017-05-22 10:16:07 +0800

    Update

--- apps/loan-manager/loan-home/router.js
+++ apps/loan-manager/loan-home/router.js
@@ -11,6 +11,8 @@ import {
 import Loan from './pages/loan'
 import UserEntey from './pages/user-entry'
 import Bill from './pages/bill'
+import FXHBillDetail from './pages/fxh-bill-detail'
+// import DMBillDetail from './pages/dm-bill-detail'
 import Promote from './pages/promote'
 import Market from './pages/market'
 import User from './pages/user'
@@ -24,12 +26,11 @@ export default (stores) => {
                 <Route exact path='/loan' component={Loan} />
                 <Route exact path='/user-entry' component={UserEntey} />
                 <Route exact path='/bill' component={() => <Redirect to='/bill/applying' />} />
-                <Route exact path='/bill/:billType' component={Bill}>
-                    {/* <Route exact path='/bill/:billType/:loanProduct/:billId' component={BillDetail} >
-                        <Route exact path='/bill/:billType/:loanProduct/:billId/repayment' component={Repayment} />
-                        <Route exact path='/bill/:billType/:loanProduct/:billId/repayment-record' component={RepaymentRecord} />
-                    </Route> */}
-                </Route>
+                <Route exact path='/bill/:billType' component={Bill} />
+                <Route exact path='/bill/fxh/:billId' component={FXHBillDetail} />
+                {/* <Route exact path='/bill/dm/:billId' component={DMBillDetail} /> */}
+                {/* <Route exact path='/bill/:billType/:loanProduct/:billId/repayment' component={Repayment} />
+                <Route exact path='/bill/:billType/:loanProduct/:billId/repayment-record' component={RepaymentRecord} /> */}
                 <Route exact path='/promote' component={Promote} />
                 <Route exact path='/market' component={Market} />
                 <Route exact path='/user' component={User} />
