d8b71ce7240e24009ae7f1115ab84ce03a4a67f5
彭焱 <pengyan@ucfgroup.com>
2017-05-22 17:12:18 +0800

    update

--- apps/loan/weixin-invite4app/react/index.jsx
+++ apps/loan/weixin-invite4app/react/index.jsx
@@ -6,7 +6,7 @@ $FW.DOMReady(function () {
         NativeBridge.share({
             title: '掌上钱包，随用随取',
             image: 'https://static.9888.cn/images/loan/invitation.jpg',
-            link: `https://m.easyloan888.com/static/loan/outside-register/index.html?channelCode=OFFICIAL&invitationCode=${USER.code}&jumpType=wx`,
+            link: `https://m.easyloan888.com/static/loan/outside-register/index.html?channelCode=OFFICIAL&invitationCode=${inviteCode}&jumpType=wx`,
             desc: '缺钱不用愁，注册放心花，借款神器，急速到账'
         })
     });
