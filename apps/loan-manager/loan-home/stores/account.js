baac17f64ecffee6c64dfce2609592c3cbe07fde
liyanzhu <liyanzhu@ucfgroup.com>
2017-05-26 11:43:39 +0800

    update

---
+++ apps/loan-manager/loan-home/stores/account.js
@@ -0,0 +1,26 @@
+import { extendObservable } from 'mobx'
+
+export default class Products {
+
+    constructor(request) {
+        this.request = request;
+        extendObservable(this, {
+            phone: '',
+            uid: '',
+            token: '',
+        })
+    }
+
+    fetchProductList = () => {
+        let API_PATH = document.getElementById('api-path').value;
+        return this.request({
+            url: `${API_PATH}/api/product/v1/productList.json`,
+            method: 'post',
+            data: { }
+        }).then(data => {
+            this.loanProducts = data.resultList;
+            this.otherProducts = data.extList;
+        })
+    }
+
+}
