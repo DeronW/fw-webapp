e377539e2c296d7614953d27d4a989083ba9a54d
liyanzhu <liyanzhu@ucfgroup.com>
2017-06-06 14:45:38 +0800

    Add: header

--- apps/insurance/home/pages/car-info.js
+++ apps/insurance/home/pages/car-info.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import CSSModules from 'react-css-modules'
 import { observer, inject } from 'mobx-react'
-import { Redirect } from 'react-router-dom'
 
-import { BrowserFactory } from 'fw-javascripts'
+import Header from '../components/header'
 
-import Nav from '../components/header'
+import styles from '../css/car-info.css'
 
-// import styles from '../css/car-info.css'
-
-// @inject('') @observer @CSSModules(styles)
-class MainPanel extends React.Component {
-
+@CSSModules(styles, {"allowMultiple": true, "errorWhenNotFound": false})
+class CarInfo extends React.Component {
+    render() {
+        return (
+            <div styleName="fake-body">
+                <Header title="车险" history={this.props.history} />
+            </div>
+        )
+    }
 }
 
-export default MainPanel
+export default CarInfo
