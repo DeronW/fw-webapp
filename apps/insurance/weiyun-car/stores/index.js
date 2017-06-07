6c5dc69f1a04087f068e285a007e6ce354f0ded0
delong <w.del@qq.com>
2017-06-07 15:08:41 +0800

    update

--- apps/insurance/weiyun-car/stores/index.js
+++ apps/insurance/weiyun-car/stores/index.js
@@ -1,5 +1,5 @@
 import CurrOrder from './curr-order.js'
-import Customer from './customer.js'
+import Customer from './customer-model.js'
 import PolicyDetail from './policy-detail-model.js'
 
 export {
