58522ad236bf46f646bee14b37ed67bae1cca6af
delong <w.del@qq.com>
2017-06-08 14:24:39 +0800

    update

--- apps/insurance/weiyun-car/helpers/native-bridge.js
+++ apps/insurance/weiyun-car/helpers/native-bridge.js
@@ -2,10 +2,13 @@ import { NativeBridgeFactory } from 'fw-javascripts'
 
 const NativeBridge = new NativeBridgeFactory('FinancialWorkshop')
 
-
-NativeBridge.extendAction('close', function () {
+NativeBridge.close = function () {
     this.action('close')
-})
+}
+
+NativeBridge.setTitle = function (title) {
+    this.action('set_title', title)
+}
 
 export default NativeBridge
 
