19aa4e735c37a809f166be820f95bd822f1d6a96
彭焱 <pengyan@ucfgroup.com>
2017-06-08 17:17:37 +0800

    update

--- apps/loan/outside-mortgage-apply-ten-million-loan/react/index.jsx
+++ apps/loan/outside-mortgage-apply-ten-million-loan/react/index.jsx
@@ -85,9 +85,11 @@ class ApplyTenMillionLoan extends React.Component {
     applyBtn = () => {
         if (!isMobilePhone(this.state.phoneVal)) {
             $FW.Component.Toast("手机号格式不正确");
-        } else if (this.state.codeVal == '') {
+        } else if(this.state.codeVal == '') {
             $FW.Component.Toast("验证码错误，请重新输入");
-        } else {
+        } else if(this.state.codeToken == ''){
+            $FW.Component.Toast("验证码错误，请重新输入");
+        }else {
             $FW.Post(`${API_PATH}/api/userBase/v1/register.json`, {
                 mobile: this.state.phoneVal,
                 codeToken: this.state.codeToken,
