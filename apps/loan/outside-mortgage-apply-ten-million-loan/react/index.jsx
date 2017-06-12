d8ac9e604c5cae7e4046d60ee92218069e4cdccb
彭焱 <pengyan@ucfgroup.com>
2017-06-12 12:22:02 +0800

    update

--- apps/loan/outside-mortgage-apply-ten-million-loan/react/index.jsx
+++ apps/loan/outside-mortgage-apply-ten-million-loan/react/index.jsx
@@ -59,7 +59,7 @@ class ApplyTenMillionLoan extends React.Component {
         } else {
             $FW.Post(`${API_PATH}/api/userBase/v1/userExist.json`,{mobile: this.state.phoneVal, sourceType: 5, version: 'v1'}).then(data=>{
                 if(data.code == 10000){
-                    this.timerTimeout = setTimeout(() => {
+                    setTimeout(() => {
                         window.location.href = '/static/loan/outside-mortgage-id-download/index.html'
                     }, 2000)
                 }else if(data.code == 20014){
@@ -129,10 +129,6 @@ class ApplyTenMillionLoan extends React.Component {
             countdown: 60,
             countdownShow: false
         })
-    }
-
-    componentWillUnmount() {
-        clearTimeout(timerTimeout)
     }
 
     render() {
