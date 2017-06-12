2e788d10271e508fad3c819b1c9115b37b0eac6b
liyanzhu <liyanzhu@ucfgroup.com>
2017-06-12 11:49:42 +0800

    Add: hisyory push

--- apps/insurance/weiyun-car/stores/policy-detail.js
+++ apps/insurance/weiyun-car/stores/policy-detail.js
@@ -26,16 +26,6 @@ export default class BasicInfo {
         }, state)
     }
 
-    submit = () => {
-        // return this.request('blablabla').then(data => {
-        //     if (data.success) {
-        //
-        //     } else {
-        //
-        //     }
-        // })
-    }
-
     setFormData = (field, value) => {
         let formattedValue = parseInt(value);
         if (field.indexOf('BuJiMian') > -1) {
@@ -46,7 +36,23 @@ export default class BasicInfo {
     }
 
     @computed get valid() {
+        if (this.HcSanFangTeYue === null && this.CheSun !== 0) return false; // 投保车损险但没有选择第三方是否投保，信息缺失
+        ['CheSun', 'Sanzhe', 'SiJi', 'ChengKe', 'DaoQiang', 'HuaHen', 'Boli', 'ZiRan', 'SheShui'].forEach((key) => {
+            if (this[key] === null) return false // 其他必选项信息缺失
+        })
+        return true;
+    }
 
+    submit = (history) => () => {
+        // if (!this.valid) return
+        history.push('/policy-quotation');
+        // return this.request('blablabla').then(data => {
+        //     if (data.success) {
+        //
+        //     } else {
+        //
+        //     }
+        // })
     }
 
     logData = () => console.log(this)
