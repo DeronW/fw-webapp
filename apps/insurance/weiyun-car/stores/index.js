d38f1f1d8e863f44e998da3a7e98ac3d14178793
sunjiankun <sunjiankun@ucfgroup.com>
2017-06-13 15:19:01 +0800

    add alipay

--- apps/insurance/weiyun-car/stores/index.js
+++ apps/insurance/weiyun-car/stores/index.js
@@ -3,11 +3,13 @@ import CarInfo from './car-info.js'
 import PolicyDetail from './policy-detail.js'
 import CustomerInfo from './customer-info.js'
 import Orders from './orders.js'
+import OrderPayment from './order-payment.js'
 
 export {
     BasicInfo,
     CarInfo,
     PolicyDetail,
     CustomerInfo,
-    Orders
+    Orders,
+    OrderPayment
 }
