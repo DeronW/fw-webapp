536824629c8bac5224029efa78e512ac181a7f53
sunjiankun <sunjiankun@ucfgroup.com>
2017-06-13 17:15:58 +0800

    FIX

--- apps/insurance/weiyun-car/stores/index.js
+++ apps/insurance/weiyun-car/stores/index.js
@@ -3,6 +3,7 @@ import CarInfo from './car-info.js'
 import PolicyDetail from './policy-detail.js'
 import CustomerInfo from './customer-info.js'
 import Orders from './orders.js'
+import OrderPayment from './order-payment.js'
 import OrderConfirm from './order-confirm.js'
 
 
@@ -12,5 +13,6 @@ export {
     PolicyDetail,
     CustomerInfo,
     Orders,
+    OrderPayment,
     OrderConfirm
 }
