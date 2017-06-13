3b71531c30bb552b66922ec52ba05506f7e5cf1d
fengxiaoran <fengxiaoran@ucfgroup.com>
2017-06-13 15:59:49 +0800

    no message

--- apps/insurance/weiyun-car/stores/index.js
+++ apps/insurance/weiyun-car/stores/index.js
@@ -4,6 +4,8 @@ import PolicyDetail from './policy-detail.js'
 import CustomerInfo from './customer-info.js'
 import Orders from './orders.js'
 import OrderPayment from './order-payment.js'
+import OrderConfirm from './order-confirm.js'
+
 
 export {
     BasicInfo,
@@ -11,5 +13,6 @@ export {
     PolicyDetail,
     CustomerInfo,
     Orders,
-    OrderPayment
+    OrderPayment,
+    OrderConfirm
 }
