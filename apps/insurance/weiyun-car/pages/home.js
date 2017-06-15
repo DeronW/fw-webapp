6821b16dc583b0cd14b93d81f0383da6f2fe5679
delong <w.del@qq.com>
2017-06-15 17:36:24 +0800

    update

--- apps/insurance/weiyun-car/pages/home.js
+++ apps/insurance/weiyun-car/pages/home.js
@@ -4,28 +4,17 @@ import { observer, inject } from 'mobx-react'
 import { Redirect, Link } from 'react-router-dom'
 
 
-// import styles from '../css/home.css'
-
-// @inject('') @observer @CSSModules(styles)
+@observer @CSSModules(styles)
 class Home extends React.Component {
     componentDidMount() {
         document.title = 'HOME'
     }
     render() {
-        return <div>
-            <Link to="/basic-info">Basic info</Link>
-            <br />
-            <Link to="/car-info">Car info</Link>
-            <br />
-            <Link to="/policy-detail">Policy detail</Link>
-            <br />
-            <Link to="/orders">orders</Link>
-            <br />
-            <Link to="/policy-quotation">policy-quotation</Link>
-            <br />
-            <br />
-            <br />
-            <br />
+        return <div styleName="bg">
+            <img styleName="banner" src={require('../images/home/banner.jpg')} />
+            <div>
+
+            </div>
         </div>
     }
 }
