910af76423dc4f903d60a9af794a10773e79ada3
delong <w.del@qq.com>
2017-06-19 11:29:39 +0800

    remove test & practice code

--- apps/loan/features 2/entry.js
+++
@@ -1,9 +0,0 @@
-import { render } from 'react-dom'
-
-import AppRouter from './router'
-
-import './css/index.css'
-
-let stores = {}
-
-render(AppRouter(stores), document.getElementById('cnt'))
--- es7-lib/loan 2/components/index.js
+++
@@ -1,4 +0,0 @@
-import Header from './header.js'
-import BottomNavBar from './bottom-nav-bar.js'
-
-export { Header, BottomNavBar}
