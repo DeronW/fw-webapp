8930044e45db62a869ddf6e86c83d5e88be098fd
liyanzhu <liyanzhu@ucfgroup.com>
2017-06-21 14:53:36 +0800

    Update

--- apps/insurance/weiyun-car/pages/policy-quotation.js
+++ apps/insurance/weiyun-car/pages/policy-quotation.js
@@ -6,81 +6,54 @@ import { Redirect } from 'react-router-dom'
 import { BrowserFactory } from 'fw-javascripts'
 
 import Header from '../components/header'
+import BottomButton from '../components/bottom-button'
 
 import styles from '../css/policy-quotation.css'
 import styles_icon_circle from '../css/icons/circle.css'
 
-@inject('quotations')
+@inject('quotations', 'current_order')
 @observer
 @CSSModules(styles, { "allowMultiple": true, "errorWhenNotFound": false })
 class PolicyQuotation extends React.Component {
 
-    state = { selected: null }; // 人保2  太平洋1  平安0
+    state = { selected: '' }; // 人保2  太平洋1  平安0
 
     selectFirm = (firmNo) => {
-        let selected = this.state.selected === firmNo ? null : firmNo;
+        let selected = this.state.selected === firmNo ? '' : firmNo;
         this.setState({ selected: selected });
-    }
-
-    submitHandler = () => {
-        this.props.history.replace('/customer')
     }
 
     render() {
 
-        let { selected } = this.state;
-        let { quotations, history } = this.props;
+        let { quotations, current_order, history } = this.props;
+
+        let gen_quotation_item = (itemId, itemName) => (
+            <div styleName="quotation-item" onClick={() => { this.selectFirm(itemId) }}>
+                <div styleName="check-box">
+                    <i className={this.state.selected === itemId ?
+                        styles_icon_circle.checked :
+                        styles_icon_circle.unchecked}></i>
+                </div>
+                <div styleName="quotation-entry" onClick={() => { history.push(`policy-quotation/${itemId}`) }}>
+                    <div styleName={`firm-name-${itemId}`}>{itemName}</div>
+                    <div styleName="price-container">
+                        <div styleName="discount-price">￥{quotations[itemId].actualPrice}</div>
+                        <div styleName="origin-price">官网报价{quotations[itemId].originPrice}</div>
+                    </div>
+                </div>
+            </div>
+        )
 
         return (
             <div>
-                <Header title="保费报价" history={this.props.history} />
-
+                <Header title="保费报价" history={history} />
                 <div styleName="quotation-container">
-                    <div styleName="quotation-item" onClick={() => { this.selectFirm(2) }}>
-                        <div styleName="check-box">
-                            <i className={selected === 2 ?
-                                styles_icon_circle.checked :
-                                styles_icon_circle.unchecked}></i>
-                        </div>
-                        <div styleName="quotation-entry" onClick={() => { history.push('policy-quotation/2') }}>
-                            <div styleName="firm-name renbao">人保车险</div>
-                            <div styleName="price-container">
-                                <div styleName="discount-price">￥{quotations['2'].actualPrice}</div>
-                                <div styleName="origin-price">官网报价{quotations['2'].originPrice}</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div styleName="quotation-item">
-                        <div styleName="check-box" onClick={() => { this.selectFirm(1) }}>
-                            <i className={selected === 1 ?
-                                styles_icon_circle.checked :
-                                styles_icon_circle.unchecked}></i>
-                        </div>
-                        <div styleName="quotation-entry" onClick={() => { history.push('policy-quotation/1') }}>
-                            <div styleName="firm-name taipingyang">太平洋车险</div>
-                            <div styleName="price-container">
-                                <div styleName="discount-price">￥{quotations['1'].actualPrice}</div>
-                                <div styleName="origin-price">官网报价{quotations['1'].originPrice}</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div styleName="quotation-item">
-                        <div styleName="check-box" onClick={() => { this.selectFirm(0) }}>
-                            <i className={selected === 0 ?
-                                styles_icon_circle.checked :
-                                styles_icon_circle.unchecked}></i>
-                        </div>
-                        <div styleName="quotation-entry" onClick={() => { history.push('policy-quotation/0') }}>
-                            <div styleName="firm-name pingan">平安车险</div>
-                            <div styleName="price-container">
-                                <div styleName="discount-price">￥{quotations['0'].actualPrice}</div>
-                                <div styleName="origin-price">官网报价{quotations['0'].originPrice}</div>
-                            </div>
-                        </div>
-                    </div>
+                    { gen_quotation_item('2', '人保车险') }
+                    { gen_quotation_item('1', '太平洋车险') }
+                    { gen_quotation_item('0', '平安车险') }
                 </div>
-
-                <a styleName="next-btn" onClick={this.submitHandler}>下一步</a>
+                <BottomButton active={this.state.selected !== ''} title={'确认提交'}
+                    onClick={() => { current_order.selectFirm(history, this.state.selected)} } />
             </div>
         )
     }
--- apps/insurance/weiyun-car/stores/current-order.js
+++ apps/insurance/weiyun-car/stores/current-order.js
@@ -2,19 +2,22 @@ import { extendObservable, computed } from 'mobx'
 
 export default class PolicyDetail {
 
-    constructor(request, state = {}) {
-        this.request = request
+    constructor(Get) {
+        this.Get = Get
 
         extendObservable(this, {
-            selectedFirm: '2', // 0:平安、1:太平洋、2:人保
-            temporaryPolicyId: '', // 试算id
+            selectedFirm: '', // 0:平安、1:太平洋、2:人保
             alipayURL: '', // 支付宝支付链接
             payStatus: '', // 支付状态， 未支付 成功 失败等等
         })
     }
 
-    setPolicyId = (id) => {
-        this.temporaryPolicyId = id;
+    selectFirm = (history, selected) => {
+        this.Get('/blabla', { selectFirm: this.selected })
+            then(data => {
+                this.selectFirm = selected;
+                history.push('/customer')
+            })
     }
 
     toPay = () => {
