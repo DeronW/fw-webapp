d8c484118cde13297f9c6d9cfc9e4d530bede61b
刘桃桃 <liutaotao@ucfgroup.com>
2017-06-29 16:21:33 +0800

    check

--- apps/loan/user-red-packet-detail/react/index.jsx
+++ apps/loan/user-red-packet-detail/react/index.jsx
@@ -32,7 +32,6 @@ class RedPacketDetail extends React.Component{
     render() {
         // 判断红包状态
         let statusText = (item) => {
-            console.log(111111111);
             if(parseInt(item.redbagStatus) == 0){
                 return <span className="status-text">注册冻结</span>
             }else if(item.redbagStatus ==1){
@@ -68,9 +67,9 @@ class RedPacketDetail extends React.Component{
                     </div>
         }
 // 没数据的空页面
-        let empty = <div className="no-data-box">
-            <img className="no-data-img" src="images/no-data.png" />
-        </div>;
+        // let empty = <div className="no-data-box">
+        //     <img className="no-data-img" src="images/no-data.png" />
+        // </div>;
 
      
 
@@ -80,41 +79,9 @@ class RedPacketDetail extends React.Component{
                 <div className="data-list">
                     {rows.map(item_list)}
                 </div>
-                {/*<div className="data-list">
-                    <div className="list-item">
-                        <div className="red-status">
-                            {rows.map(statusText)}
-                            <span className="status-num">15</span>
-                        </div>
-                        <div className="sub-red-status">
-                            <span className="sub-status-text">好友（尾号1231）首借已还款</span>
-                            <span className="status-time">2016-12-16</span>
-                        </div>
-                    </div>
-                    <div className="list-item">
-                        <div className="red-status">
-                            <span className="status-text">可提现</span>
-                            <span className="status-num">15</span>
-                        </div>
-                        <div className="sub-red-status">
-                            <span className="sub-status-text">好友（尾号1231）首借已还款</span>
-                            <span className="status-time">2016-12-16</span>
-                        </div>
-                    </div>
-                    <div className="list-item">
-                        <div className="red-status">
-                            <span className="status-text">可提现</span>
-                            <span className="status-num">15</span>
-                        </div>
-                        <div className="sub-red-status">
-                            <span className="sub-status-text">好友（尾号1231）首借已还款</span>
-                            <span className="status-time">2016-12-16</span>
-                        </div>
-                    </div>
-                </div>*/}
                 {/*已加载完全部数据提示*/}
                 {hasData && <div className="data-completion">已加载完全部数据</div>}
-                {rows.length === 0 && !hasData && empty}
+                {/*{rows.length === 0 && !hasData && empty}*/}
             </div>
         )
     }
