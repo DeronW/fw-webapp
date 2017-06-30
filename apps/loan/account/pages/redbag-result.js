a974c4e95dbeb1fd134d4394cbba6b14d3cd8cf0
彭焱 <pengyan@ucfgroup.com>
2017-06-30 21:00:54 +0800

    update

--- apps/loan/account/pages/redbag-result.js
+++ apps/loan/account/pages/redbag-result.js
@@ -38,19 +38,24 @@ export default class RedBagResult extends React.Component {
                     </div>
                 </div>}
                 {redbag.failReason && <div styleName="fail-result-box">
-                    <div styleName="wrap-box">
-                        <div styleName="fail-icon"><img src={require("../images/fail-icon.png")} /></div>
-                        <div styleName="loan-result4">
-                            <div styleName="waiting-result">
-                                <div styleName="icon5"></div>
-                                <div styleName="icon5-info">提现失败</div>
-                                <div styleName="icon5-info-btm">{redbag.failReason}</div>
+                    <div>
+                        <div styleName="wrap-box">
+                            <div styleName="fail-icon"><img src={require("../images/fail-icon.png")} /></div>
+                            <div styleName="loan-result4">
+                                <div styleName="waiting-result">
+                                    <div styleName="icon5"></div>
+                                    <div styleName="icon5-info">提现失败</div>
+                                    <div styleName="icon5-info-btm">{redbag.failReason}</div>
+                                </div>
                             </div>
                         </div>
+                        <div styleName="customer-service">
+                            <div styleName="service-wrap"><img src={require("../images/phone.png")} />如有问题请致电：<a
+                                href="tel:400-102-0066">400-102-0066</a></div>
+                        </div>
                     </div>
-                    <div styleName="customer-service">
-                        <div styleName="service-wrap"><img src={require("../images/phone.png")} />如有问题请致电：<a
-                            href="tel:400-102-0066">400-102-0066</a></div>
+                    <div styleName="btn-wrap">
+                        <div styleName="credit-btn" onClick={this.closeHandler}>关闭</div>
                     </div>
                 </div>}
             </div>
