f023e764d5ea35dbce2bd83cb3f7403c8c955040
彭焱 <pengyan@ucfgroup.com>
2017-06-30 12:26:17 +0800

    update

--- apps/loan/account/stores/index.js
+++ apps/loan/account/stores/index.js
@@ -1,17 +1,11 @@
 import Account from './account.js'
 import BankCard from './bank-card.js'
-<<<<<<< HEAD
 import RedBag from './red-bag.js'
-=======
 import RedPacketDetail from './red-packet-detail.js'
->>>>>>> b6d211cffdff8301c0f176adc1087d6347639caf
 
 export default {
     Account,
     BankCard,
-<<<<<<< HEAD
-    RedBag
-=======
+    RedBag,
     RedPacketDetail
->>>>>>> b6d211cffdff8301c0f176adc1087d6347639caf
 }
