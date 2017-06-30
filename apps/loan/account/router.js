843f4ed2e3e69e0edfcdc8a1d472998e5f780589
刘桃桃 <liutaotao@ucfgroup.com>
2017-06-30 15:48:39 +0800

    合并stores层为一个

--- apps/loan/account/router.js
+++ apps/loan/account/router.js
@@ -11,7 +11,7 @@ import Login from './pages/login.js'
 import SetPassword from './pages/set-password.js'
 
 import RedBag from './pages/red-bag.js'
-import RedPacketDetail from './pages/red-packet-detail.js'
+import RedBagRecords from './pages/red-bag-records.js'
 import RedPacketResult from './pages/red-packet-result.js'
 import BankCard from './pages/bank-card.js'
 import BankCardAdd from './pages/bank-card-add.js'
@@ -25,7 +25,7 @@ export default (stores) => {
                 <Route exact path='/login' component={Login} />
                 <Route exact path='/set-password' component={SetPassword} />
                 <Route exact path='/red-bag' component={RedBag} />
-                <Route exact path='/red-packet-detail' component={RedPacketDetail} />
+                <Route exact path='/pages/red-bag-records' component={RedBagRecords} />
                 <Route exact path='/red-packet-result' component={RedPacketResult} />
                 <Route exact path='/bank-card' component={BankCard} />
                 <Route exact path='/bank-card-add' component={BankCardAdd} />
--- apps/loan/account/stores/red-bag.js
+++ apps/loan/account/stores/red-bag.js
@@ -1,61 +1,82 @@
 import { extendObservable, computed } from 'mobx'
 import { Components } from 'fw-javascripts'
+import { Storage } from '../../lib/helpers'
+import * as $FW from 'fw-javascripts'
 
-export default class RedBag{
+export default class RedBag {
     constructor(Post) {
         this.Post = Post
 
         extendObservable(this, {
-            batchGid:'',
-            freezeAmt:'',
-            hasWithdrawAmt:'',
-            cardList:[],
-            borrowBtnStatus:''
+            batchGid: '',
+            freezeAmt: '',
+            hasWithdrawAmt: '',
+            cardList: [],
+            borrowBtnStatus: '',
+            page: 1,
+            rows: [],
+            hasData: true
         })
     }
 
     @computed get default_card_number() {
         let filtered = this.cardList.filter(e => e.isRealNameBindCard === true);
-        return filtered[0] && {cardNo:filtered[0].cardNo,uuid:filtered[0].uuid}
+        return filtered[0] && { cardNo: filtered[0].cardNo, uuid: filtered[0].uuid }
     }
 
 
     fetch_packet_num = () => {
-        return this.Post('/api/redbag/v1/summary.json').then(data=>{
+        return this.Post('/api/redbag/v1/summary.json').then(data => {
             this.batchGid = data.batchGid;
             this.freezeAmt = data.freezeAmt;
             this.hasWithdrawAmt = data.hasWithdrawAmt;
-        },e => Components.showToast(e.message))
+        }, e => Components.showToast(e.message))
     }
 
     fetch_user_status = () => {
-        return this.Post('/api/loan/v1/baseinfo.json',{productId:1}).then(data=>{
+        return this.Post('/api/loan/v1/baseinfo.json', { productId: 1 }).then(data => {
             this.borrowBtnStatus = data.borrowBtnStatus;
-        },e => Components.showToast(e.message))
+        }, e => Components.showToast(e.message))
     }
 
     fetch_bankcard_list = () => {
-        return this.Post('/api/bankcard/v1/bankcardlist.json').then(data=>{
+        return this.Post('/api/bankcard/v1/bankcardlist.json').then(data => {
             this.cardList = data.userBankList.withdrawBankcard;
-        },e => Components.showToast(e.message))
+        }, e => Components.showToast(e.message))
     }
 
     getSMSCode = () => {
-            return this.Post('/api/redbag/v1/veriftycode.json',{
-                batchGid:this.batchGid
-            }).then(data=>{},e => Components.showToast(e.message))
+        return this.Post('/api/redbag/v1/veriftycode.json', {
+            batchGid: this.batchGid
+        }).then(data => {}, e => Components.showToast(e.message))
     }
 
-    withdrawConfirm = (value,uuid,history) => {
-        return this.Post('/api/redbag/v1/apply.json',{
-            batchGid:this.batchGid,
-            verifyCode:value,
-            withdrawAmt:this.withdrawAmt,
-            withdrawCardUuid:uuid
-        }).then(data=>{
+    withdrawConfirm = (value, uuid, history) => {
+        return this.Post('/api/redbag/v1/apply.json', {
+            batchGid: this.batchGid,
+            verifyCode: value,
+            withdrawAmt: this.withdrawAmt,
+            withdrawCardUuid: uuid
+        }).then(data => {
             history.push('/red-packet-result');
-        }, e =>{
+        }, e => {
             history.push('/red-packet-result');
+        })
+    }
+    // 明细页下拉加载更多
+     loadMore = (done) => {
+        if (!this.hasData) return done && done();
+        // let user = $FW.Store.getUserDict();
+
+        return this.Post(`/api/redbag/v1/list.json`, {
+            pageSize: 20,
+            pageIndex: this.page
+        }).then(data => {
+            let RedPacketDetailList = data.resultList;
+            this.rows = RedPacketDetailList;
+            this.page = this.page < data.totalPage ? this.page + 1 : this.page = 0;
+            this.hasData = !!RedPacketDetailList.length;
+            done && done()
         })
     }
 
