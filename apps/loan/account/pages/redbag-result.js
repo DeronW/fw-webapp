f925ea75c89a884570d7886cfef8bcbbbceebd1d
彭焱 <pengyan@ucfgroup.com>
2017-07-03 18:54:53 +0800

    update

--- apps/loan/account/pages/redbag-result.js
+++ apps/loan/account/pages/redbag-result.js
@@ -26,39 +26,42 @@ export default class RedBagResult extends React.Component {
         return (
             <div>
                 <Header title="提现结果" history={history} />
-                {redbag.withdrawResult.success && <div styleName="waiting-result-box">
-                    <div styleName="wrap-box">
-                        <div styleName="success-icon">
-                            <img src={require("../images/success-icon.png")} /></div>
-                        <div styleName="loan-result1">
-                            <div styleName="icon1"></div>
-                            <div styleName="icon1-info">提现请求成功，等待银行处理</div>
-                            <div styleName="time1">{redbag.applyTimeStr}</div>
-                            <div styleName="line"></div>
-                            <div styleName="waiting-result">
-                                <div styleName="icon2"></div>
-                                <div styleName="icon2-info">预计1-3个工作日到账</div>
+                <div styleName="result-wrap">
+                    {redbag.withdrawResult.success && <div styleName="waiting-result-box">
+                        <div styleName="wrap-box">
+                            <div styleName="success-icon">
+                                <img src={require("../images/success-icon.png")} /></div>
+                            <div styleName="loan-result1">
+                                <div styleName="icon1"></div>
+                                <div styleName="icon1-info">提现请求成功，等待银行处理</div>
+                                <div styleName="time1">{redbag.applyTimeStr}</div>
+                                <div styleName="line"></div>
+                                <div styleName="waiting-result">
+                                    <div styleName="icon2"></div>
+                                    <div styleName="icon2-info">预计1-3个工作日到账</div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>}
-                {redbag.withdrawResult.reason && <div styleName="fail-result-box">
-                    <div styleName="wrap-box">
-                        <div styleName="fail-icon"><img src={require("../images/fail-icon.png")} /></div>
-                        <div styleName="loan-result4">
-                            <div styleName="waiting-result">
-                                <div styleName="icon5"></div>
-                                <div styleName="icon5-info">提现失败</div>
-                                <div styleName="icon5-info-btm">{redbag.withdrawResult.reason}</div>
+                    </div>}
+                    {redbag.withdrawResult.reason && <div styleName="fail-result-box">
+                        <div styleName="wrap-box">
+                            <div styleName="fail-icon"><img src={require("../images/fail-icon.png")} /></div>
+                            <div styleName="loan-result4">
+                                <div styleName="waiting-result">
+                                    <div styleName="icon5"></div>
+                                    <div styleName="icon5-info">提现失败</div>
+                                    <div styleName="icon5-info-btm">{redbag.withdrawResult.reason}</div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div styleName="customer-service">
-                        <div styleName="service-wrap">
-                            <img src={require("../images/phone.png")} />如有问题请致电：
+                        <div styleName="customer-service">
+                            <div styleName="service-wrap">
+                                <img src={require("../images/phone.png")} />如有问题请致电：
                                 <a href="tel:400-102-0066">400-102-0066</a></div>
-                    </div>
-                </div>}
+                        </div>
+                    </div>}
+                </div>
+
                 <div styleName="btn-wrap">
                     <div styleName="credit-btn" onClick={this.closeHandler}>关闭</div>
                 </div>
