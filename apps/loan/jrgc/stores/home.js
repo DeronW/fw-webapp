a3cc3979819ff2425e1502d5f32e9c795f911105
sunjiankun <sunjiankun@ucfgroup.com>
2017-07-11 11:19:22 +0800

    dev

--- apps/loan/jrgc/stores/home.js
+++ apps/loan/jrgc/stores/home.js
@@ -2,12 +2,22 @@ import { extendObservable, computed } from 'mobx'
 import { Components } from 'fw-javascripts'
 import { Storage } from '../../lib/helpers'
 
-export default class Home{
-    constructor(Post){
+export default class Home {
+    constructor(Post) {
         this.Post = Post
 
-        extendObservable(this,{
-            
+        extendObservable(this, {
+            loanProductList: [],
+            subProductList: [],
+            showBulletin: false,
+            bulletinCnt: ''
         })
+    }
+
+    getDataHandler = () => {
+        this.Post(`/api/product/v1/productList.json`)
+            .then(data => {
+                this.loanProductList = data.resultList
+            }, e => Components.showToast(e))
     }
 }
