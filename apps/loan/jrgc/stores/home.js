4a23f32c1f5a4c9e443346cdf4ef897b89c00c86
sunjiankun <sunjiankun@ucfgroup.com>
2017-07-11 15:30:02 +0800

    fix

--- apps/loan/jrgc/stores/home.js
+++ apps/loan/jrgc/stores/home.js
@@ -2,6 +2,7 @@ import { extendObservable, computed } from 'mobx'
 import { Components } from 'fw-javascripts'
 import { Storage } from '../../lib/helpers'
 
+import { Browser } from '../../lib/helpers'
 export default class Home {
     constructor(Post) {
         this.Post = Post
@@ -15,9 +16,39 @@ export default class Home {
     }
 
     getDataHandler = () => {
+        this.Post(`/api/product/v1/noticeList.json`)
+            .then(data => {
+                let newBulletinCnt = data.noticeContent;
+                let token = Storage.getUserDict().token;
+
+                // if bulettin is secondary and it's read within the valid token, we just ignore that bulletin
+                this.showBulletin = true; 
+                this.bulletinCnt = newBulletinCnt;
+
+                if (data.gradeType == '2' && Storage.isBulletinRead(token, newBulletinCnt)) return
+                Storage.setBulletin(token, newBulletinCnt);
+
+            })
         this.Post(`/api/product/v1/productList.json`)
             .then(data => {
                 this.loanProductList = data.resultList
             }, e => Components.showToast(e))
+        this.Post(`/api/product/v1/recommendedList.json`)
+            .then(data => {
+                this.subProductList = data.resultList
+            }, e => Components.showToast(e))
     }
+
+    closeBulletin = () => {
+        this.showBulletin = false;
+    }
+
+    gotoHandler = (link, toNative, need_login) => {
+        if (Browser.inFXHApp && toNative) return NativeBridge.toNative(toNative);
+
+        if (link.indexOf('://') < 0) link = location.protocol + '//' + location.hostname + link;
+
+        Browser.inApp ? NativeBridge.goto(link, need_login) : location.href = encodeURI(link);
+    }
+
 }
