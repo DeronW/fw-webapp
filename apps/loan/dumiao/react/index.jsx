4d8ab486615320fb4a2eccb6046a3df82b82c937
彭焱 <pengyan@ucfgroup.com>
2017-07-17 11:21:01 +0800

    update

--- apps/loan/dumiao/react/index.jsx
+++ apps/loan/dumiao/react/index.jsx
@@ -74,13 +74,15 @@ class BorrowMoney extends React.Component {
             $FXH.Post(`${API_PATH}/api/loan/v1/dmStatus.json`)
                 .then(data => {
                     if (data.canStatus == 0) {
-                        this.setState({
-                            canStatus: 0, canMessage: data.canMessage, loanUuid: data.loanUuid
-                        });
+                        // this.setState({
+                        //     canStatus: 0, canMessage: data.canMessage, loanUuid: data.loanUuid
+                        // });
+                        this.setState({loanShow:true, failMsg:"您无法申请读秒借款", title:"提示"});
                     } else if (data.canStatus == 1) {
-                        this.setState({
-                            canStatus: 1, loanUuid: data.loanUuid, canMessage: data.canMessage
-                        });
+                        // this.setState({
+                        //     canStatus: 1, loanUuid: data.loanUuid, canMessage: data.canMessage
+                        // });
+                        this.setState({loanShow:true, failMsg:"您无法申请读秒借款", title:"提示"});
                     } else if (data.canStatus == 2) {
                         this.setState({ canStatus: 2 });
                     }
