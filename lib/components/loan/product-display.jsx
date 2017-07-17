5adc070e41811474cb975888e9a665073e255777
彭焱 <pengyan@ucfgroup.com>
2017-07-17 15:52:55 +0800

    update

--- lib/components/loan/product-display.jsx
+++ lib/components/loan/product-display.jsx
@@ -25,7 +25,7 @@ class ProductDisplay extends React.Component {
 
     gotoHandler = (link, need_login, next_title) => {
         if (link.indexOf('://') < 0) link = location.protocol + '//' + location.hostname + link;
-        $FW.Browser.inApp() ? NativeBridge.goto(link, need_login, next_title) : location.href = encodeURI(link);
+        return $FW.Browser.inApp() ? NativeBridge.goto(link, need_login, next_title) : location.href = encodeURI(link);
     }
 
     render() {
@@ -132,7 +132,7 @@ class ProductDisplay extends React.Component {
 
         let singleProduct = (item, index) => {
             return (
-                <div key={index} onClick={this.gotoHandler(item.productDetailUrl, false, item.productName)} style={_single_product_link}>
+                <div key={index} onClick={this.gotoHandler(item.productDetailUrl,false,item.productName)} style={_single_product_link}>
                      <img src={item.productLogo} style={_product_logo}/>
                      <span style={_product_name}>{item.productName}</span>
                      <span style={_product_label}>{item.productLabelList[0].labelValue}</span>
