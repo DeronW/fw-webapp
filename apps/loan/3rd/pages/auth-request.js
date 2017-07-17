09d6c10e92268587712b5341ebe59f8b8111cce3
liyanzhu <liyanzhu@ucfgroup.com>
2017-07-17 16:32:26 +0800

    Add: auth-request page

--- apps/loan/3rd/pages/auth-request.js
+++ apps/loan/3rd/pages/auth-request.js
@@ -3,17 +3,46 @@ import CSSModules from 'react-css-modules'
 
 import { Header } from '../../lib/components'
 
-import styles from '../css/auth-fail.css'
+import styles from '../css/auth-request.css'
 
 
 @CSSModules(styles, { "allowMultiple": true, "errorWhenNotFound": false })
 class AuthFail extends React.Component {
+
+    state = {
+        captchaImgUrl: require("../images/auth-request/captcha-img-holder.png"),
+        smsBtnText: '获取验证码'
+    }
 
     render() {
         let { history } = this.props;
         return (
             <div>
                 <Header title="授权" history={history} />
+                <div styleName="auth-info">请授权
+                    <span styleName="auth-phone"></span>
+                    登录放心花
+                </div>
+                <div>
+                    <div styleName="cooperation-logo-container">
+                        <i styleName="third-party-logo"></i>
+                        <i styleName="fxh-logo"></i>
+                    </div>
+                    <div styleName="input-field-grp">
+                        <div styleName="input-field">
+                            <i styleName="captcha-icon"></i>
+                            <input styleName="captcha-input" />
+                            <div styleName="captcha-img-container">
+                                <img src={this.state.captchaImgUrl} />
+                            </div>
+                        </div>
+                        <div styleName="input-field">
+                            <i styleName="sms-icon"></i>
+                            <input styleName="sms-input" />
+                            <div styleName="sms-btn">{this.state.smsBtnText}</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
