5527e4814bd4ba91dc2c683ec60ea23f7d2f2bda
彭焱 <pengyan@ucfgroup.com>
2017-07-17 14:52:41 +0800

    update

--- lib/components/loan/product-display.jsx
+++ lib/components/loan/product-display.jsx
@@ -23,6 +23,8 @@ class ProductDisplay extends React.Component {
             }, e => { $FW.Component.Toast(e.message) });
     }
 
+
+
     render() {
 
         let _product_mask = {
@@ -125,9 +127,15 @@ class ProductDisplay extends React.Component {
             borderTop:"1px solid #f0f0f0"
         }
 
+        function gotoHandler(link, toNative, need_login, next_title) {
+            if ($FW.Browser.inFXHApp() && toNative) return NativeBridge.toNative(toNative);
+            if (link.indexOf('://') < 0) link = location.protocol + '//' + location.hostname + link;
+            $FW.Browser.inApp() ? NativeBridge.goto(link, need_login, next_title) : location.href = encodeURI(link);
+        }
+
         let singleProduct = (item, index) => {
             return (
-                <a key={index} href={item.productDetailUrl} style={_single_product_link}>
+                <a key={index} onClick={()=>gotoHandler(item.productDetailUrl, false, item.productName)} style={_single_product_link}>
                      <img src={item.productLogo} style={_product_logo}/>
                      <span style={_product_name}>{item.productName}</span>
                      <span style={_product_label}>{item.productLabelList[0].labelValue}</span>
@@ -139,7 +147,7 @@ class ProductDisplay extends React.Component {
 
         return (
                 <div>
-                    {this.state.show && <div style={_product_mask}>
+                    {this.state.show && <div style={_product_mask} onClick={this.closeHandler}>
                         <div style={_product_popup}>
                             <div style={_product_title}>{this.props.popTitle}</div>
                             <div style={_product_fail_reason}>{this.props.errorMessage}</div>
