3a8db1750612b72564b6dbf1f2850b1ad331b0b8
彭焱 <pengyan@ucfgroup.com>
2017-07-18 20:55:19 +0800

    update

--- apps/loan/apply-confirm/react/components/confirm-loan.jsx
+++ apps/loan/apply-confirm/react/components/confirm-loan.jsx
@@ -73,7 +73,7 @@ class ConfirmLoan extends React.Component{
             if(data.loanStatus == 2 || data.loanStatus == 3){
                 this.setState({codePop:false,loanShow:true,failMsg:data.failReason})
             }else if(data.loanStatus >= 4){
-                clearInterval(this.timer);
+                finishFlag = true
             }else{
                 finishFlag = false
             }
@@ -82,7 +82,7 @@ class ConfirmLoan extends React.Component{
                 if(data.loanStatus == 2 || data.loanStatus == 3){
                     this.setState({codePop:false,loanShow:true,failMsg:data.failReason})
                 }else if(data.loanStatus >= 4){
-                    clearInterval(this.timer);
+                    finishFlag = true
                 }else{
                     finishFlag = false
                 }
