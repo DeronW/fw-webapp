91f56df86ff36564d57d951141fb3d21c04a9c4f
sunjiankun <sunjiankun@ucfgroup.com>
2017-07-18 18:05:17 +0800

    fix

--- apps/loan/jrgc/pages/login.js
+++ apps/loan/jrgc/pages/login.js
@@ -30,11 +30,11 @@ class Login extends React.Component {
             <div styleName="tip">很抱歉，访问此页面暂时出现问题</div>
             <div styleName="tip-img"><img src={require("../images/icon.png")} /></div>
             <div styleName="tip-info">你可点击下方按钮刷新页面<br />或拨打<a href="tel:400-102-0066">400-102-0066</a>联系客服</div>
-            <a styleName="relogin" onClick={() => login.reload_jrgc()}> 刷新 </a>
+            <a styleName="relogin" onClick={login.reload_jrgc}> 刷新 </a>
         </div>
         return <div>
             {!Browser.inJRGCApp && <Header title={'跳转'} />}
-            {show ? noticePanel : ''}
+            {show && noticePanel}
         </div>
     }
 }
