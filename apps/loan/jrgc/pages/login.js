28398c04cd0d7c5eff817f162f5c211a85c1ce7c
delong <w.del@qq.com>
2017-07-18 18:15:01 +0800

    update

--- apps/loan/jrgc/pages/login.js
+++ apps/loan/jrgc/pages/login.js
@@ -15,6 +15,7 @@ class Login extends React.Component {
     state = {
         show: false
     }
+
     componentDidMount() {
         let { login } = this.props;
         login.gotoLogin();
@@ -26,16 +27,16 @@ class Login extends React.Component {
     render() {
         let { login } = this.props;
         let { show } = this.state;
-        let noticePanel = <div styleName="delayPanel" >
+
+        let noticePanel = <div styleName="delayPanel">
             <div styleName="tip">很抱歉，访问此页面暂时出现问题</div>
             <div styleName="tip-img"><img src={require("../images/icon.png")} /></div>
             <div styleName="tip-info">你可点击下方按钮刷新页面<br />或拨打<a href="tel:400-102-0066">400-102-0066</a>联系客服</div>
             <a styleName="relogin" onClick={login.reload_jrgc}> 刷新 </a>
         </div>
-        return <div>
-            {!Browser.inJRGCApp && <Header title={'跳转'} />}
-            {show && noticePanel}
-        </div>
+
+        return show && noticePanel
     }
 }
+
 export default Login
