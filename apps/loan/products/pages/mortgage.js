edabebd8abe56f2afa8d9a440f415020e8feedc9
delong <w.del@qq.com>
2017-07-20 13:26:44 +0800

    add new page

---
+++ apps/loan/products/pages/mortgage.js
@@ -0,0 +1,8 @@
+
+class Mortgage {
+    render() {
+        return <div></div>
+    }
+}
+
+export default Mortgage
--- apps/loan/products/router.js
+++ apps/loan/products/router.js
@@ -6,11 +6,16 @@ import {
     Switch
 } from 'react-router-dom'
 
+import Mortgage from './pages/mortgage.js'
+import MortgageDownload from './pages/mortgage-download.js'
+
 
 export default () => {
     return <Router>
         <Provider>
             <Switch>
+                <Route path='/mortgage' component={Mortgage} />
+                <Route path='/mortgage-download' component={MortgageDownload} />
             </Switch>
         </Provider>
     </Router>
