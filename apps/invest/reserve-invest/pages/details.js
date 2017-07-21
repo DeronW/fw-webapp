8431e0af791644fccc85e751f6627126d6d04a36
fengxiaoran <fengxiaoran@ucfgroup.com>
2017-07-21 10:43:26 +0800

    details

--- apps/invest/reserve-invest/pages/details.js
+++ apps/invest/reserve-invest/pages/details.js
@@ -1,3 +1,15 @@
 import React from 'react'
 import CSSModules from 'react-css-modules'
-import { observer, inject } from 'mobx-react'
+import {observer, inject} from 'mobx-react'
+import Header from '../components/header'
+import styles from '../css/details.css'
+
+@CSSModules(styles, {"allowMultiple": true, "errorWhenNotFound": false})
+class Details extends React.Component {
+    render() {
+        return <div>
+            <Header title="详情" history={history}/>
+        </div>
+    }
+}
+
