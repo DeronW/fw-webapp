6b2d8648f035bbd57ed278b0ce130c3f82a7a4f9
sunjiankun <sunjiankun@ucfgroup.com>
2017-07-21 10:41:46 +0800

    dev

--- apps/invest/reserve-invest/router.js
+++ apps/invest/reserve-invest/router.js
@@ -5,3 +5,19 @@ import {
     Route,
     Switch
 } from 'react-router-dom'
+
+import Details from './pages/details'
+import MyReserve from './pages/my-reserve'
+import SubmitReserve from './pages/submit-reserve'
+
+export default (stores) => {
+    return <Router>
+        <Proviser {...stores}>
+            <Switch>
+                <Route exact path="/details" component={Details} />
+                <Route exact path="/my-reserve" component={MyReserve} />
+                <Route exact path="/submit-reserve" component={SubmitReserve} />
+            </Switch>
+        </Proviser>
+    </Router>
+}
