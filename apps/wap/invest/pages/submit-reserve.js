f0f2b7ada97fad126c13d4702c42d1123b1d6503
sunjiankun <sunjiankun@ucfgroup.com>
2017-07-24 11:40:53 +0800

    dev

--- apps/wap/invest/pages/submit-reserve.js
+++ apps/wap/invest/pages/submit-reserve.js
@@ -9,7 +9,17 @@ import styles from '../css/submit-reserve.css'
 class SubmitReserve extends React.Component{
     render() {
         return <div>
-            <Header title="提交预约" history={history}/>
+            <Header title="提交预约" history={history} show_close={false}/>
+            <div styleName="submitPanel">
+                <div styleName="reserveMoney">预约金额</div>
+                <div styleName="userMoney">
+                    <div styleName="money">可用余额 <span>{`￥${45214}`}</span></div>
+                    <div styleName="inputMoney">
+                        <input type="text" placeholder="50元起投" value=""/>
+                        <span onClick={}>全投</span>
+                    </div>
+                </div>
+            </div>
         </div>
     }
 }
