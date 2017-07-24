c1882293871894985a5ecdc1d18ba2237520b0e3
fengxiaoran <fengxiaoran@ucfgroup.com>
2017-07-24 15:33:47 +0800

    update

--- apps/wap/invest/pages/submit-reserve.js
+++ apps/wap/invest/pages/submit-reserve.js
@@ -5,18 +5,39 @@ import {observer, inject} from 'mobx-react'
 import Header from '../components/header.js'
 import styles from '../css/submit-reserve.css'
 
+@inject('submitReserve')
 @CSSModules(styles, {"allowMultiple": true, "errorWhenNotFound": false})
 class SubmitReserve extends React.Component {
+    componentDidMount() {
+
+    }
+
+    inputChangeHandler = name => e => {
+        console.log(111)
+        this.props.submitReserve.setFormData(name, e.target.value)
+    }
+
+    allMadeHandler = () => {
+        console.log(2222)
+        let {submitReserve} = this.props
+        this.props.submitReserve.setFormData('reserveMoney', submitReserve.currentMoney)
+    }
+
     render() {
+        let {submitReserve} = this.props
         return <div>
             <Header title="提交预约" history={history} show_close={false}/>
             <div styleName="submitPanel">
                 <div styleName="reserveMoney">预约金额</div>
                 <div styleName="userMoney">
-                    <div styleName="money">可用余额 <span>{`￥${45214}`}</span></div>
+                    <div styleName="money">可用余额
+                        <span>{`￥${submitReserve.currentMoney}`}</span>
+                    </div>
                     <div styleName="inputMoney">
-                        <input type="text" placeholder="50元起投" value=""/>
-                        <span>全投</span>
+                        <input type="text" placeholder="50元起投" value={submitReserve.reserveMoney} onChange={this.inputChangeHandler('reserveMoney')}/>
+                        <span styleName="allmadeBtn" onClick={this.allMadeHandler}>
+                            全投
+                        </span>
                     </div>
                 </div>
             </div>
--- apps/wap/invest/stores/submit-reserve.js
+++ apps/wap/invest/stores/submit-reserve.js
@@ -1,10 +1,16 @@
-import { extendObservable,computed } from 'mobx'
+import {extendObservable, computed} from 'mobx'
 
-export default class SubmitReserve{
-    constructor(Post){
+export default class SubmitReserve {
+    constructor(Post) {
         this.Post = Post;
-        extendObservable(this,{
-
+        extendObservable(this, {
+            currentMoney: '45214',//用户账户的现有金额
+            reserveMoney: '',//用户输入的预约金额
         })
+    }
+
+    setFormData = (field, value) => {
+        this[field] = value
+        console.log(this.reserveMoney)
     }
 }
