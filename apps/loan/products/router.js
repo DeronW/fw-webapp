d58fa56afbbf57b327288c792e14ce433a0fc6f6
彭焱 <pengyan@ucfgroup.com>
2017-07-28 18:50:35 +0800

    update

--- apps/loan/products/router.js
+++ apps/loan/products/router.js
@@ -12,22 +12,22 @@ import MortgageOutsideEntry from './pages/mortgage-outside-entry.js'
 import MortgageOutsideApply from './pages/mortgage-outside-apply.js'
 import MortgageApply from './pages/mortgage-apply.js'
 import MortgageSuccess from './pages/mortgage-success.js'
-import LoopLoan from './pages/loop-loan.js'
-import LoopLoanLoan from './pages/loop-loan-loan.js'
-import LoopLoanResult from './pages/loop-loan-result.js'
-import LoopLoanAuthorization from './pages/loop-loan-authorization.js'
-import LoopLoanCard from './pages/loop-loan-card.js'
+import LoopLoan from './pages/loan-youyi-index.js'
+import LoopLoanLoan from './pages/loan-youyi-form.js'
+import LoopLoanResult from './pages/loan-youyi-result.js'
+import LoopLoanAuthorization from './pages/loan-youyi-authorization.js'
+import LoopLoanCard from './pages/loan-youyi-card.js'
 
 export default (stores) => {
     return <Router>
         <Provider {...stores}>
             <Switch>
                 <Route path='/' exact component={Home} />
-                <Route path='/loop-loan' component={LoopLoan} />
-                <Route path='/loop-loan-loan' component={LoopLoanLoan} />
-                <Route path='/loop-loan-result' component={LoopLoanResult} />
-                <Route path='/loop-loan-authorization' component={LoopLoanAuthorization} />
-                <Route path='/loop-loan-card' component={LoopLoanCard} />
+                <Route path='/loan-youyi-index' component={LoopLoan} />
+                <Route path='/loan-youyi-form' component={LoopLoanLoan} />
+                <Route path='/loan-youyi-result' component={LoopLoanResult} />
+                <Route path='/loan-youyi-authorization' component={LoopLoanAuthorization} />
+                <Route path='/loan-youyi-card' component={LoopLoanCard} />
                 <Route path='/mortgage-download' component={MortgageDownload} />
                 <Route path='/mortgage-outside-entry' component={MortgageOutsideEntry} />
                 <Route path='/mortgage-outside-apply' component={MortgageOutsideApply} />
