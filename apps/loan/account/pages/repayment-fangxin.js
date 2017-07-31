2658c33a4ca1eb57b8bfbd74a3b05476d6978eaa
sunjiankun <sunjiankun@ucfgroup.com>
2017-07-31 17:51:26 +0800

    dev

--- apps/loan/account/pages/repayment-fangxin.js
+++ apps/loan/account/pages/repayment-fangxin.js
@@ -2,63 +2,71 @@ import React from 'react'
 import CSSModules from 'react-css-modules'
 import { observer, inject } from 'mobx-react'
 
-import { Utils } from 'fw-javascripts'
-
 import { Header } from '../../lib/components'
 import styles from '../css/repayment-fangxin.css'
 
-@inject()
+@inject("repayment_fangxin")
 @observer
 @CSSModules(styles, { allowMultiple: true, errorWhenNotFound: false })
 class RepaymentFangXin extends React.Component {
 
     componentDidMount() {
-        document.title = "还款明细"
+        document.title = "还款明细";
+        let { repayment_fangxin } = this.props;
+        repayment_fangxin.repaymentHandler();
     }
-    inputAmountHandler = () => {
-        
+
+    inputAmountHandler = () => (e) => {
+        let { repayment_fangxin } = this.props;
+        repayment_fangxin.setLoanAmount(e.target.value)
     }
+
+    allAmountHandler = (value) => () => {
+        let { repayment_fangxin } = this.props;
+        repayment_fangxin.setLoanAmount(value)
+    }
+    
     render() {
-        let { history } = this.props;
+        let { history, repayment_fangxin } = this.props;
         return <div styleName="repayment">
             <Header title="还款明细" history={history} />
             <div styleName="banner">
-                <img src={require("../images/repayment-fxh/ue.png")} alt=""/>
+                <img src={repayment_fangxin.logo} alt="" />
                 <span>优易借</span>
             </div>
             <div styleName="amount">
                 <div styleName="money amountMoney">
-                    <div styleName="amountNum">10000</div>
+                    <div styleName="amountNum">{repayment_fangxin.loanLeftAmount}</div>
                     <div styleName="amountName">待还金额(元)</div>
                 </div>
                 <div styleName="money amoutLate">
-                    <div styleName="amountNum">23.2</div>
+                    <div styleName="amountNum">{repayment_fangxin.overdueFee}</div>
                     <div styleName="amountName">逾期费(元)</div>
                 </div>
             </div>
             <div styleName="amountPanel">
                 <div styleName="amountItem">
                     <div styleName="itemName">还款日</div>
-                    <div styleName="itemTime">2017-04-26</div>
+                    <div styleName="itemTime">{repayment_fangxin.dueTime}</div>
                 </div>
                 <div styleName="amountItem">
                     <div styleName="itemName">已还金额</div>
-                    <div styleName="itemAlready">1002.00
-                        <img src={require("../images/repayment-fxh/entry.png")} alt=""/>
+                    <div styleName="itemAlready">{repayment_fangxin.repaymentAmount}
+                        <img src={require("../images/repayment-fxh/entry.png")} alt="" />
                     </div>
                 </div>
-                
+
             </div>
             <div styleName="amountPanel">
                 <div styleName="amountItem">
                     <div styleName="itemName">选择银行卡</div>
-                    <div styleName="itemAlready">工商银行(2333)
-                        <img src={require("../images/repayment-fxh/entry.png")} alt=""/>
+                    <div styleName="itemAlready">{`${repayment_fangxin.withdrawBankShortName}(${repayment_fangxin.withdrawCardNo})`}
+                        <img src={require("../images/repayment-fxh/entry.png")} alt="" />
                     </div>
                 </div>
                 <div styleName="amountItem">
-                    <input styleName="itemInput" type="text" placeholder="输入还款金额" value="" onChange={this.inputAmountHandler()}/>
-                    <div styleName="itemAll">全部还清</div>
+                    <input styleName="itemInput" type="number" placeholder="输入还款金额" value={repayment_fangxin.inputAmount} onChange={this.inputAmountHandler()} />
+                    <div styleName="itemAll" onClick={this.allAmountHandler(repayment_fangxin.loanLeftAmount)}>全部还清</div>
                 </div>
             </div>
             <div styleName="amountBottom">
