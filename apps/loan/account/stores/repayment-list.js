8e09e0c53be65869671b3ca37baf0abf86e1252b
刘桃桃 <liutaotao@ucfgroup.com>
2017-07-31 16:47:26 +0800

    clear the unnecessary annotation

--- apps/loan/account/stores/repayment-list.js
+++ apps/loan/account/stores/repayment-list.js
@@ -12,19 +12,10 @@ export default class RepaymentList {
         })
     }
     getRepaymentList = () => {
-        // let USER = Storage.getUserDict();
-        // this.uid = USER.uid;
-        // this.userGid = USER.Gid;
-
-
         this.Post(`/api/order/v1/orderList.json`, {
-            // loopLoanUuid: this.loopLoanUuid,
             page: this.page,
             pageSize: 10,
-            // uid: this.uid,
-            // userGid: this.userGid,
             loanStatus: 2
-
         }).then(data => {
             this.resultList = data.resultList
         })
