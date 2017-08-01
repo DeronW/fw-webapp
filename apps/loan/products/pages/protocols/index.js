c0554063c26f5699f3582e2ed189c76a7265425a
彭焱 <pengyan@ucfgroup.com>
2017-08-01 17:17:10 +0800

    update

--- apps/loan/products/pages/protocols/index.js
+++ apps/loan/products/pages/protocols/index.js
@@ -5,6 +5,10 @@ import Cost from './cost.js'
 import Dumiao from './dumiao.js'
 import Partner from './partner.js'
 import InfoCollect from './info-collect.js'
+import YouyiLoan from './youyi-loan'
+import YouyiLoanService from './youyi-loan-service'
+import YouyiRepayment from './youyi-repayment'
+import YouyiRepaymentService from './youyi-repayment-service'
 
 export {
     Register,
@@ -12,5 +16,10 @@ export {
     Cost,
     Dumiao,
     Partner,
-    InfoCollect
+    InfoCollect,
+    YouyiLoan,
+    YouyiLoanService,
+    YouyiRepayment,
+    YouyiRepaymentService
+
 }
