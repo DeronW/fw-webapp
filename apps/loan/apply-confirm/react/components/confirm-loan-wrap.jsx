c6e46019ea6a553156caa66e0aeadc224b3d7d4f
彭焱 <pengyan@ucfgroup.com>
2017-08-02 12:08:11 +0800

    update

--- apps/loan/apply-confirm/react/components/confirm-loan-wrap.jsx
+++ apps/loan/apply-confirm/react/components/confirm-loan-wrap.jsx
@@ -83,16 +83,6 @@ class ConfirmLoanWrap extends React.Component{
                                                feeExtList={this.props.feeExtList} /> : null}
             {this.state.noticeShow ?
                 <Notice content={this.props.latedescription} callbackNoticeHide={this.noticeHide} /> : null}
-            {/*{this.state.verifyCodeShow ?*/}
-                {/*<VerifyCode callbackCloseHanler={this.closeHandler} callbackResultShow={this.resultShow}*/}
-                            {/*bankShortName={filtered[0].bankShortName} cardNo={filtered[0].cardNo}*/}
-                            {/*callbackGetLoanResultSuccess={this.getLoanResultSuccess}*/}
-                            {/*callbackGetLoanResultFail={this.getLoanResultFail}*/}
-                            {/*callbackGetLoanResultCheck={this.getLoanResultCheck}*/}
-                            {/*reSetState={this.reGetState}*/}
-                            {/*stateProps={this.state.reSetState}*/}
-                {/*/> : null}*/}
-
             {this.state.loanResult ?
                 <LoanResult callbackResultHide={this.resultHide} bankShortName={filtered[0].bankShortName}
                             cardNo={filtered[0].cardNo} success={this.state.successResult}
