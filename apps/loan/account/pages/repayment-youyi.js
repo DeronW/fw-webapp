0f4daa0c189aedd28332bd63f08c78c4fff2e901
liyanzhu <liyanzhu@ucfgroup.com>
2017-08-04 10:46:17 +0800

    minify

--- apps/loan/account/pages/repayment-youyi.js
+++ apps/loan/account/pages/repayment-youyi.js
@@ -190,13 +190,6 @@ class Repayment extends React.Component {
                     {amountEditItem}
                 </div>
 
-                {/* <div className={protocolChecked ? styles['checked-protocol'] : styles['unchecked-protocol']}
-                    onClick={this.toggleProtocol}>
-                    同意
-                    <a href="/static/loan/products/index.html#/protocols/youyi-repayment">《委托扣款授权书（还款）》</a>、
-                    <a href="/static/loan/products/index.html#/protocols/youyi-repayment-service">《委托扣款授权书（支付服务费）》</a>
-                </div> */}
-
                 <div styleName="submit-btn-container">
                     <a styleName="submit-btn"
                         onClick={this.handleSubmit}>
