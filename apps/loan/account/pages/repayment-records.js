49d7f5152dbcd9bae5fdb764b371922001a1c87d
刘桃桃 <liutaotao@ucfgroup.com>
2017-08-07 18:11:52 +0800

    same

--- apps/loan/account/pages/repayment-records.js
+++ apps/loan/account/pages/repayment-records.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import CSSModules from 'react-css-modules'
-import {observer, inject} from 'mobx-react'
-import {Redirect} from 'react-router'
-import {Link} from 'react-router-dom'
-import {Header, BottomNavBar} from '../../lib/components'
-import {Browser, Post, NativeBridge} from '../../lib/helpers'
-import {Event, Utils} from 'fw-javascripts'
+import { observer, inject } from 'mobx-react'
+import { Redirect } from 'react-router'
+import { Link } from 'react-router-dom'
+import { Header, BottomNavBar } from '../../lib/components'
+import { Browser, Post, NativeBridge } from '../../lib/helpers'
+import { Event, Utils } from 'fw-javascripts'
 
 import styles from '../css/repayment-records.css'
 
@@ -26,52 +26,46 @@ export default class RepaymentRecords extends React.Component {
     componentDidMount() {
         document.title = '还款';
         NativeBridge.setTitle("还款");
-        this
-            .loadMoreHandler(null)
-            .then(() => {
+        this.loadMoreHandler(null).then(() => {
 
-                Event.touchBottom(this.loadMoreHandler);
-            });
+        Event.touchBottom(this.loadMoreHandler);
+        });
         // let {repayment_youyi} = this.props; Post(`/api/order/v1/orderList.json`, {
-        // page: this.state.curPage,     pageSize: 10,     loanStatus: 2 }).then(data =>
-        // {     this.setState({resultList: data.resultList}); })
+        //  page: this.state.curPage,     pageSize: 10,     loanStatus: 2 }).then(data
+        // => {     this.setState({resultList: data.resultList}); })
 
     }
     componentWillUnmount() {
         Event.cancelTouchBottom();
     }
     loadMoreHandler = (done) => {
-        let {resultList, curPage} = this.state;
+        let { resultList, curPage } = this.state;
 
-        if (curPage === 0) 
+        if (curPage === 0)
             return done && done();
-        
+
         Post(`/api/order/v1/orderList.json`, {
             pageSize: 10,
             page: curPage,
             loanStatus: 2
         }).then(data => {
             resultList.push(...data.resultList)
-            this.setState({
-                curPage: (curPage < data.totalPage
-                    ? curPage + 1
-                    : 0)
-            })
-            this.setState({resultList: resultList});
+            this.setState({ curPage: (curPage < data.totalPage ? curPage + 1 : 0) })
+            this.setState({ resultList: resultList });
 
             done && done()
         })
     }
     toRepaymentDetail = (productId, uuid, loanGid) => () => {
-        let {repayment_youyi, repayment_fangxin, history} = this.props;
+        let { repayment_youyi, repayment_fangxin, history } = this.props;
         // 根据返回的productId跳转到不同的还款页面
         productId == '1' && history.push(`/repayment-fangxin?id=${loanGid}`);
         productId == '21' && history.push(`/repayment-fenqi?id=${uuid}`);
         productId == '11' && history.push(`/repayment-youyi?id=${uuid}`);
     }
     render() {
-        let {history} = this.props;
-        let {resultList} = this.state;
+        let { history } = this.props;
+        let { resultList } = this.state;
         let repayment_item = (item, index) => {
             return <div styleName="item-self" key={index}>
                 <div styleName="top">
@@ -106,17 +100,17 @@ export default class RepaymentRecords extends React.Component {
             </div>
         }
         let noData = <div styleName="no-data">
-            <img src={require("../images/no-data.png")} alt="" styleName="blank-img"/>
+            <img src={require("../images/no-data.png")} alt="" styleName="blank-img" />
             <p styleName="blank-text">暂无数据</p>
         </div>
 
         return <div styleName="bg">
-            <Header title="还款" history={history}/> {/*内容部分*/}
+            <Header title="还款" history={history} /> {/*内容部分*/}
             <div styleName="repayment-content">
                 {resultList.length > 0 && resultList.map(repayment_item)}
             </div>
             {resultList.length == 0 && noData}
-            <BottomNavBar history={history}/>
+            <BottomNavBar history={history} />
         </div>
     }
 }
