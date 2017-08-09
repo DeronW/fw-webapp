6d9f5a57cfb4d353df1c7ee191c621892b91cc5f
delong <w.del@qq.com>
2017-08-09 18:14:07 +0800

    update

--- apps/loan/lib/components/header.js
+++ apps/loan/lib/components/header.js
@@ -2,15 +2,37 @@ import React from 'react'
 import CSSModules from 'react-css-modules'
 import styles from '../css/header.css'
 import { Theme } from '../helpers'
-import { Browser } from '../helpers/'
+import { Browser, NativeBridge } from '../helpers/'
+
+
+function goBack(props) {
+    let old_pop = window.onpopstate
+
+    let moved = false
+    window.onpopstate = () => moved = true
+
+    if (props.goBack) {
+        props.goBack()
+    } else {
+        props.history && props.history.goBack()
+    }
+
+    setTimeout(() => {
+        window.onpopstate = old_pop
+        !moved && NativeBridge.close()
+    }, 100)
+}
 
 const Header = CSSModules(styles, {
     "allowMultiple": true,
     "errorWhenNotFound": false
 })(props => {
-    if (!Theme.get('header') && props.enable != 'force') return null;
 
-    let goBack = props.goBack || (props.history && props.history.goBack)
+    if (!Theme.get('header') && props.enable != 'force') {
+        return null
+    } else {
+        NativeBridge.trigger('hide_header')
+    }
 
     return <div styleName={Browser.inIOSApp ?
         "header-placeholder iosapp-header-placeholder" : "header-placeholder"} >
