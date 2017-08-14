92d3577f982791dfcb4d0ff3941500077fddaac0
delong <w.del@qq.com>
2017-08-14 12:00:02 +0800

    update

--- apps/wap/p2p/pages/not-found.js
+++ apps/wap/p2p/pages/not-found.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import CSSModules from 'react-css-modules'
+import { observer, inject } from 'mobx-react'
+import { Header } from '../components'
+import styles from '../css/not-found.css'
 
+
+@CSSModules(styles, {
+    allowMultiple: true,
+    errorWhenNotFound: false
+})
 class NotFound extends React.Component {
 
     render() {
-        return <div>
-            <a>
-                404
-            </a>
+        return <div styleName="bg">
+            <Header title="404" history={this.props.history} />
+            <img styleName="img" src={require('../images/not-found/404bg.png')} />
+            <div styleName="txt">404</div>
+            <div styleName="txt">找不到页面了</div>
         </div>
     }
 }
