08f14fb76973009262c9bb7d95d6b24416f6da4a
liyanzhu <liyanzhu@ucfgroup.com>
2017-08-15 12:33:33 +0800

    Fix

--- apps/loan/3rd/pages/fail.js
+++ apps/loan/3rd/pages/fail.js
@@ -31,7 +31,7 @@ class AuthFail extends React.Component {
 
         return (
             <div>
-                <Header title="授权失败" history={history} />
+                <Header title="联系客服" history={history} />
                 <div styleName="info-container">
                     <img styleName="info-img" src={require("../images/fail/auth-fail.png")}/>
                     <div styleName="info-text">很抱歉，访问此页面暂时出现问题</div>
