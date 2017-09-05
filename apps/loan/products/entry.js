557573b3eae81f45f2adca3347019b2e183e6e50
彭焱 <pengyan@ucfgroup.com>
2017-09-05 17:58:30 +0800

    update

--- apps/loan/products/entry.js
+++ apps/loan/products/entry.js
@@ -1,17 +1,13 @@
 import { render } from 'react-dom'
 import AppRouter from './router.js'
-
 import './css/index.css'
-
 import Stores from './stores'
 import { Post, StoreSpy } from '../lib/helpers'
-
 let stores = {
     mortgage: new Stores.Mortgage(Post),
     loopLoan: new Stores.LoopLoan(Post),
     fxh: new Stores.Fxh(Post)
 }
-
 new StoreSpy(stores).wiretap()
-
 render(AppRouter(stores), document.getElementById('cnt'))
+
