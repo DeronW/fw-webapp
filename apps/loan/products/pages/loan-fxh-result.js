90436340f9e01e2b95e92daca78b3e335d97047f
刘桃桃 <liutaotao@ucfgroup.com>
2017-09-13 15:37:07 +0800

    test

--- apps/loan/products/pages/loan-fxh-result.js
+++ apps/loan/products/pages/loan-fxh-result.js
@@ -65,7 +65,7 @@ export default class FxhResult extends React.Component {
                         setTimeout(() => {
                             Browser.inApp ? NativeBridge.goto(`${data.activityRecomUrl}`,false,"放心花"):
                             location.href  = `${data.activityRecomUrl}`;
-                        },2000)
+                        },12000)
                     }
                 // setTimeout(() => {
                 //     Browser.inApp ? NativeBridge.goto(`https://m.easyloan888.com/static/loan/features/index.html#/invite-activity?yqm=F172001`,false,"放心花"):
