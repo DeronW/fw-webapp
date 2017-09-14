ebe2abd92fff016b3813122088559a0346537035
sunjiankun <sunjiankun@ucfgroup.com>
2017-09-14 17:59:27 +0800

    update

--- apps/manager/jrgc/pages/home.js
+++ apps/manager/jrgc/pages/home.js
@@ -2,13 +2,15 @@ import React from 'react'
 import CSSModules from 'react-css-modules'
 import { observer, inject } from 'mobx-react'
 
+import { Header } from '../components';
 import styles from '../css/home.css'
 
-@CSSModules(styles,{ "allowMultiple": true, "errorWhenNotFound": false })
-class Home extends React.Component{
-    render(){
+@CSSModules(styles, { "allowMultiple": true, "errorWhenNotFound": false })
+class Home extends React.Component {
+    render() {
         return <div>
-            Home
+            <Header title="我的" history={history} />
+            <div>Home</div>
         </div>
     }
 }
