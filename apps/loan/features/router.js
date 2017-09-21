214ab66c867ecab568600805d2435a02de9881b8
delong <w.del@qq.com>
2017-09-21 11:28:42 +0800

    format

--- apps/loan/features/router.js
+++ apps/loan/features/router.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {Provider} from 'mobx-react'
-import {HashRouter as Router, Route, Switch} from 'react-router-dom'
+import { Provider } from 'mobx-react'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 
 import More from './pages/more.js'
 import AboutUs from './pages/about-us.js'
@@ -17,23 +17,23 @@ import JrgcappDownload from './pages/jrgcapp-download.js'
 import WeixinInvite from './pages/weixin-invite.js'
 
 
-export default(stores) => {
+export default (stores) => {
     return <Router>
         <Provider {...stores}>
             <Switch>
-                <Route exact path='/waiting' component={Waiting}/>
-                <Route exact path='/more' component={More}/>
-                <Route exact path='/about-us' component={AboutUs}/>
-                <Route exact path='/faq' component={FAQ}/>
-                <Route exact path='/market-app' component={MarketApp}/>
-                <Route exact path='/contact-us' component={ContactUs}/>
-                <Route exact path='/credit-cards' component={CreditCards}/>
-                <Route exact path='/not-found' component={NotFound}/>
-                <Route exact path='/download' component={Download}/>
-                <Route exact path='/invite-activity' component={InviteActivity}/>
-                <Route exact path='/follow-fxh' component={FxhappDownload}/>
-                <Route exact path='/follow-jrgc' component={JrgcappDownload}/>
-            <Route exact path='/weixin-invite' component={WeixinInvite}/>
+                <Route exact path='/waiting' component={Waiting} />
+                <Route exact path='/more' component={More} />
+                <Route exact path='/about-us' component={AboutUs} />
+                <Route exact path='/faq' component={FAQ} />
+                <Route exact path='/market-app' component={MarketApp} />
+                <Route exact path='/contact-us' component={ContactUs} />
+                <Route exact path='/credit-cards' component={CreditCards} />
+                <Route exact path='/not-found' component={NotFound} />
+                <Route exact path='/download' component={Download} />
+                <Route exact path='/invite-activity' component={InviteActivity} />
+                <Route exact path='/follow-fxh' component={FxhappDownload} />
+                <Route exact path='/follow-jrgc' component={JrgcappDownload} />
+                <Route exact path='/weixin-invite' component={WeixinInvite} />
             </Switch>
         </Provider>
     </Router>
