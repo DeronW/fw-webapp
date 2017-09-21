8cd61c8d36f706b03f7c44b76eb9a4605d702b72
liyanzhu <liyanzhu@ucfgroup.com>
2017-09-21 17:14:11 +0800

    Add: performance page

--- apps/manager/jrgc/router.js
+++ apps/manager/jrgc/router.js
@@ -7,12 +7,14 @@ import {
 } from 'react-router-dom'
 
 import { Home } from './pages'
+import * as Performance from './pages/performance'
 
 export default (stores) => {
     return <Router>
         <Provider {...stores}>
             <Switch>
                 <Route exact path="/" component={Home}/>
+                <Route exact path="/performance" component={Performance.Performance}/>
             </Switch>
         </Provider>
     </Router>
