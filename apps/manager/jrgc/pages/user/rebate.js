939a35cbc29a1e21d36460585ab7a66a09c7cf0e
fengxiaoran <fengxiaoran@ucfgroup.com>
2017-09-22 10:49:33 +0800

    update

--- apps/manager/jrgc/pages/user/rebate.js
+++ apps/manager/jrgc/pages/user/rebate.js
@@ -19,15 +19,63 @@ class Rebate extends React.Component {
 
         let tabs = ['全部', '微金', '尊享', '黄金']
         let tab_func = (item, index) => {
-            return <div key={index}
-                        styleName={tab_num == index ? "tab tabActive" : "tab"}
-                        onClick={() => this.switchTabHandler(index)}>{item}
+            return <div key={index} styleName={tab_num == index ? "tab tabActive" : "tab"}
+                        onClick={() => this.switchTabHandler(index)}>
+                {item}
             </div>
         }
 
         let all_section = () => {
             return <div>
-                this is all section
+                <div styleName="allChart"></div>
+                <div styleName="allData">
+                    <div styleName="dataLine">
+                        <div styleName="lineItem">
+                            <div styleName="name">总返利</div>
+                            <div styleName="count">¥100,000,000.00</div>
+                        </div>
+                        <div styleName="lineItem">
+                            <div styleName="name">今日返利</div>
+                            <div styleName="count">¥4.06</div>
+                        </div>
+                    </div>
+                    <div styleName="dataLine lastLine">
+                        <div styleName="lineItem">
+                            <div styleName="name">已发返利</div>
+                            <div styleName="count">¥4.06</div>
+                        </div>
+                        <div styleName="lineItem">
+                            <div styleName="name">待发返利</div>
+                            <div styleName="count">¥4.06</div>
+                        </div>
+                    </div>
+                </div>
+                <div styleName="users">
+                    <div styleName="userItem">
+                        <div styleName="itemDetail">
+                            <div styleName="detailLine">
+                                <div styleName="detailLeft">李丽华</div>
+                                <div styleName="detailRight">¥7000.00</div>
+                            </div>
+                            <div styleName="detailLine">
+                                <div styleName="detailLeft userDes">利随享28930 | 8.5% | 48天</div>
+                                <div styleName="detailRight userDate">2017-08-13 00:00:00</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div styleName="userItem">
+                        <div styleName="itemDetail">
+                            <div styleName="detailLine">
+                                <div styleName="detailLeft">李丽华</div>
+                                <div styleName="detailRight">¥7000.00</div>
+                            </div>
+                            <div styleName="detailLine">
+                                <div styleName="detailLeft userDes">利随享28930 | 8.5% | 48天</div>
+                                <div styleName="detailRight userDate">2017-08-13 00:00:00</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         }
 
