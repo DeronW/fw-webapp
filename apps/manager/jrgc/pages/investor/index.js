fa855007f572c1a0091999791d76b64f0bc12d26
sunjiankun <sunjiankun@ucfgroup.com>
2017-09-27 14:31:00 +0800

    update

--- apps/manager/jrgc/pages/investor/index.js
+++ apps/manager/jrgc/pages/investor/index.js
@@ -1,10 +1,12 @@
 import Investor from './investor'
+import Search from './search'
 import Info from './info'
 import Score from './score'
 import Bean from './bean'
 
 export {
     Investor,
+    Search,
     Info,
     Score,
     Bean
--- apps/manager/jrgc/router.js
+++ apps/manager/jrgc/router.js
@@ -33,6 +33,7 @@ export default (stores) => {
 
                 {/*客户相关页面*/}
                 <Route exact path="/investor" component={Investor.Investor}/>
+                <Route exact path="/investor-search" component={Investor.Search}/>
                 <Route exact path="/investor-info" component={Investor.Info}/>
                 <Route exact path="/investor-score" component={Investor.Score}/>
                 <Route exact path="/investor-bean" component={Investor.Bean}/>
