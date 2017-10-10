cd238e21a2e3549d3b397d3133f6bc9323632180
fengxiaoran <fengxiaoran@ucfgroup.com>
2017-10-10 16:22:23 +0800

    update

--- apps/manager/jrgc/stores/user.js
+++ apps/manager/jrgc/stores/user.js
@@ -1,31 +1,49 @@
-import { extendObservable } from 'mobx'
+import {extendObservable} from 'mobx'
 
 export default class User {
-    constructor() {
+    constructor(Post) {
+        this.Post = Post
         this.data = {}
         extendObservable(this.data, {
             user: {
                 notice: [],
-                banners:[]
+                banners: []
+            },
+            coupon: {
+                totalCount: null,
+                couponList: [],
             }
         })
     }
+
     fetchNotice = () => {
         this.data.user.notice = [{
             des: '第三方发',
-            url:'https://www.baidu.com/?tn=78040160_5_pg&ch=1'
+            url: 'https://www.baidu.com/?tn=78040160_5_pg&ch=1'
         }, {
             des: '电话号给的图',
-            url:'https://www.360.cn/'
+            url: 'https://www.360.cn/'
         }, {
             des: '阿森松岛',
-            url:'https://www.baidu.com/'
+            url: 'https://www.baidu.com/'
         }]
 
         this.data.user.banners = [
             {
-                img:''
+                img: ''
             }
         ]
+    }
+
+    fetchCouponList = (couponStatus, couponType) => {
+        this.Post('/api/finManager/coupon/v2/myCouponList.shtml', {
+            couponStatus: couponStatus,
+            couponType: couponType,
+            pageNo: 1,
+            pageSize: 10
+        }).then(data => {
+            this.data.coupon.totalCount = data.moneySum
+            this.data.coupon.couponList = data.pageData.result
+        })
     }
 }
