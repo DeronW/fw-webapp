ac1406e8383d5bcb30357e8bc6dd617d1e0391f8
delong <w.del@qq.com>
2017-10-16 09:41:55 +0800

    update

--- apps/loan/3rd/pages/outside/register.js
+++ apps/loan/3rd/pages/outside/register.js
@@ -18,11 +18,17 @@ class Register extends React.Component {
 
     state = {
         phone: '',
-        captcha: ''
+        captcha: '',
+        sms_code: '',
+        invite_code: '',
+        pwd: '',
+
+        captcha_url: '',
+        verifyToken: ''
     }
 
     componentDidMount() {
-
+        this.refreshCaptcha()
     }
 
     changeHandler = field_name => e => {
@@ -33,9 +39,19 @@ class Register extends React.Component {
         this.setState((prevState, _) => ({ [name]: '' }))
     }
 
+    refreshCaptcha = () => {
+        captcha_url
+    }
+
+    registerHandler = () => {
+        let err
+
+        err ? Components.showToast(err) : Post
+    }
+
     render() {
 
-        let { phone, captcha } = this.state
+        let { phone, captcha, sms_code, pwd, invite_code } = this.state
 
         let empty_btn = field_name => {
             if (!this.state[field_name]) return null;
@@ -58,25 +74,36 @@ class Register extends React.Component {
                     <img styleName="icon"
                         src={require('../../images/outside/register/icon-code.png')} />
                     <input placeholder="请输入图片验证码" maxLength="6"
-                        value={captcha} />
+                        value={captcha}
+                        onChange={this.changeHandler('captcha')}
+                    />
+                    <img onClick={this.refreshCaptcha} />
                 </div>
                 <div styleName="field">
                     <img styleName="icon"
                         src={require('../../images/outside/register/icon-code.png')} />
-                    <input placeholder="请输入短信验证码" type="number" maxLength="11" value={phone} />
+                    <input placeholder="请输入短信验证码" type="number" maxLength="11"
+                        onChange={this.changeHandler('sms_code')}
+                        value={sms_code} />
                 </div>
                 <div styleName="field">
                     <img styleName="icon"
                         src={require('../../images/outside/register/icon-pwd.png')} />
-                    <input placeholder="请输入密码，8-16位数字字母组合" type="password" value={phone} />
+                    <input placeholder="请输入密码，8-16位数字字母组合"
+                        type="password"
+                        onChange={this.changeHandler('pwd')}
+                        value={pwd} />
                 </div>
                 <div styleName="field">
                     <img styleName="icon"
                         src={require('../../images/outside/register/icon-invite.png')} />
-                    <input placeholder="请输入邀请码" value={phone} />
+                    <input placeholder="请输入邀请码" value={invite_code}
+                        onChange={this.changeHandler('invite_code')}
+                    />
                 </div>
+                <a styleName="btn-register" onClick={this.registerHandler}>立即领钱</a>
             </div>
-            <a styleName="login" href="/static/loan/account/index.html#/entry">已有账号？立即登录 &gt;&gt;</a>
+            <a styleName="btn-login" href="/static/loan/account/index.html#/entry">已有账号？立即登录 &gt;&gt;</a>
         </div>
     }
 }
