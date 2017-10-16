fc2e5c405f2fdc8494812da575ce5c0879238d25
fengxiaoran <fengxiaoran@ucfgroup.com>
2017-10-16 15:04:33 +0800

    update

--- apps/manager/jrgc/stores/investor-account.js
+++ apps/manager/jrgc/stores/investor-account.js
@@ -6,13 +6,15 @@ export default class InvestorAccount {
         this.Post = Post
         this.data = {}
         extendObservable(this.data, {
+            id: null,
             zx: {
                 info: {},
                 type: '0',
                 payments: {
                     '0': {name: '未回', page_no: 1, list: []},
                     '1': {name: '已回', page_no: 1, list: []}
-                }
+                },
+                payments_count: null
             },
             p2p: {
                 info: {}
@@ -27,7 +29,6 @@ export default class InvestorAccount {
                 },
                 goldPrice: '',
                 amount: {},
-                id: null,
                 totalCount: null
             }
         })
@@ -51,11 +52,11 @@ export default class InvestorAccount {
     //黄金记录列表和累计黄金
     fetchGoldList = (done) => {
         const PAGE_SIZE = 10
-        let {type, records, id} = this.data.hj, current_record = records[type]
+        let {type, records} = this.data.hj, current_record = records[type]
         if (current_record.page_no === 0) return done && done()
         if (current_record.page_no === 1) current_record.list.splice(0, current_record.list.length)
         this.Get('/api/finManager/cust/v2/enjoyGold.shtml', {
-            custId: id,
+            custId: this.data.id,
             orderStatusCode: type,
             pageNo: current_record.page_no,
             pageSize: PAGE_SIZE
@@ -71,7 +72,7 @@ export default class InvestorAccount {
     }
     //获取客户id
     getCustId = (id) => {
-        this.data.hj.id = id
+        this.data.id = id
     }
     //重置type
     resetGoldListType = (status) => {
@@ -86,7 +87,7 @@ export default class InvestorAccount {
     //获取微金账户信息页
     fetchAccountP2P = (custId) => {
         this.Get('/api/finManager/cust/v2/wjAccount.shtml', {
-            custId: custId
+            custId: this.data.id
         }).then(data => {
             this.data.p2p.info = data.result
         })
@@ -96,9 +97,42 @@ export default class InvestorAccount {
     //尊享账户信息页
     fetchAccountZX = (custId) => {
         this.Get('/api/finManager/cust/v2/zxAccount.shtml', {
-            custId: custId
+            custId: this.data.id
         }).then(data => {
             this.data.zx.info = data.result
         })
+    }
+
+    //尊享  他的回款明细
+    fetchZXPayment = (done) => {
+        let {type, payments} = this.data.zx, current_payment = payments[type]
+        const PAGE_SIZE = 10
+        if (current_payment.page_no === 0) return done && done()
+        if (current_payment.page_no == 1) current_payment.list.splice(0, current_payment.list.length)
+        this.Get('/api/finManager/cust/v2/zxPayment.shtml', {
+            custId: this.data.id,
+            pageNo: current_payment.page_no,
+            pageSize: PAGE_SIZE,
+            status: type
+        }).then(data => {
+            this.data.zx.payments_count = data.pageData.pagination.totalCount
+            current_payment.list.push(...data.result)
+            current_payment.page_no < data.pageData.pagination.totalPage
+                ? current_payment.page_no++
+                : current_payment.page_no = 0
+            done && done()
+        })
+    }
+
+    //重置尊享回款明细的状态
+    resetZXPaymentType = (type) => {
+        this.data.zx.type = type
+        this.fetchZXPayment()
+    }
+
+    //充值尊享回款明细的页码
+    resetZXPaymentPageNo = () => {
+        let {type, payments} = this.data.zx, current_payment = payments[type]
+        current_payment.page_no = 1
     }
 }
