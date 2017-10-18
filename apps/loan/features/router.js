062153bc3e7b83691ee997a4a4abaef2ecf3f450
彭焱 <pengyan@ucfgroup.com>
2017-10-18 18:14:28 +0800

    update

--- apps/loan/features/router.js
+++ apps/loan/features/router.js
@@ -13,7 +13,6 @@ import Download from './pages/download.js'
 import Waiting from './pages/waiting.js'
 import FollowFXH from './pages/follow-fxh.js'
 import FollowJRGC from './pages/follow-jrgc.js'
-import InviteActivity from './pages/invite-activity.js'
 
 export default (stores) => {
     return <Router>
@@ -30,7 +29,6 @@ export default (stores) => {
                 <Route exact path='/download' component={Download} />
                 <Route exact path='/follow-fxh' component={FollowFXH} />
                 <Route exact path='/follow-jrgc' component={FollowJRGC} />
-                <Route exact path='/invite-activity' component={InviteActivity} />
             </Switch>
         </Provider>
     </Router>
