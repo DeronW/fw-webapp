822fa414f142093ffadef707f7c356831f3d0cb1
sunjiankun <sunjiankun@ucfgroup.com>
2017-10-20 14:32:01 +0800

    add notice

--- apps/manager/jrgc/helpers/index.js
+++ apps/manager/jrgc/helpers/index.js
@@ -1,8 +1,9 @@
-import { Get,Post } from './request.js'
+import { Ajax,Get,Post } from './request.js'
 import StoreSpy from './store-spy.js'
 import NativeBridge from './native-bridge.js'
 
 export {
+    Ajax,
     Get,
     Post,
     NativeBridge,
