02a566f10d20f6f248d2b9b4f3f0b07874614f35
sunjiankun <sunjiankun@ucfgroup.com>
2017-10-23 15:56:47 +0800

    update

--- apps/manager/jrgc/pages/investor/birthday.js
+++ apps/manager/jrgc/pages/investor/birthday.js
@@ -36,21 +36,7 @@ class Birthday extends React.Component {
         return <div styleName="bg">
             <Header title="近期过生日的客户" history={history} />
             <div styleName="birthdayList">
-                {/*birthday.list.map(birFn)*/}
-                <div styleName="person" onClick={() => this.gotoHandler()}>
-                    <img src={require('../../images/investor/birthday/clock.png')} />
-                    <div styleName="name">大熊</div>
-                    <div styleName="date">(9月20日)</div>
-                    <img styleName="arrow" src={require('../../images/investor/birthday/arrow.png')} />
-                    <div styleName="des">今日生日</div>
-                </div>
-                <div styleName="person" onClick={() => this.gotoHandler()}>
-                    <img src={require('../../images/investor/birthday/greyClock.png')} />
-                    <div styleName="name">大熊</div>
-                    <div styleName="date">(9月20日)</div>
-                    <img styleName="arrow" src={require('../../images/investor/birthday/arrow.png')} />
-                    <div styleName="des">还有11天</div>
-                </div>
+                {birthday.list.map(birFn)}
             </div>
             <div styleName="load">已经全部加载完毕</div>
         </div>
