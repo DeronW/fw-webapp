28cc58e028d11c0b25a9ada959d3b96dff3545f7
delong <w.del@qq.com>
2017-11-02 11:05:23 +0800

    try

--- apps/wap/p2p/components/header.js
+++ apps/wap/p2p/components/header.js
@@ -9,31 +9,32 @@ import { Browser, NativeBridge } from '../helpers/'
  <Header title={} history={} show_back={} sub_title={} sub_link={} />
  */
 
-function goBack(props) {
-
-    // 如果传入自定义返回方法, 直接调用并结束
-    if (props.backArrowHandler) {
-        return props.backArrowHandler()
-    }
-
-    // 写了一大堆, 其实是为了兼容在App中点击返回箭头的时候, 关闭当前webviwe
-    let old_pop = window.onpopstate
-
-    let moved = false
-    window.onpopstate = () => moved = true
-
-    props.history && props.history.goBack()
-
-    setTimeout(() => {
-        window.onpopstate = old_pop
-        !moved && NativeBridge.close()
-    }, 100)
-}
-
 const Header = CSSModules(styles, {
     allowMultiple: true,
     errorWhenNotFound: false
 })(props => {
+
+    let goBack = (props) => {
+
+        // 如果传入自定义返回方法, 直接调用并结束
+        if (props.backArrowHandler) {
+            return props.backArrowHandler()
+        }
+
+        // 写了一大堆, 其实是为了兼容在App中点击返回箭头的时候, 关闭当前webviwe
+        let old_pop = window.onpopstate
+
+        let moved = false
+        window.onpopstate = () => moved = true
+
+        props.history && props.history.goBack()
+
+        setTimeout(() => {
+            window.onpopstate = old_pop
+            !moved && NativeBridge.close()
+        }, 100)
+    }
+
     let back = !props.noBack &&
         <a styleName="btn btn-back" onClick={() => goBack(props)}></a>
 
