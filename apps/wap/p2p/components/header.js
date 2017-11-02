b8f99d95869ad2c830752a5535b89e0fe77fd765
delong <w.del@qq.com>
2017-11-02 11:12:51 +0800

    try

--- apps/wap/p2p/components/header.js
+++ apps/wap/p2p/components/header.js
@@ -2,41 +2,40 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import CSSModules from 'react-css-modules'
 import styles from '../css/components/header.css'
-import { Browser, NativeBridge } from '../helpers/'
+import { Browser, NativeBridge } from '../helpers'
 
 /*
  parameters
  <Header title={} history={} show_back={} sub_title={} sub_link={} />
  */
 
+function goBack(props) {
+
+    // 如果传入自定义返回方法, 直接调用并结束
+    if (props.backArrowHandler) {
+        return props.backArrowHandler()
+    }
+
+    // 写了一大堆, 其实是为了兼容在App中点击返回箭头的时候, 关闭当前webviwe
+    let old_pop = window.onpopstate
+
+    let moved = false
+    window.onpopstate = () => moved = true
+
+    props.history && props.history.goBack()
+
+    setTimeout(() => {
+        window.onpopstate = old_pop
+        alert('moved:' + moved + ', ' + NativeBridge.isReady)
+        !moved && NativeBridge.close()
+    }, 100)
+}
+
 const Header = CSSModules(styles, {
     allowMultiple: true,
     errorWhenNotFound: false
 })(props => {
 
-    let goBack = (props) => {
-
-        // 如果传入自定义返回方法, 直接调用并结束
-        if (props.backArrowHandler) {
-            return props.backArrowHandler()
-        }
-
-        // 写了一大堆, 其实是为了兼容在App中点击返回箭头的时候, 关闭当前webviwe
-        let old_pop = window.onpopstate
-
-        let moved = false
-        window.onpopstate = () => moved = true
-
-        alert(moved)
-
-        props.history && props.history.goBack()
-
-        setTimeout(() => {
-            window.onpopstate = old_pop
-            alert(moved)
-            !moved && NativeBridge.close()
-        }, 100)
-    }
 
     let back = !props.noBack &&
         <a styleName="btn btn-back" onClick={() => goBack(props)}></a>
