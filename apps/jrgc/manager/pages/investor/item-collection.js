6348c8a838fa113fa11f30d96432824cae2c8024
sunjiankun <sunjiankun@ucfgroup.com>
2017-11-07 17:57:56 +0800

    fix

--- apps/jrgc/manager/pages/investor/item-collection.js
+++ apps/jrgc/manager/pages/investor/item-collection.js
@@ -31,15 +31,15 @@ class itemCollection extends React.Component {
                 </div>
                 <div styleName="item">
                     <span>预期年化收益率</span>
-                    <span styleName="bold">{item.loanAnnualRate}</span>
+                    <span styleName="bold">{item.loanAnnualRate}%</span>
                 </div>
                 <div styleName="item">
                     <span>起息日</span>
-                    <span>{item.loadDate}天</span>
+                    <span>{item.loanDate}天</span>
                 </div>
                 <div styleName="item">
                     <span>计划回款日</span>
-                    <span>{item.repayTime }天</span>
+                    <span>{item.repayTime}天</span>
                 </div>
                 <div styleName="item">
                     <span>出借金额</span>
@@ -58,7 +58,7 @@ class itemCollection extends React.Component {
             <div styleName="bar">
                 <div styleName="header">
                     <a styleName="btn" onClick={history.goBack}> </a>
-                    {info.colName}
+                    <span>{info.colName}</span>
                 </div>
                 <div styleName="earnings">年化收益 <span>{info.colRate}%</span></div>
                 <div styleName="time">投资期限 <span>{info.colPeriod}</span></div>
@@ -69,7 +69,7 @@ class itemCollection extends React.Component {
                     <div styleName="number">可投项目 {info.canBuyCount}个</div>
                 </div>
                 <div styleName="circle">
-                    <SVGCircleProgress percent={100-(info.percentage)} weight={12}
+                    <SVGCircleProgress percent={Number(info.percentage)} weight={12}
                         radius={120} bgColor={'#64353f'} progressColor={'#fde143'} />
                     <div styleName="percent">{info.percentage}%</div>
                 </div>
